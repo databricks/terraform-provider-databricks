d12a3b4a6ede77548c23a3a31de6bb8f3dc4e2d1
Sriharsha Tikkireddy <sri.tikkireddy@databricks.com>
2020-04-19 20:38:45 -0400

    refactored unit test coverage for secrets, scopes and acls

--- client/service/secret_scopes_test.go
+++ client/service/secret_scopes_test.go
@@ -32,7 +32,7 @@ func TestSecretScopesAPI_Create(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			var input args
-			AssertRequestWithMockServer(t, &tt.args, http.MethodPost, "/api/2.0/secrets/scopes/create", &input, tt.response, nil, tt.wantErr, func(client DBApiClient) (interface{}, error) {
+			AssertRequestWithMockServer(t, &tt.args, http.MethodPost, "/api/2.0/secrets/scopes/create", &input, tt.response, http.StatusOK, nil, tt.wantErr, func(client DBApiClient) (interface{}, error) {
 				return nil, client.SecretScopes().Create(tt.args.Scope, tt.args.InitialManagePrincipal)
 			})
 		})
@@ -61,7 +61,7 @@ func TestSecretScopesAPI_Delete(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			var input args
-			AssertRequestWithMockServer(t, &tt.args, http.MethodPost, "/api/2.0/secrets/scopes/delete", &input, tt.response, nil, tt.wantErr, func(client DBApiClient) (interface{}, error) {
+			AssertRequestWithMockServer(t, &tt.args, http.MethodPost, "/api/2.0/secrets/scopes/delete", &input, tt.response, http.StatusOK, nil, tt.wantErr, func(client DBApiClient) (interface{}, error) {
 				return nil, client.SecretScopes().Delete(tt.args.Scope)
 			})
 		})
@@ -103,7 +103,7 @@ func TestSecretScopesAPI_Read(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			var input args
-			AssertRequestWithMockServer(t, &tt.args, http.MethodGet, "/api/2.0/secrets/scopes/list?", &input, tt.response, tt.want, tt.wantErr, func(client DBApiClient) (interface{}, error) {
+			AssertRequestWithMockServer(t, &tt.args, http.MethodGet, "/api/2.0/secrets/scopes/list?", &input, tt.response, http.StatusOK, tt.want, tt.wantErr, func(client DBApiClient) (interface{}, error) {
 				return client.SecretScopes().Read(tt.args.ScopeName)
 			})
 		})
@@ -144,7 +144,7 @@ func TestSecretScopesAPI_List(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			AssertRequestWithMockServer(t, nil, http.MethodGet, "/api/2.0/secrets/scopes/list?", nil, tt.response, tt.want, tt.wantErr, func(client DBApiClient) (interface{}, error) {
+			AssertRequestWithMockServer(t, nil, http.MethodGet, "/api/2.0/secrets/scopes/list?", nil, tt.response, http.StatusOK, tt.want, tt.wantErr, func(client DBApiClient) (interface{}, error) {
 				return client.SecretScopes().List()
 			})
 		})

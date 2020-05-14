798d894fea3d166ce6babc2da56b316e39d38a8b
Lawrence Gripper <info@grippers.co.uk>
2020-05-14 08:42:28 +0000

    Fix not calling setup when creating config. Remove validateapi workaround.
    Co-authored-by: Stuart Leeks <stuart@leeks.net>

--- databricks/azure_auth_test.go
+++ databricks/azure_auth_test.go
@@ -1,13 +1,12 @@
 package databricks
 
 import (
-	"errors"
+	"os"
+	"testing"
+
 	"github.com/databrickslabs/databricks-terraform/client/model"
 	"github.com/databrickslabs/databricks-terraform/client/service"
 	"github.com/stretchr/testify/assert"
-	"log"
-	"os"
-	"testing"
 )
 
 func GetIntegrationDBClientOptions() *service.DBApiClientConfig {
@@ -57,36 +56,4 @@ func TestAzureAuthCreateApiToken(t *testing.T) {
 	}()
 
 	assert.NoError(t, instancePoolErr, instancePoolErr)
-}
-
-func TestValidateWorkspaceApis(t *testing.T) {
-
-	// Eventually will pass after a few attempts
-	err := validateWorkspaceApis(0, 1, func(attempt int) error {
-		log.Printf("Attempt: %v hello world\n", attempt)
-		if attempt == 2 {
-			return nil
-		}
-		return errors.New("com.databricks.backend.manager.util.UnknownWorkerEnvironmentException: Unknown worker environment WorkerEnvId")
-	})
-	assert.NoError(t, err, err)
-
-	// Eventually will fail with an error after a few attempts
-	expectedError := errors.New("failing valid error")
-	err = validateWorkspaceApis(0, 1, func(attempt int) error {
-		log.Printf("Attempt: %v hello world\n", attempt)
-		if attempt == 2 {
-			return expectedError
-		}
-		return errors.New("com.databricks.backend.manager.util.UnknownWorkerEnvironmentException: Unknown worker environment WorkerEnvId")
-	})
-	assert.Error(t, err, err)
-
-	// Eventually will timeout with an error after 0 attempts
-	//expectedError := errors.New("failing valid error")
-	err = validateWorkspaceApis(0, 0, func(attempt int) error {
-		log.Printf("Attempt: %v hello world\n", attempt)
-		return errors.New("com.databricks.backend.manager.util.UnknownWorkerEnvironmentException: Unknown worker environment WorkerEnvId")
-	})
-	assert.Error(t, err, err)
 }

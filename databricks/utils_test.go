67edef8ce4726d65a41051a9aaf3a8b136469bbf
Sriharsha Tikkireddy <sri.tikkireddy@databricks.com>
2020-06-15 06:18:07 -0400

    added databricks_group, databricks_group_member & databricks_group_role to manage the scim resources separately

--- databricks/utils_test.go
+++ databricks/utils_test.go
@@ -29,3 +29,22 @@ func TestIsClusterMissingFalseWhenErrorNotInCorrectFormat(t *testing.T) {
 
 	assert.False(t, result)
 }
+
+func TestValidateInstanceProfileARN(t *testing.T) {
+	testCases := []struct {
+		instanceProfileARN  string
+		errorCount  int
+	}{
+		{"arn:aws:iam::999999999999:instance-profile/my-fake-instance-profile", 0},
+		{"arn:aws:iam::999999999999:role/not-an-instance-profile", 1},
+		{"", 1},
+		{"invalid-profile", 1},
+	}
+	for _, tc := range testCases {
+		_, errs := ValidateInstanceProfileARN(tc.instanceProfileARN, "key")
+
+		assert.Lenf(t, errs, tc.errorCount, "directory '%s' does not generate the expected error count", tc.instanceProfileARN)
+	}
+}
+
+

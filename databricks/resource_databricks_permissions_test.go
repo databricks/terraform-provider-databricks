de71190999cc620f17f83fc64e94a25343bd12ec
Serge Smertin <serge.smertin@databricks.com>
2020-06-24 00:07:01 +0200

    Added real unit tests for resource

--- databricks/resource_databricks_permissions_test.go
+++ databricks/resource_databricks_permissions_test.go
@@ -2,15 +2,165 @@ package databricks
 
 import (
 	"fmt"
+	"net/http"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/databrickslabs/databricks-terraform/client/model"
 	"github.com/databrickslabs/databricks-terraform/client/service"
 )
+
+var (
+	TestingUser      = "ben"
+	TestingAdminUser = "admin"
+)
+
+func TestPermissionsRead(t *testing.T) {
+	d, err := ResourceTester(t, []HTTPFixture{
+		{
+			Method:   http.MethodGet,
+			Resource: "/api/2.0/preview/permissions/clusters/abc?",
+			Response: model.ObjectACL{
+				ObjectID:   "/clusters/abc",
+				ObjectType: "clusters",
+				AccessControlList: []*model.AccessControl{
+					{
+						UserName: &TestingUser,
+						AllPermissions: []*model.Permission{
+							{
+								PermissionLevel: "CAN_READ",
+								Inherited:       false,
+							},
+						},
+					},
+					{
+						UserName: &TestingAdminUser,
+						AllPermissions: []*model.Permission{
+							{
+								PermissionLevel: "CAN_MANAGE",
+								Inherited:       false,
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			Method:   http.MethodGet,
+			Resource: "/api/2.0/preview/scim/v2/Me?",
+			Response: model.User{
+				UserName: TestingAdminUser,
+			},
+		},
+	}, resourcePermissions, map[string]interface{}{},
+		func(d *schema.ResourceData, c interface{}) error {
+			d.SetId("/clusters/abc")
+			return resourcePermissionsRead(d, c)
+		})
+	assert.NoError(t, err, err)
+	assert.Equal(t, "/clusters/abc", d.Id())
+	assert.Equal(t, TestingUser, d.Get("access_control.0.user_name"))
+	assert.Equal(t, "CAN_READ", d.Get("access_control.0.permission_level"))
+	assert.Equal(t, 1, d.Get("access_control.#"))
+}
+
+func TestPermissionsDelete(t *testing.T) {
+	d, err := ResourceTester(t, []HTTPFixture{
+		{
+			Method:          http.MethodPut,
+			Resource:        "/api/2.0/preview/permissions/clusters/abc",
+			ExpectedRequest: model.ObjectACL{},
+		},
+	}, resourcePermissions, map[string]interface{}{},
+		func(d *schema.ResourceData, c interface{}) error {
+			d.SetId("/clusters/abc")
+			return resourcePermissionsDelete(d, c)
+		})
+	assert.NoError(t, err, err)
+	assert.Equal(t, "/clusters/abc", d.Id())
+}
+
+func TestPermissionsCreate_invalid(t *testing.T) {
+	_, err := ResourceTester(t, []HTTPFixture{}, resourcePermissions, map[string]interface{}{},
+		resourcePermissionsCreate)
+	assert.EqualError(t, err, "At least one type of resource identifiers must be set")
+}
+
+func TestPermissionsCreate_no_access_control(t *testing.T) {
+	_, err := ResourceTester(t, []HTTPFixture{}, resourcePermissions,
+		map[string]interface{}{
+			"cluster_id": "abc",
+		}, resourcePermissionsCreate)
+	assert.EqualError(t, err, "At least one access_control is required")
+}
+
+func TestPermissionsCreate(t *testing.T) {
+	d, err := ResourceTester(t, []HTTPFixture{
+		{
+			Method:   http.MethodPatch,
+			Resource: "/api/2.0/preview/permissions/clusters/abc",
+			ExpectedRequest: model.AccessControlChangeList{
+				AccessControlList: []*model.AccessControlChange{
+					{
+						UserName:        &TestingUser,
+						PermissionLevel: "CAN_USE",
+					},
+				},
+			},
+		},
+		{
+			Method:   http.MethodGet,
+			Resource: "/api/2.0/preview/permissions/clusters/abc?",
+			Response: model.ObjectACL{
+				ObjectID:   "/clusters/abc",
+				ObjectType: "clusters",
+				AccessControlList: []*model.AccessControl{
+					{
+						UserName: &TestingUser,
+						AllPermissions: []*model.Permission{
+							{
+								PermissionLevel: "CAN_READ",
+								Inherited:       false,
+							},
+						},
+					},
+					{
+						UserName: &TestingAdminUser,
+						AllPermissions: []*model.Permission{
+							{
+								PermissionLevel: "CAN_MANAGE",
+								Inherited:       false,
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			Method:   http.MethodGet,
+			Resource: "/api/2.0/preview/scim/v2/Me?",
+			Response: model.User{
+				UserName: TestingAdminUser,
+			},
+		},
+	}, resourcePermissions, map[string]interface{}{
+		"cluster_id": "abc",
+		"access_control": []interface{}{
+			map[string]interface{}{
+				"user_name":        TestingUser,
+				"permission_level": "CAN_USE",
+			},
+		},
+	}, resourcePermissionsCreate)
+	assert.NoError(t, err, err)
+	assert.Equal(t, TestingUser, d.Get("access_control.0.user_name"))
+	assert.Equal(t, "CAN_READ", d.Get("access_control.0.permission_level"))
+	assert.Equal(t, 1, d.Get("access_control.#"))
+}
 
 func TestAccDatabricksPermissionsResourceFullLifecycle(t *testing.T) {
 	var permissions model.ObjectACL

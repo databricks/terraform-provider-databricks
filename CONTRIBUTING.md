083c23287d5e722ed3e975d077c4bf13c61e4179
Serge Smertin <serge.smertin@databricks.com>
2020-06-24 00:07:01 +0200

    Added real unit tests for resource

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,1 +1,60 @@
 We happily welcome contributions to databricks-terraform. We use GitHub Issues to track community reported issues and GitHub Pull Requests for accepting changes.
+
+## Unit testing resources
+
+In order to unit test a resource, which runs fast and could be included in code coverage, one should use `ResourceTester`, that launches embedded HTTP server with `HTTPFixture`'s containing all calls that should have been made in given scenario:
+
+```go
+func TestPermissionsCreate(t *testing.T) {
+	_, err := ResourceTester(t, []HTTPFixture{
+		{
+            Method:   http.MethodPatch,
+            // requires full URI
+            Resource: "/api/2.0/preview/permissions/clusters/abc",
+            // works with entities, not JSON. Diff is displayed in case of missmatch 
+			ExpectedRequest: model.AccessControlChangeList{
+				AccessControlList: []*model.AccessControlChange{
+					{
+						UserName:        &TestingUser,
+						PermissionLevel: "CAN_USE",
+					},
+				},
+			},
+		},
+		{
+			Method:   http.MethodGet,
+			Resource: "/api/2.0/preview/permissions/clusters/abc?",
+			Response: model.AccessControlChangeList{
+				AccessControlList: []*model.AccessControlChange{
+					{
+						UserName:        &TestingUser,
+						PermissionLevel: "CAN_MANAGE",
+					},
+				},
+			},
+		},
+		{
+			Method:   http.MethodGet,
+			Resource: "/api/2.0/preview/scim/v2/Me?",
+			Response: model.User{
+				UserName: "chuck.norris",
+			},
+		},
+    }, 
+    // next argument is function, that creates resource (to make schema for ResourceData)
+    resourcePermissions, 
+    // state represented as native structure (though a bit clunky)
+    map[string]interface{}{
+		"cluster_id": "abc",
+		"access_control": []interface{}{
+			map[string]interface{}{
+				"user_name":        TestingUser,
+				"permission_level": "CAN_USE",
+			},
+		},
+    },
+    // the last argument is a function, that performs a stage on resource (Create/update/delete/read)
+    resourcePermissionsCreate)
+	assert.NoError(t, err, err)
+}
+```
--- databricks/utils_test.go
+++ databricks/utils_test.go
@@ -1,10 +1,81 @@
 package databricks
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/databrickslabs/databricks-terraform/client/service"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/stretchr/testify/assert"
 )
+
+// HTTPFixture defines request structure for test
+type HTTPFixture struct {
+	Method          string
+	Resource        string
+	Response        interface{}
+	Status          int
+	ExpectedRequest interface{}
+}
+
+// ResourceTester helps testing HTTP resources with fixtures
+func ResourceTester(t *testing.T,
+	fixtures []HTTPFixture,
+	resouceFunc func() *schema.Resource,
+	state map[string]interface{},
+	whatever func(d *schema.ResourceData, c interface{}) error) (*schema.ResourceData, error) {
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		found := false
+		for _, fixture := range fixtures {
+			if req.Method == fixture.Method && req.RequestURI == fixture.Resource {
+				if fixture.Status == 0 {
+					rw.WriteHeader(200)
+				} else {
+					rw.WriteHeader(fixture.Status)
+				}
+				if fixture.ExpectedRequest != nil {
+					buf := new(bytes.Buffer)
+					_, err := buf.ReadFrom(req.Body)
+					assert.NoError(t, err, err)
+					jsonStr, err := json.Marshal(fixture.ExpectedRequest)
+					assert.NoError(t, err, err)
+					assert.JSONEq(t, string(jsonStr), buf.String())
+				}
+				if fixture.Response != nil {
+					responseBytes, err := json.Marshal(fixture.Response)
+					if err != nil {
+						assert.NoError(t, err, err)
+						t.FailNow()
+					}
+					_, err = rw.Write(responseBytes)
+					assert.NoError(t, err, err)
+				}
+				found = true
+				break
+			}
+		}
+		if !found {
+			assert.Fail(t, fmt.Sprintf("Received unexpected call: %s %s", req.Method, req.RequestURI))
+			t.FailNow()
+		}
+	}))
+
+	defer server.Close()
+	var config service.DBApiClientConfig
+	config.Host = server.URL
+	config.Setup()
+
+	var client service.DBApiClient
+	client.SetConfig(&config)
+
+	resource := resouceFunc()
+	resourceData := schema.TestResourceDataRaw(t, resource.Schema, state)
+	return resourceData, whatever(resourceData, &client)
+}
 
 func TestIsClusterMissingTrueWhenClusterIdSpecifiedPresent(t *testing.T) {
 	errorMessage := "{\"error_code\":\"INVALID_PARAMETER_VALUE\",\"message\":\"Cluster 123 does not exist\"}"

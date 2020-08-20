8c930bfedef6c080cfea3fd0cba792faddea8223
Serge Smertin <259697+nfx@users.noreply.github.com>
2020-08-20 14:39:36 +0200

    Added testing for auth edge cases (#240)
    * Added testing for auth edge cases
    * Adjusting tests for machines without az cli
    * added testgen docs
    * Added Azure VNet injection example
    * switch az cli mock from sh to bash
    
    Co-authored-by: Serge Smertin <serge.smertin@databricks.com>

---
+++ scripts/azvnet-integration/main.tf
@@ -0,0 +1,36 @@
+resource "azurerm_databricks_workspace" "this" {
+  name                        = "${local.prefix}-workspace"
+  resource_group_name         = azurerm_resource_group.example.name
+  location                    = azurerm_resource_group.example.location
+  sku                         = "premium"
+  managed_resource_group_name = "${local.prefix}-workspace-rg"
+
+  custom_parameters {
+    no_public_ip = false
+    virtual_network_id          = azurerm_virtual_network.example.id
+    public_subnet_name          = azurerm_subnet.public.name
+    private_subnet_name         = azurerm_subnet.private.name
+  }
+
+  tags = local.tags
+}
+
+output "databricks_azure_workspace_resource_id" {
+  // The ID of the Databricks Workspace in the Azure management plane.
+  value = azurerm_databricks_workspace.this.id
+}
+
+output "workspace_url" {
+  // The workspace URL which is of the format 'adb-{workspaceId}.{random}.azuredatabricks.net'
+  // this is not named as DATABRICKS_HOST, because it affect authentication
+  value = "https://${azurerm_databricks_workspace.this.workspace_url}/"
+}
+
+output "cloud_env" {
+  value = "azure"
+}
+
+output "test_node_type" {
+  // or i3.xlarge for AWS
+  value = "Standard_D3_v2"
+}

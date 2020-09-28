45a1c3c60c3f735b431800822443b81e387db4aa
Serge Smertin <serge.smertin@databricks.com>
2020-09-28 19:03:49 +0200

    Initial commit for log delivery resource

---
+++ docs/resources/mws_log_delivery.md
@@ -0,0 +1,36 @@
+# databricks_mws_log_delivery Resource
+
+-> **Note** This resource has evolving API, which may change in future versions of provider.
+
+This resource configures delivery of various log types from databricks workspaces. Backend does not support removal of log delivery configuration, so provider handles it through simply disabling the config.
+
+## Example Usage
+
+```hcl
+resource "databricks_mws_customer_managed_keys" "my_cmk" {
+    account_id   = var.account_id
+    credentials_id = databricks_mws_credentials.log_writer.credentials_id
+    storage_configuration_id = databricks_mws_storage_configurations.log_bucket.storage_configuration_id
+    config_name = "Usage Logs"
+    log_type = "BILLABLE_USAGE"
+    output_format = "CSV"
+}
+```
+
+## Argument reference
+
+* `account_id` - The Databricks account ID that hosts the log delivery configuration.
+* `config_name`	- The optional human-readable name of the log delivery configuration. Defaults to empty.
+* `log_type` - The type of log delivery. Currently only [BILLABLE_USAGE](https://docs.databricks.com/administration-guide/account-settings/usage.html#download-usage-as-a-csv-file) is supported
+* `output_format` - The file type of log delivery. Currently only `CSV` supported.
+* `credentials_id` - The ID for a Databricks [credential configuration](mws_credentials.md) that represents the AWS IAM role with policy and trust relationship as described in the main billable usage documentation page.
+* `storage_configuration_id` - The ID for a Databricks [storage configuration](mws_storage_configurations.md) that represents the S3 bucket with bucket policy as described in the main billable usage documentation page.
+* `workspace_ids_filter` - (Optional) The optional filter for workspaces. By default, this log configuration applies to all workspaces associated with your account ID. For some types of deployments there is only one workspace per account ID so this field is unnecessary. If your account is on the E2 version of the platform or on a select custom plan that allows multiple workspaces per account, you may have multiple workspaces associated with your account ID. You can optionally set this field to array of workspace IDs (each one is an int64) that this configuration applies to. If you plan to use different log delivery configurations for different workspaces, set this explicitly rather than leaving it blank. If you leave this blank and your account ID is associated in the future with additional workspaces, this configuration also applies to the new workspaces.
+* `delivery_path_prefix` - (Optional) The optional delivery path prefix within AWS S3 storage. Defaults to empty, which means that logs are delivered to the root of the bucket. This must be a valid S3 object key. This must not start or end with a slash character.
+* `delivery_start_time`	- (Optional) The optional start month and year for delivery, specified in YYYY-MM format. Defaults to current year and month. Usage is not available before 2019-03.
+
+## Attribute reference
+
+Following attributes are exported
+
+* `config_id` - Databricks log delivery configuration ID.
---
+++ mws/resource_log_delivery.go
@@ -0,0 +1,131 @@
+package mws
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/databrickslabs/databricks-terraform/common"
+	"github.com/databrickslabs/databricks-terraform/internal"
+	"github.com/databrickslabs/databricks-terraform/internal/util"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
+
+// LogDelivery wrapper
+type LogDelivery struct {
+	LogDeliveryConfiguration LogDeliveryConfiguration `json:"log_delivery_configuration"`
+}
+
+// LogDeliveryList lists log delivery configurations
+type LogDeliveryList struct {
+	LogDeliveryConfigurations []LogDeliveryConfiguration `json:"log_delivery_configurations"`
+}
+
+// LogDeliveryConfiguration describes log delivery
+type LogDeliveryConfiguration struct {
+	AccountID              string   `json:"account_id"`
+	ConfigID               string   `json:"config_id,omitempty" tf:"computed"`
+	CredentialsID          string   `json:"credentials_id"`
+	StorageConfigurationID string   `json:"storage_configuration_id"`
+	WorkspaceIdsFilter     []string `jsong:"workspace_ids_filter,omitempty"`
+	ConfigName             string   `json:"config_name,omitempty"`
+	Status                 string   `json:"status,omitempty"`
+	LogType                string   `json:"log_type"`
+	OutputFormat           string   `json:"output_format"`
+	DeliveryPathPrefix     string   `json:"delivery_path_prefix,omitempty"`
+	DeliveryStartTime      string   `json:"delivery_start_time,omitempty"`
+}
+
+// LogDeliveryAPI ...
+type LogDeliveryAPI struct {
+	client *common.DatabricksClient
+}
+
+// NewLogDeliveryAPI ...
+func NewLogDeliveryAPI(m interface{}) LogDeliveryAPI {
+	return LogDeliveryAPI{client: m.(*common.DatabricksClient)}
+}
+
+// Read reads log delivery configuration
+func (a LogDeliveryAPI) Read(accountID, configID string) (LogDeliveryConfiguration, error) {
+	var ld LogDelivery
+	err := a.client.Get(fmt.Sprintf("/accounts/%s/log-delivery/%s", accountID, configID), nil, &ld)
+	return ld.LogDeliveryConfiguration, err
+}
+
+// Create new log delivery configuration
+func (a LogDeliveryAPI) Create(ldc LogDeliveryConfiguration) (string, error) {
+	var ld LogDelivery
+	err := a.client.Post(fmt.Sprintf("/accounts/%s/log-delivery", ldc.AccountID), LogDelivery{
+		LogDeliveryConfiguration: ldc,
+	}, &ld)
+	// todo: verify with empty response - structs should have empty default strings
+	return ld.LogDeliveryConfiguration.ConfigID, err
+}
+
+// Disable log delivery configuration - e.g. delete it
+func (a LogDeliveryAPI) Disable(accountID, configID string) error {
+	return a.client.Patch(fmt.Sprintf("/accounts/%s/log-delivery/%s", accountID, configID), map[string]string{
+		"status": "DISABLED",
+	})
+}
+
+// ResourceLogDelivery ..
+func ResourceLogDelivery() *schema.Resource {
+	p := util.NewPairID("account_id", "config_id")
+	s := internal.StructToSchema(LogDeliveryConfiguration{},
+		func(s map[string]*schema.Schema) map[string]*schema.Schema {
+			s["account_id"].ForceNew = true
+			// nolint
+			s["config_name"].ValidateFunc = validation.StringLenBetween(0, 255)
+			return s
+		})
+	readContext := func(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+		accountID, configID, err := p.Unpack(d)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+		ldc, err := NewLogDeliveryAPI(m).Read(accountID, configID)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+		err = internal.StructToData(ldc, s, d)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+		return nil
+	}
+	return &schema.Resource{
+		Schema:      s,
+		ReadContext: readContext,
+		CreateContext: func(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+			var ldc LogDeliveryConfiguration
+			err := internal.DataToStructPointer(d, s, &ldc)
+			if err != nil {
+				return diag.FromErr(err)
+			}
+			configID, err := NewLogDeliveryAPI(m).Create(ldc)
+			if err != nil {
+				return diag.FromErr(err)
+			}
+			err = d.Set("config_id", configID)
+			if err != nil {
+				return diag.FromErr(err)
+			}
+			p.Pack(d)
+			return readContext(ctx, d, m)
+		},
+		DeleteContext: func(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+			accountID, configID, err := p.Unpack(d)
+			if err != nil {
+				return diag.FromErr(err)
+			}
+			err = NewLogDeliveryAPI(m).Disable(accountID, configID)
+			if err != nil {
+				return diag.FromErr(err)
+			}
+			return nil
+		},
+	}
+}

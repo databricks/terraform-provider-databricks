d36576759a578efdacb2affb3da40acd04e2c40f
Alex Ott <alexey.ott@databricks.com>
2020-11-25 17:09:08 +0100

    more tests for clusters & jobs

--- importer/importer_test.go
+++ importer/importer_test.go
@@ -302,14 +302,16 @@ func TestImportingClusters(t *testing.T) {
 			Response: getJSONObject("test-data/get-cluster-permissions-test1-response.json"),
 		},
 		{
-			Method:   "GET",
-			Resource: "/api/2.0/dbfs/get-status?path=dbfs:%2FFileStore%2Fjars%2Fe388b491_7af3_4a41_9d3f_1966272df00f-spark_mssql_connector_assembly_1_2_0-99f79.jar",
-			Response: getJSONObject("test-data/get-dbfs-library-status.json"),
+			Method:       "GET",
+			Resource:     "/api/2.0/dbfs/get-status?path=dbfs:%2FFileStore%2Fjars%2Ftest.jar",
+			ReuseRequest: true,
+			Response:     getJSONObject("test-data/get-dbfs-library-status.json"),
 		},
 		{
-			Method:   "GET",
-			Resource: "/api/2.0/dbfs/read?length=1000000&path=dbfs%3A%2FFileStore%2Fjars%2Fe388b491_7af3_4a41_9d3f_1966272df00f-spark_mssql_connector_assembly_1_2_0-99f79.jar",
-			Response: getJSONObject("test-data/get-dbfs-library-data.json"),
+			Method:       "GET",
+			Resource:     "/api/2.0/dbfs/read?length=1000000&path=dbfs%3A%2FFileStore%2Fjars%2Ftest.jar",
+			ReuseRequest: true,
+			Response:     getJSONObject("test-data/get-dbfs-library-data.json"),
 		},
 		{
 			Method:   "GET",
@@ -348,6 +350,37 @@ func TestImportingClusters(t *testing.T) {
 			Response: getJSONObject("test-data/get-cluster-policy-permissions.json"),
 		},
 		{
+			Method:   "GET",
+			Resource: "/api/2.0/clusters/get?cluster_id=awscluster",
+			Response: getJSONObject("test-data/get-cluster-awscluster-response.json"),
+		},
+		{
+			Method:   "POST",
+			Resource: "/api/2.0/clusters/events",
+			ExpectedRequest: compute.EventsRequest{
+				ClusterID:  "awscluster",
+				Order:      "DESC",
+				EventTypes: []compute.ClusterEventType{"PINNED", "UNPINNED"},
+				Limit:      1,
+			},
+			Response: compute.EventDetails{},
+		},
+		{
+			Method:   "GET",
+			Resource: "/api/2.0/libraries/cluster-status?cluster_id=awscluster",
+			Response: getJSONObject("test-data/libraries-cluster-status-test2.json"),
+		},
+		{
+			Method:   "GET",
+			Resource: "/api/2.0/preview/permissions/clusters/awscluster",
+			Response: getJSONObject("test-data/get-cluster-permissions-awscluster-response.json"),
+		},
+		{
+			Method:   "GET",
+			Resource: "/api/2.0/instance-profiles/list",
+			Response: getJSONObject("test-data/list-instance-profiles.json"),
+		},
+		{
 			Method:       "GET",
 			Resource:     "/api/2.0/preview/scim/v2/Me",
 			ReuseRequest: true,
@@ -367,6 +400,7 @@ func TestImportingClusters(t *testing.T) {
 	assert.NoError(t, err)
 }
 
+// TODO: add spark_submit_task job
 func TestImportingJobs(t *testing.T) {
 	defer common.CleanupEnvironment()()
 	_, server, err := qa.HttpFixtureClient(t, []qa.HTTPFixture{
@@ -435,6 +469,60 @@ func TestImportingJobs(t *testing.T) {
 			Method:   "GET",
 			Resource: "/api/2.0/preview/permissions/cluster-policies/123",
 			Response: getJSONObject("test-data/get-cluster-policy-permissions.json"),
+		},
+		{
+			Method:   "GET",
+			Resource: "/api/2.0/jobs/get?job_id=15",
+			Response: getJSONObject("test-data/get-job-15.json"),
+		},
+		{
+			Method:   "GET",
+			Resource: "/api/2.0/preview/permissions/jobs/15",
+			Response: getJSONObject("test-data/get-job-permissions-15.json"),
+		},
+		{
+			Method:       "GET",
+			Resource:     "/api/2.0/dbfs/get-status?path=dbfs:%2FFileStore%2Fjars%2Ftest.jar",
+			ReuseRequest: true,
+			Response:     getJSONObject("test-data/get-dbfs-library-status.json"),
+		},
+		{
+			Method:       "GET",
+			Resource:     "/api/2.0/dbfs/read?length=1000000&path=dbfs%3A%2FFileStore%2Fjars%2Ftest.jar",
+			ReuseRequest: true,
+			Response:     getJSONObject("test-data/get-dbfs-library-data.json"),
+		},
+		{
+			Method:       "GET",
+			Resource:     "/api/2.0/instance-pools/get?instance_pool_id=pool1",
+			ReuseRequest: true,
+			Response:     getJSONObject("test-data/get-instance-pool1.json"),
+		},
+		{
+			Method:       "GET",
+			Resource:     "/api/2.0/preview/permissions/instance-pools/pool1",
+			ReuseRequest: true,
+			Response:     getJSONObject("test-data/get-instance-pool1-permissions.json"),
+		},
+		{
+			Method:   "GET",
+			Resource: "/api/2.0/jobs/get?job_id=16",
+			Response: getJSONObject("test-data/get-job-16.json"),
+		},
+		{
+			Method:   "GET",
+			Resource: "/api/2.0/preview/permissions/jobs/16",
+			Response: getJSONObject("test-data/get-job-permissions-16.json"),
+		},
+		{
+			Method:   "GET",
+			Resource: "/api/2.0/jobs/get?job_id=17",
+			Response: getJSONObject("test-data/get-job-17.json"),
+		},
+		{
+			Method:   "GET",
+			Resource: "/api/2.0/preview/permissions/jobs/17",
+			Response: getJSONObject("test-data/get-job-permissions-17.json"),
 		},
 		{
 			Method:       "GET",

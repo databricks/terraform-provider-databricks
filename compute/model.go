89aae68ab1e7eb7558cffed5e892d9312ae0d292
Serge Smertin <serge.smertin@databricks.com>
2020-11-27 00:35:01 +0100

    Added context to InstancePoolsAPI

--- compute/model.go
+++ compute/model.go
@@ -345,7 +345,7 @@ type Command struct {
 // InstancePoolAwsAttributes contains aws attributes for AWS Databricks deployments for instance pools
 type InstancePoolAwsAttributes struct {
 	Availability        AwsAvailability `json:"availability,omitempty"`
-	ZoneID              string          `json:"zone_id,omitempty"`
+	ZoneID              string          `json:"zone_id"`
 	SpotBidPricePercent int32           `json:"spot_bid_price_percent,omitempty"`
 }
 
@@ -364,13 +364,14 @@ type InstancePoolDiskSpec struct {
 
 // InstancePool describes the instance pool object on Databricks
 type InstancePool struct {
-	InstancePoolName                   string                     `json:"instance_pool_name,omitempty"`
+	InstancePoolID                     string                     `json:"instance_pool_id,omitempty" tf:"computed"`
+	InstancePoolName                   string                     `json:"instance_pool_name"`
 	MinIdleInstances                   int32                      `json:"min_idle_instances,omitempty"`
 	MaxCapacity                        int32                      `json:"max_capacity,omitempty"`
+	IdleInstanceAutoTerminationMinutes int32                      `json:"idle_instance_autotermination_minutes"`
 	AwsAttributes                      *InstancePoolAwsAttributes `json:"aws_attributes,omitempty"`
-	NodeTypeID                         string                     `json:"node_type_id,omitempty"`
+	NodeTypeID                         string                     `json:"node_type_id"`
 	CustomTags                         map[string]string          `json:"custom_tags,omitempty"`
-	IdleInstanceAutoTerminationMinutes int32                      `json:"idle_instance_autotermination_minutes,omitempty"`
 	EnableElasticDisk                  bool                       `json:"enable_elastic_disk,omitempty"`
 	DiskSpec                           *InstancePoolDiskSpec      `json:"disk_spec,omitempty"`
 	PreloadedSparkVersions             []string                   `json:"preloaded_spark_versions,omitempty"`
@@ -386,15 +387,15 @@ type InstancePoolStats struct {
 
 // InstancePoolAndStats encapsulates a get response from the GET api for instance pools on Databricks
 type InstancePoolAndStats struct {
-	InstancePoolID                     string                     `json:"instance_pool_id,omitempty"`
-	InstancePoolName                   string                     `json:"instance_pool_name,omitempty"`
+	InstancePoolID                     string                     `json:"instance_pool_id,omitempty" tf:"computed"`
+	InstancePoolName                   string                     `json:"instance_pool_name"`
 	MinIdleInstances                   int32                      `json:"min_idle_instances,omitempty"`
 	MaxCapacity                        int32                      `json:"max_capacity,omitempty"`
 	AwsAttributes                      *InstancePoolAwsAttributes `json:"aws_attributes,omitempty"`
-	NodeTypeID                         string                     `json:"node_type_id,omitempty"`
-	DefaultTags                        map[string]string          `json:"default_tags,omitempty"`
+	NodeTypeID                         string                     `json:"node_type_id"`
+	DefaultTags                        map[string]string          `json:"default_tags,omitempty" tf:"computed"`
 	CustomTags                         map[string]string          `json:"custom_tags,omitempty"`
-	IdleInstanceAutoTerminationMinutes int32                      `json:"idle_instance_autotermination_minutes,omitempty"`
+	IdleInstanceAutoTerminationMinutes int32                      `json:"idle_instance_autotermination_minutes"`
 	EnableElasticDisk                  bool                       `json:"enable_elastic_disk,omitempty"`
 	DiskSpec                           *InstancePoolDiskSpec      `json:"disk_spec,omitempty"`
 	PreloadedSparkVersions             []string                   `json:"preloaded_spark_versions,omitempty"`

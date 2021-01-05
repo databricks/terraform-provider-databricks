625912dcbcdb3db14efa6158ffd309b2a1d65224
Navid Bazzazzadeh <bazzazzadeh@gmail.com>
2021-01-05 15:36:30 -0500

    the first round of code review

---
+++ mws/acceptance/vpc_endpoint_test.go
@@ -0,0 +1,30 @@
+package acceptance
+
+import (
+	"os"
+	"testing"
+
+	"github.com/databrickslabs/databricks-terraform/internal/acceptance"
+)
+
+func TestMwsAccVpcEndpoint(t *testing.T) {
+	cloudEnv := os.Getenv("CLOUD_ENV")
+	if cloudEnv != "MWS" {
+		t.Skip("Cannot run test on non-MWS environment")
+	}
+	acceptance.Test(t, []acceptance.Step{
+		{
+			Template: `provider "databricks" {
+				host     = "{env.DATABRICKS_HOST}"
+				username = "{env.DATABRICKS_USERNAME}"
+				password = "{env.DATABRICKS_PASSWORD}"
+			}
+			resource "databricks_mws_vpc_endpoint" "my_vpc_endpoint" {
+				account_id   = "{env.DATABRICKS_ACCOUNT_ID}"
+				vpc_endpoint_name = "my_vpc_name"
+				region = "{env.TEST_REGION}"
+				aws_vpc_endpoint_id       = "vpc-endpoint-11111111"
+			}`,
+		},
+	})
+}

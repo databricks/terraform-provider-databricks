96cd3ac351950de8d7829f06904c980c768c7af4
Navid Bazzazzadeh <bazzazzadeh@gmail.com>
2021-01-16 22:07:43 -0500

    apply new comments

--- mws/acceptance/vpc_endpoint_test.go
+++ mws/acceptance/vpc_endpoint_test.go
@@ -14,13 +14,14 @@ func TestMwsAccVpcEndpoint(t *testing.T) {
 	}
 	acceptance.Test(t, []acceptance.Step{
 		{
-			Template: `resource "databricks_mws_vpc_endpoint" "my_vpc_endpoint_relay" {
-				account_id   = "{env.DATABRICKS_ACCOUNT_ID}"
-				vpc_endpoint_name = "my_t2_vpce_relay_name"
-				region = "{env.TEST_REGION}"
-				aws_vpc_endpoint_id       = "{env.AWS_VPC_RELAY_ENDPOINT_ID}"
-				aws_account_id = "997819012307"
-			}`,
+			Template: `
+				resource "databricks_mws_vpc_endpoint" "this" {
+					account_id   = "{env.DATABRICKS_ACCOUNT_ID}"
+					vpc_endpoint_name = "tf-{var.RANDOM}"
+					region = "{env.TEST_REGION}"
+					aws_vpc_endpoint_id       = "{env.AWS_VPC_RELAY_ENDPOINT_ID}"
+					aws_account_id = "{env.DATABRICKS_AWS_ACCOUNT_ID}"
+				}`,
 		},
 	})
 }

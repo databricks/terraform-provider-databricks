ea463ece3a8c7e5511565feaab857dea7b7f558a
Navid Bazzazzadeh <bazzazzadeh@gmail.com>
2021-01-16 21:29:39 -0500

    all 3 resources in TF

--- scripts/mws-pl-integration/main.tf
+++ scripts/mws-pl-integration/main.tf
@@ -24,8 +24,32 @@ resource "databricks_mws_vpc_endpoint" "vpce_relay"{
     region= data.external.env.result.TEST_REGION
 }
 
-output "test_databricks_mws_vpc_endpoint" {
+resource "databricks_mws_vpc_endpoint" "vpce_rest"{
+    account_id = data.external.env.result.DATABRICKS_ACCOUNT_ID
+    aws_vpc_endpoint_id =data.external.env.result.DATABRICKS_AWS_VPCE_REST_ID
+    vpc_endpoint_name=data.external.env.result.DATABRICKS_AWS_VPCE_REST_NAME
+    region= data.external.env.result.TEST_REGION
+}
+
+
+resource "databricks_mws_private_access_settings" "pas"{
+    account_id = data.external.env.result.DATABRICKS_ACCOUNT_ID
+    private_access_settings_name=data.external.env.result.DATABRICKS_PAS_NAME
+    region= data.external.env.result.TEST_REGION
+
+}
+
+output "test_databricks_mws_vpc_relay_endpoint" {
     value = databricks_mws_vpc_endpoint.vpce_relay.vpc_endpoint_id
 }
+
+output "test_databricks_mws_vpc_rest_endpoint" {
+    value = databricks_mws_vpc_endpoint.vpce_rest.vpc_endpoint_id
+}
+
+output "test_databricks_mws_private_access_settings"{
+  value = databricks_mws_private_access_settings.pas.private_access_settings_id
+}
+
 
 

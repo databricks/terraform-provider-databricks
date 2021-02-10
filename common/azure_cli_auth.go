d37e80fc217bb88eb8b76ec14a75ad63cc6682be
Serge Smertin <serge.smertin@databricks.com>
2021-02-10 21:16:01 +0100

    Simplify Azure auth by getting resource from workspace url

--- common/azure_cli_auth.go
+++ common/azure_cli_auth.go
@@ -96,7 +96,7 @@ func (aa *AzureAuth) cliAuthorizer(resource string) (autorest.Authorizer, error)
 }
 
 func (aa *AzureAuth) configureWithAzureCLI() (func(r *http.Request) error, error) {
-	if aa.resourceID() == "" {
+	if aa.databricksClient != nil && !aa.databricksClient.IsAzure() {
 		return nil, nil
 	}
 	if aa.IsClientSecretSet() {

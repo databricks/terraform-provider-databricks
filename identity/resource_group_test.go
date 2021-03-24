11550d2f0c2f5bfebd8004238bd3d6c81a22da43
Sriharsha Tikkireddy <sri.tikkireddy@databricks.com>
2021-03-24 19:47:25 -0400

    fixed up the update tests for appropraite coverage

--- identity/resource_group_test.go
+++ identity/resource_group_test.go
@@ -3,6 +3,8 @@ package identity
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/databrickslabs/terraform-provider-databricks/common"
 
 	"github.com/databrickslabs/terraform-provider-databricks/qa"
@@ -131,13 +133,13 @@ func TestResourceGroupRead_Error(t *testing.T) {
 	assert.Equal(t, "abc", d.Id(), "Id should not be empty for error reads")
 }
 
-func TestResourceGroupUpdate(t *testing.T) {
+func TestResourceGroupUpdate_AddPerms(t *testing.T) {
 	d, err := qa.ResourceFixture{
 		Fixtures: []qa.HTTPFixture{
 			{
 				Method:   "PATCH",
 				Resource: "/api/2.0/preview/scim/v2/Groups/abc",
-				Response: GroupPatchRequest{
+				ExpectedRequest: GroupPatchRequest{
 					Schemas: []URN{"urn:ietf:params:scim:api:messages:2.0:PatchOp"},
 					Operations: []GroupPatchOperations{
 						{
@@ -146,6 +148,12 @@ func TestResourceGroupUpdate(t *testing.T) {
 							Value: []ValueListItem{
 								{
 									Value: "allow-cluster-create",
+								},
+								{
+									Value: "sql-analytics-access",
+								},
+								{
+									Value: "allow-instance-pool-create",
 								},
 							},
 						},
@@ -159,19 +167,101 @@ func TestResourceGroupUpdate(t *testing.T) {
 					Schemas:     []URN{"urn:ietf:params:scim:schemas:core:2.0:Group"},
 					DisplayName: "Data Ninjas",
 					ID:          "abc",
+					Entitlements: []entitlementsListItem{
+						{
+							AllowSQLAnalyticsAccessEntitlement,
+						},
+						{
+							AllowClusterCreateEntitlement,
+						},
+						{
+							AllowInstancePoolCreateEntitlement,
+						},
+					},
 				},
 			},
 		},
 		Resource: ResourceGroup(),
-		State: map[string]interface{}{
+		InstanceState: map[string]string{
 			"display_name":               "Data Ninjas",
-			"allow_instance_pool_create": true,
+			"allow_instance_pool_create": "false",
+			"allow_cluster_create":       "false",
+			"allow_sql_analytics_access": "false",
 		},
+		HCL: `
+		display_name = "Data Ninjas"
+		allow_instance_pool_create = true
+		allow_cluster_create = true
+		allow_sql_analytics_access = true
+		`,
 		Update: true,
 		ID:     "abc",
 	}.Apply(t)
 	assert.NoError(t, err, err)
 	assert.Equal(t, "abc", d.Id(), "Id should be the same as in reading")
+	assert.Equal(t, "Data Ninjas", d.Get("display_name"))
+	assert.Equal(t, true, d.Get("allow_cluster_create"))
+	assert.Equal(t, true, d.Get("allow_instance_pool_create"))
+	assert.Equal(t, true, d.Get("allow_sql_analytics_access"))
+}
+
+func TestResourceGroupUpdate_RemovePerms(t *testing.T) {
+	d, err := qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "PATCH",
+				Resource: "/api/2.0/preview/scim/v2/Groups/abc",
+				ExpectedRequest: GroupPatchRequest{
+					Schemas: []URN{"urn:ietf:params:scim:api:messages:2.0:PatchOp"},
+					Operations: []GroupPatchOperations{
+						{
+							Op:   "remove",
+							Path: "entitlements[value eq \"allow-cluster-create\"]",
+						},
+						{
+							Op:   "remove",
+							Path: "entitlements[value eq \"sql-analytics-access\"]",
+						},
+						{
+							Op:   "remove",
+							Path: "entitlements[value eq \"allow-instance-pool-create\"]",
+						},
+					},
+				},
+			},
+			{
+				Method:   "GET",
+				Resource: "/api/2.0/preview/scim/v2/Groups/abc",
+				Response: ScimGroup{
+					Schemas:      []URN{"urn:ietf:params:scim:schemas:core:2.0:Group"},
+					DisplayName:  "Data Ninjas",
+					ID:           "abc",
+					Entitlements: []entitlementsListItem{},
+				},
+			},
+		},
+		Resource: ResourceGroup(),
+		Update:   true,
+		ID:       "abc",
+		InstanceState: map[string]string{
+			"display_name":               "Data Ninjas",
+			"allow_instance_pool_create": "true",
+			"allow_cluster_create":       "true",
+			"allow_sql_analytics_access": "true",
+		},
+		HCL: `
+		display_name = "Data Ninjas"
+		allow_instance_pool_create = false
+		allow_cluster_create = false
+		allow_sql_analytics_access = false
+		`,
+	}.Apply(t)
+	require.NoError(t, err, err)
+	assert.Equal(t, "abc", d.Id(), "Id should not be empty")
+	assert.Equal(t, "Data Ninjas", d.Get("display_name"))
+	assert.Equal(t, false, d.Get("allow_cluster_create"))
+	assert.Equal(t, false, d.Get("allow_instance_pool_create"))
+	assert.Equal(t, false, d.Get("allow_sql_analytics_access"))
 }
 
 func TestResourceGroupUpdate_Error(t *testing.T) {

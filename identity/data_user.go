45e9a92bda1fa4dfecf351e2f978c0a884e35597
Pedro Sereno <pedro.galvao@wildlifestudios.com>
2021-05-13 15:43:56 -0300

    move data source user to schema map

--- identity/data_user.go
+++ identity/data_user.go
@@ -5,42 +5,35 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/databrickslabs/terraform-provider-databricks/common"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
 
 // DataSourceUser returns information about user specified by user name
 func DataSourceUser() *schema.Resource {
-	type entity struct {
-		UserName     string `json:"user_name"`
-		Home         string `json:"home" tf:"computed"`
-		Alphanumeric string `json:"alphanumeric" tf:"computed"`
-	}
-
-	s := common.StructToSchema(entity{}, func(
-		s map[string]*schema.Schema) map[string]*schema.Schema {
-		// nolint once SDKv2 has Diagnostics-returning validators, change
-		s["user_name"].ValidateFunc = validation.StringIsNotEmpty
-		return s
-	})
-
 	return &schema.Resource{
-		Schema: s,
+		Schema: map[string]*schema.Schema{
+			"user_name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"home": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"alphanumeric": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
 		ReadContext: func(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-			var this entity
-			err := common.DataToStructPointer(d, s, &this)
-			if err != nil {
-				return diag.FromErr(err)
-			}
 			usersAPI := NewUsersAPI(ctx, m)
-			userList, err := usersAPI.Filter(fmt.Sprintf("userName eq '%s'", this.UserName))
+			userList, err := usersAPI.Filter(fmt.Sprintf("userName eq '%s'", d.Get("user_name")))
 			if err != nil {
 				return diag.FromErr(err)
 			}
 			if len(userList) == 0 {
-				return diag.FromErr(fmt.Errorf("cannot find user %s", this.UserName))
+				return diag.FromErr(fmt.Errorf("cannot find user %s", d.Get("user_name")))
 			}
 			d.Set("user_name", userList[0].UserName)
 			d.Set("home", fmt.Sprintf("/Users/%s", userList[0].UserName))

f17e41fd5a796f420e90ff93ca9c69f8685d2825
Serge Smertin <serge.smertin@databricks.com>
2021-07-07 11:12:38 +0200

    test corner cases

--- identity/data_user_test.go
+++ identity/data_user_test.go
@@ -1,8 +1,10 @@
 package identity
 
 import (
+	"context"
 	"testing"
 
+	"github.com/databrickslabs/terraform-provider-databricks/common"
 	"github.com/databrickslabs/terraform-provider-databricks/qa"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -37,4 +39,40 @@ func TestDataSourceUser(t *testing.T) {
 	assert.Equal(t, d.Get("user_name"), "mr.test@example.com")
 	assert.Equal(t, d.Get("home"), "/Users/mr.test@example.com")
 	assert.Equal(t, d.Get("alphanumeric"), "mr_test")
+}
+
+func TestDataSourceUserGerUser(t *testing.T) {
+	qa.HTTPFixturesApply(t, []qa.HTTPFixture{
+		{
+			Method:   "GET",
+			Resource: "/api/2.0/preview/scim/v2/Users/a",
+			Response: ScimUser{
+				ID: "a",
+			},
+		},
+		{
+			Method:   "GET",
+			Resource: "/api/2.0/preview/scim/v2/Users?filter=userName%20eq%20%27searching_error%27",
+			Status: 404,
+			Response: common.APIError {
+				Message: "searching_error",
+			},
+		},
+		{
+			Method:   "GET",
+			Resource: "/api/2.0/preview/scim/v2/Users?filter=userName%20eq%20%27empty_search%27",
+			Response: UserList {},
+		},
+	}, func(ctx context.Context, client *common.DatabricksClient) {
+		usersAPI := NewUsersAPI(ctx, client)
+		user, err := getUser(usersAPI, "a", "")
+		assert.NoError(t, err)
+		assert.Equal(t, "a", user.ID)
+
+		_, err = getUser(usersAPI, "", "searching_error")
+		assert.EqualError(t, err, "searching_error")
+
+		_, err = getUser(usersAPI, "", "empty_search")
+		assert.EqualError(t, err, "cannot find user empty_search")
+	})
 }

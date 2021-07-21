76be3c18d55dc9e9ec0c36273f1b3c1bb44dd235
Serge Smertin <serge.smertin@databricks.com>
2021-07-21 15:58:14 +0200

    Added obo token resource unit and acceptance tests

--- identity/resource_obo_token_test.go
+++ identity/resource_obo_token_test.go
@@ -59,3 +59,121 @@ func TestAwsAccOboFlow(t *testing.T) {
 	require.NoError(t, err)
 	assert.Equal(t, r.TokenInfo.TokenID, oboToken.TokenInfo.TokenID)
 }
+
+func TestResourceOboTokenRead(t *testing.T) {
+	d, err := qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "GET",
+				Resource: "/api/2.0/token-management/tokens/abc",
+
+				Response: TokenResponse{
+					TokenInfo: &TokenInfo{
+						Comment: "Hello, world!",
+					},
+				},
+			},
+		},
+		Resource: ResourceOboToken(),
+		Read:     true,
+		New:      true,
+		ID:       "abc",
+	}.Apply(t)
+	assert.NoError(t, err, err)
+	assert.Equal(t, "abc", d.Id(), "Id should not be empty")
+	assert.Equal(t, "Hello, world!", d.Get("comment"))
+}
+
+func TestResourceOboTokenRead_Error(t *testing.T) {
+	qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "GET",
+				Resource: "/api/2.0/token-management/tokens/abc",
+				Status:   500,
+				Response: common.APIError{
+					Message: "nope",
+				},
+			},
+		},
+		Resource: ResourceOboToken(),
+		Read:     true,
+		New:      true,
+		ID:       "abc",
+	}.ExpectError(t, "nope")
+}
+
+func TestResourceOboTokenCreate_Error(t *testing.T) {
+	qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "POST",
+				Resource: "/api/2.0/token-management/on-behalf-of/tokens",
+				Status:   500,
+				Response: common.APIError{
+					Message: "nope",
+				},
+			},
+		},
+		Resource: ResourceOboToken(),
+		Create:   true,
+		New:      true,
+	}.ExpectError(t, "nope")
+}
+
+func TestResourceOboTokenCreate(t *testing.T) {
+	d, err := qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "POST",
+				Resource: "/api/2.0/token-management/on-behalf-of/tokens",
+				ExpectedRequest: OboToken{
+					ApplicationID:   "abc",
+					LifetimeSeconds: 60,
+					Comment:         "e",
+				},
+				Response: TokenResponse{
+					TokenValue: "s#Cr3t!11",
+					TokenInfo: &TokenInfo{
+						TokenID: "bcd",
+					},
+				},
+			},
+			{
+				Method:   "GET",
+				Resource: "/api/2.0/token-management/tokens/bcd",
+				Response: TokenResponse{
+					TokenInfo: &TokenInfo{
+						Comment: "Hello, world!",
+					},
+				},
+			},
+		},
+		Resource: ResourceOboToken(),
+		Create:   true,
+		HCL: `
+		application_id = "abc"
+		comment = "e"
+		lifetime_seconds = 60
+		`,
+		New: true,
+	}.Apply(t)
+	assert.NoError(t, err, err)
+	assert.Equal(t, "bcd", d.Id(), "Id should not be empty")
+	assert.Equal(t, "Hello, world!", d.Get("comment"))
+}
+
+func TestResourceOboTokenDelete(t *testing.T) {
+	qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "DELETE",
+				Resource: "/api/2.0/token-management/tokens/abc",
+			},
+		},
+		Resource: ResourceOboToken(),
+		Delete:   true,
+		New:      true,
+		ID:       "abc",
+	}.ApplyNoError(t)
+}

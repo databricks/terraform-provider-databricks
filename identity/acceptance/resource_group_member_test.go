9b3c1777884952c48d768053d1353126b86f2fa0
Serge Smertin <serge.smertin@databricks.com>
2021-08-02 16:00:15 +0200

    Make azsp tests more stable

--- identity/acceptance/resource_group_member_test.go
+++ identity/acceptance/resource_group_member_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAccComplexValueResource(t *testing.T) {
+func TestAccGroupMemberResource(t *testing.T) {
 	acceptance.Test(t, []acceptance.Step{
 		{
 			Template: `

32d271a364f9db70eee5e4a2dbacafb464140599
Serge Smertin <259697+nfx@users.noreply.github.com>
2021-11-22 18:06:07 +0100

    Added testing for importables (#932)

---
+++ exporter/importables_test.go
@@ -0,0 +1,157 @@
+package exporter
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/databrickslabs/terraform-provider-databricks/access"
+	"github.com/databrickslabs/terraform-provider-databricks/common"
+	"github.com/databrickslabs/terraform-provider-databricks/identity"
+	"github.com/databrickslabs/terraform-provider-databricks/policies"
+	"github.com/databrickslabs/terraform-provider-databricks/pools"
+	"github.com/databrickslabs/terraform-provider-databricks/provider"
+	"github.com/stretchr/testify/assert"
+)
+
+func importContextForTest() *importContext {
+	p := provider.DatabricksProvider()
+	return &importContext{
+		Importables: resourcesMap,
+		Resources:   p.ResourcesMap,
+		testEmits:   map[string]bool{},
+	}
+}
+
+func TestInstancePool(t *testing.T) {
+	d := pools.ResourceInstancePool().TestResourceData()
+	d.Set("instance_pool_name", "blah-bah")
+	name := resourcesMap["databricks_instance_pool"].Name(d)
+	assert.Equal(t, "blah-bah", name)
+
+	d = pools.ResourceInstancePool().TestResourceData()
+	d.SetId("abc-bcd-def")
+	name = resourcesMap["databricks_instance_pool"].Name(d)
+	assert.Equal(t, "def", name)
+
+	ic := importContextForTest()
+	ic.meAdmin = true
+	err := resourcesMap["databricks_instance_pool"].Import(ic, &resource{
+		ID:   "abc",
+		Data: d,
+	})
+	assert.NoError(t, err)
+	assert.True(t, ic.testEmits["databricks_permissions[inst_pool_def] (id: /instance-pools/abc)"])
+}
+
+func TestClusterPolicy(t *testing.T) {
+	d := policies.ResourceClusterPolicy().TestResourceData()
+	d.Set("name", "bcd")
+	definition := map[string]map[string]string{
+		"aws_attributes.instance_profile_arn": {
+			"value": "def",
+		},
+		"instance_pool_id": {
+			"defaultValue": "efg",
+		},
+	}
+	policy, _ := json.Marshal(definition)
+	d.Set("definition", string(policy))
+	ic := importContextForTest()
+	err := ic.Importables["databricks_cluster_policy"].Import(ic, &resource{
+		ID:   "abc",
+		Data: d,
+	})
+	assert.NoError(t, err)
+	assert.Len(t, ic.testEmits, 3)
+	assert.True(t, ic.testEmits["databricks_permissions[clust_policy_bcd] (id: /cluster-policies/abc)"])
+	assert.True(t, ic.testEmits["databricks_instance_pool[<unknown>] (id: efg)"])
+	assert.True(t, ic.testEmits["databricks_instance_profile[<unknown>] (id: def)"])
+}
+
+func TestGroup(t *testing.T) {
+	ic := importContextForTest()
+	ic.allGroups = []identity.ScimGroup{
+		{
+			DisplayName: "foo",
+			ID:          "123",
+			Roles: []identity.ComplexValue{
+				{
+					Value: "abc",
+				},
+			},
+			Members: []identity.ComplexValue{
+				// this is just for log line printing
+				{Value: "a001"},
+				{Value: "a002"},
+				{Value: "a003"},
+				{Value: "a004"},
+				{Value: "a005"},
+				{Value: "a006"},
+				{Value: "a007"},
+				{Value: "a008"},
+				{Value: "a009"},
+				{Value: "a010"},
+				{Value: "a011"},
+			},
+			Groups: []identity.ComplexValue{
+				{
+					Value: "parent-group",
+					Type:  "direct",
+				},
+			},
+		},
+	}
+	r := &resource{
+		Value:     "foo",
+		Attribute: "display_name",
+	}
+	err := ic.Importables["databricks_group"].Search(ic, r)
+	assert.NoError(t, err)
+	assert.Equal(t, "123", r.ID)
+
+	err = ic.Importables["databricks_group"].Import(ic, r)
+	assert.NoError(t, err)
+	assert.Len(t, ic.testEmits, 4)
+	assert.True(t, ic.testEmits["databricks_group_instance_profile[<unknown>] (id: 123|abc)"])
+	assert.True(t, ic.testEmits["databricks_instance_profile[<unknown>] (id: abc)"])
+	assert.True(t, ic.testEmits["databricks_group[<unknown>] (id: parent-group)"])
+	assert.True(t, ic.testEmits["databricks_group_member[_foo] (id: parent-group|123)"])
+}
+
+func TestPermissions(t *testing.T) {
+	p := access.ResourcePermissions()
+	d := p.TestResourceData()
+	d.SetId("abc")
+	ic := importContextForTest()
+	name := ic.Importables["databricks_permissions"].Name(d)
+	assert.Equal(t, "abc", name)
+
+	d.MarkNewResource()
+	err := common.StructToData(access.PermissionsEntity{
+		AccessControlList: []access.AccessControlChange{
+			{
+				UserName: "a",
+			},
+			{
+				GroupName: "b",
+			},
+		},
+	}, p.Schema, d)
+	assert.NoError(t, err)
+
+	err = ic.Importables["databricks_permissions"].Import(ic, &resource{
+		Data: d,
+	})
+	assert.NoError(t, err)
+	assert.Len(t, ic.testEmits, 2)
+	assert.True(t, ic.testEmits["databricks_user[<unknown>] (user_name: a)"])
+	assert.True(t, ic.testEmits["databricks_group[<unknown>] (display_name: b)"])
+}
+
+func TestSecretScope(t *testing.T) {
+	d := access.ResourceSecretScope().TestResourceData()
+	d.Set("name", "abc")
+	ic := importContextForTest()
+	name := ic.Importables["databricks_secret_scope"].Name(d)
+	assert.Equal(t, "abc", name)
+}

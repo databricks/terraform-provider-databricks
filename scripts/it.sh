9a3c90b0143baf6dd574180fbd2f1b86551b57df
Serge Smertin <serge.smertin@databricks.com>
2021-12-02 19:55:07 +0100

    wip

--- scripts/it.sh
+++ scripts/it.sh
@@ -7,19 +7,19 @@ if [[ $@ == *"--debug"* ]]; then
     echo "[*] To see debug logs: tail -f $PWD/tf.log"
 fi
 
+TEST_FILTER=${1:-TestAcc}
+
 TMPDIR=/tmp TF_ACC=1 gotestsum \
     --format short-verbose \
     --raw-command go test -v -json \
-    -test.timeout 35m \
-    -run $1 ../../... 2>&1 | tee out.log
-
-echo "✓ To output of existing tests: less $PWD/out.log"
+    -test.timeout 30m \
+    -run $TEST_FILTER ../... 2>&1 | tee out.log
 
 FAILURES=$(grep "\-\-\- FAIL" out.log | sed 's/--- FAIL: / \* \[ \] /g' | sort)
 PASSES=$(grep PASS out.log | grep Test | sort | sed 's/PASS/ \* \[x\]/')
 
 cat <<-EOF > test-report.log
-$1
+$TEST_FILTER
 ---
 ${FAILURES}
 ${PASSES}

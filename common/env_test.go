38d848446d99fd3c482258f0d5ca5b71fea587f6
Serge Smertin <serge.smertin@databricks.com>
2021-12-07 21:45:25 +0100

    Increased coverage for CommonEnvironmentClient

--- common/env_test.go
+++ common/env_test.go
@@ -2,11 +2,14 @@ package common
 
 import (
 	"context"
+	"log"
 	"os"
+	"sync"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCommonEnvironmentClient(t *testing.T) {
@@ -14,6 +17,8 @@ func TestCommonEnvironmentClient(t *testing.T) {
 	defer CleanupEnvironment()()
 	os.Setenv("DATABRICKS_TOKEN", ".")
 	os.Setenv("DATABRICKS_HOST", ".")
+	os.Setenv("DATABRICKS_DEBUG_HEADERS", "true")
+	os.Setenv("DATABRICKS_DEBUG_TRUNCATE_BYTES", "1024")
 	c := CommonEnvironmentClient()
 	c2 := CommonEnvironmentClient()
 	ctx := context.Background()
@@ -25,4 +30,15 @@ func TestCommonEnvironmentClient(t *testing.T) {
 		TerraformVersion: "0.12",
 	}
 	assert.Equal(t, "databricks-tf-provider/"+version+" (+cluster) terraform/0.12", c.userAgent(ctx))
+
+	defer func() {
+		paniced := recover()
+		log.Printf("[INFO] paniced with: %s", paniced)
+		require.NotNil(t, paniced, "Must have paniced!")
+	}()
+	commonClient = nil
+	onceClient = sync.Once{}
+	os.Setenv("ARM_ENVIRONMENT", "ANY")
+	// and this one will panic
+	CommonEnvironmentClient()
 }

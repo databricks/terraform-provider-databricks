9b088c92ba5b77f68856eee2dd510ec8555c7f51
Serge Smertin <259697+nfx@users.noreply.github.com>
2021-12-24 13:39:42 +0100

    Start v0.4.3 (#1004)

--- common/version.go
+++ common/version.go
@@ -3,7 +3,7 @@ package common
 import "context"
 
 var (
-	version = "0.4.2"
+	version = "0.4.3"
 	// ResourceName is resource name without databricks_ prefix
 	ResourceName contextKey = 1
 	// Provider is the current instance of provider

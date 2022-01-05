0aeb7994f1412489601a730bceeef61ba1d5b640
Serge Smertin <259697+nfx@users.noreply.github.com>
2022-01-05 15:02:41 +0100

    Adding new experimental resources (#1023)
    Added `databricks_table` resource

---
+++ catalog/resource_table.go
@@ -0,0 +1,102 @@
+package catalog
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/databrickslabs/terraform-provider-databricks/common"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+type TablesAPI struct {
+	client  *common.DatabricksClient
+	context context.Context
+}
+
+func NewTablesAPI(ctx context.Context, m interface{}) TablesAPI {
+	return TablesAPI{m.(*common.DatabricksClient), ctx}
+}
+
+type ColumnInfo struct {
+	Name             string `json:"name"`
+	TypeText         string `json:"type_text"`
+	TypeJson         string `json:"type_json,omitempty"`
+	TypeName         string `json:"type_name"`
+	TypePrecision    int32  `json:"type_precision,omitempty"`
+	TypeScale        int32  `json:"type_scale,omitempty"`
+	TypeIntervalType string `json:"type_interval_type,omitempty"`
+	Position         int32  `json:"position"`
+	Comment          string `json:"comment,omitempty"`
+	Nullable         bool   `json:"nullable,omitempty" tf:"default:true"`
+	PartitionIndex   int32  `json:"partition_index,omitempty"`
+}
+
+type TableInfo struct {
+	Name                  string            `json:"name" tf:"force_new"`
+	CatalogName           string            `json:"catalog_name"`
+	SchemaName            string            `json:"schema_name"`
+	TableType             string            `json:"table_type"`
+	DataSourceFormat      string            `json:"data_source_format"`
+	ColumnInfos           []ColumnInfo      `json:"columns" tf:"alias:column"`
+	StorageLocation       string            `json:"storage_location"`
+	StorageCredentialName string            `json:"storage_credential_name,omitempty"`
+	ViewDefinition        string            `json:"view_definition,omitempty"`
+	Owner                 string            `json:"owner,omitempty" tf:"computed"`
+	Comment               string            `json:"comment,omitempty"`
+	Properties            map[string]string `json:"properties,omitempty"`
+}
+
+func (ti TableInfo) FullName() string {
+	return fmt.Sprintf("%s.%s.%s", ti.CatalogName, ti.SchemaName, ti.Name)
+}
+
+func (a TablesAPI) createTable(ti *TableInfo) error {
+	return a.client.Post(a.context, "/unity-catalog/tables", ti, ti)
+}
+
+func (a TablesAPI) getTable(name string) (ti TableInfo, err error) {
+	err = a.client.Get(a.context, "/unity-catalog/tables/"+name, nil, &ti)
+	return
+}
+
+func (a TablesAPI) updateTable(ti TableInfo) error {
+	return a.client.Patch(a.context, "/unity-catalog/tables/"+ti.Name, ti)
+}
+
+func (a TablesAPI) deleteTable(name string) error {
+	return a.client.Delete(a.context, "/unity-catalog/tables/"+name, nil)
+}
+
+func ResourceTable() *schema.Resource {
+	tableSchema := common.StructToSchema(TableInfo{},
+		func(m map[string]*schema.Schema) map[string]*schema.Schema {
+			return m
+		})
+	return common.Resource{
+		Schema: tableSchema,
+		Create: func(ctx context.Context, d *schema.ResourceData, c *common.DatabricksClient) error {
+			var ti TableInfo
+			common.DataToStructPointer(d, tableSchema, &ti)
+			if err := NewTablesAPI(ctx, c).createTable(&ti); err != nil {
+				return err
+			}
+			d.SetId(ti.FullName())
+			return nil
+		},
+		Read: func(ctx context.Context, d *schema.ResourceData, c *common.DatabricksClient) error {
+			ti, err := NewTablesAPI(ctx, c).getTable(d.Id())
+			if err != nil {
+				return err
+			}
+			return common.StructToData(ti, tableSchema, d)
+		},
+		Update: func(ctx context.Context, d *schema.ResourceData, c *common.DatabricksClient) error {
+			var ti TableInfo
+			common.DataToStructPointer(d, tableSchema, &ti)
+			return NewTablesAPI(ctx, c).updateTable(ti)
+		},
+		Delete: func(ctx context.Context, d *schema.ResourceData, c *common.DatabricksClient) error {
+			return NewTablesAPI(ctx, c).deleteTable(d.Id())
+		},
+	}.ToResource()
+}

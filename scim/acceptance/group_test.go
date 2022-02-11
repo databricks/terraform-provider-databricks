cf909d369462ab30fd319047e57b56b7977bdfb5
Serge Smertin <259697+nfx@users.noreply.github.com>
2022-02-11 12:19:10 +0100

    Suppress diff for `databricks_group`.`external_id` (#1100)
    Fixes #1099

--- scim/acceptance/group_test.go
+++ scim/acceptance/group_test.go
@@ -1,16 +1,55 @@
 package acceptance
 
 import (
+	"context"
 	"fmt"
 	"os"
 
+	"github.com/databrickslabs/terraform-provider-databricks/common"
 	"github.com/databrickslabs/terraform-provider-databricks/internal/acceptance"
+	"github.com/databrickslabs/terraform-provider-databricks/qa"
+	"github.com/databrickslabs/terraform-provider-databricks/scim"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 
 	"testing"
 )
+
+// https://github.com/databrickslabs/terraform-provider-databricks/issues/1099
+func TestAccGroupsExternalIdAndScimProvisioning(t *testing.T) {
+	if _, ok := os.LookupEnv("CLOUD_ENV"); !ok {
+		t.Skip("Acceptance tests skipped unless env 'CLOUD_ENV' is set")
+	}
+	name := qa.RandomName("tfgroup")
+	acceptance.AccTest(t, resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				Config: `resource "databricks_group" "this" {
+					display_name = "` + name + `"
+					allow_cluster_create = true
+				}`,
+				Check: acceptance.ResourceCheck("databricks_group.this",
+					func(ctx context.Context, client *common.DatabricksClient, id string) error {
+						groupsAPI := scim.NewGroupsAPI(ctx, client)
+						group, err := groupsAPI.Read(id)
+						if err != nil {
+							return err
+						}
+						// external SCIM change
+						return groupsAPI.UpdateNameAndEntitlements(
+							id, group.DisplayName, qa.RandomName("ext-id"), group.Entitlements)
+					}),
+			},
+			{
+				Config: `resource "databricks_group" "this" {
+					display_name = "` + name + `"
+					allow_cluster_create = true
+				}`,
+			},
+		},
+	})
+}
 
 func TestAccGroupResource(t *testing.T) {
 	if _, ok := os.LookupEnv("CLOUD_ENV"); !ok {

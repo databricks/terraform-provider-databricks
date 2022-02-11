aeb29331e81222f862388471c9917f0d6bc26161
Serge Smertin <serge.smertin@databricks.com>
2022-02-11 17:01:23 +0100

    increment version

--- scripts/gcp-integration/main.tf
+++ scripts/gcp-integration/main.tf
@@ -77,7 +77,7 @@ output "databricks_host" {
   value = databricks_mws_workspaces.this.workspace_url
 }
 
-output "databricks_token" {
+output "token" {
   value     = databricks_mws_workspaces.this.token[0].token_value
   sensitive = true
 }

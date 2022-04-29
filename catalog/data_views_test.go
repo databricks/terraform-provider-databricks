76121146cbad49b02d541745fe2bcf19594cd75e
Serge Smertin <259697+nfx@users.noreply.github.com>
2022-04-29 13:49:58 +0200

    Added `databricks_views` data resource (#1283)
    * Added `databricks_views` data resource for listing of views in Unity Catalog managed schema
    * Made `databricks_tables` return only managed or external tables in Unity Catalog.
    
    Fix #1274

---
+++ catalog/data_views_test.go
@@ -0,0 +1,53 @@
+package catalog
+
+import (
+	"testing"
+
+	"github.com/databrickslabs/terraform-provider-databricks/qa"
+)
+
+func TestViewsData(t *testing.T) {
+	qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "GET",
+				Resource: "/api/2.0/unity-catalog/tables/?catalog_name=a&schema_name=b",
+				Response: Tables{
+					Tables: []TableInfo{
+						{
+							CatalogName: "a",
+							SchemaName: "b",
+							Name: "c",
+							TableType: "MANAGED",
+						},
+						{
+							CatalogName: "a",
+							SchemaName: "b",
+							Name: "d",
+							TableType: "VIEW",
+						},
+					},
+				},
+			},
+		},
+		Resource: DataSourceViews(),
+		HCL: `
+		catalog_name = "a"
+		schema_name = "b"`,
+		Read:        true,
+		NonWritable: true,
+		ID:          "_",
+	}.ApplyAndExpectData(t, map[string]interface{}{
+		"ids": []string{"a.b.d"},
+	})
+}
+
+func TestViewsData_Error(t *testing.T) {
+	qa.ResourceFixture{
+		Fixtures:    qa.HTTPFailures,
+		Resource:    DataSourceViews(),
+		Read:        true,
+		NonWritable: true,
+		ID:          "_",
+	}.ExpectError(t, "I'm a teapot")
+}

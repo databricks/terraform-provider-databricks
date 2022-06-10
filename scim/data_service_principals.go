d702eedb295a790c63de9c6c69a50793400f6136
vuong-nguyen <44292934+nkvuong@users.noreply.github.com>
2022-06-10 17:24:46 +0700

    Added `databricks_service_principal` and `databricks_service_principals` data resources (#1370)
    * return `application_id` from `databricks_current_user` if that user is a Service Principal
    * add `databricks_service_principal` data source based on `application_id`
    * add `databricks_service_principals` data source based on `display_name`
    
    Co-authored-by: Ron DeFreitas <drax3d@gmail.com>

---
+++ scim/data_service_principals.go
@@ -0,0 +1,35 @@
+package scim
+
+import (
+	"context"
+	"fmt"
+	"sort"
+
+	"github.com/databrickslabs/terraform-provider-databricks/common"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+// DataSourceServicePrincipals searches for service principals based on display_name
+func DataSourceServicePrincipals() *schema.Resource {
+	type spnsData struct {
+		DisplayNameContains string   `json:"display_name_contains,omitempty" tf:"computed"`
+		ApplicationIDs      []string `json:"application_ids,omitempty" tf:"computed,slice_set"`
+	}
+	return common.DataResource(spnsData{}, func(ctx context.Context, e interface{}, c *common.DatabricksClient) error {
+		response := e.(*spnsData)
+		spnAPI := NewServicePrincipalsAPI(ctx, c)
+
+		spList, err := spnAPI.filter(fmt.Sprintf("displayName co '%s'", response.DisplayNameContains))
+		if err != nil {
+			return err
+		}
+		if len(spList) == 0 {
+			return fmt.Errorf("cannot find SPs with display name containing %s", response.DisplayNameContains)
+		}
+		for _, sp := range spList {
+			response.ApplicationIDs = append(response.ApplicationIDs, sp.ApplicationID)
+		}
+		sort.Strings(response.ApplicationIDs)
+		return nil
+	})
+}

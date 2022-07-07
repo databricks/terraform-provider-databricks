699716a2073f3e5d86cf5adb8e5fa027f807583f
vuong-nguyen <44292934+nkvuong@users.noreply.github.com>
2022-07-07 04:35:42 -0700

    Fix update for `databricks_storage_credential` (#1403)
    * flatten the array for storage credentials
    * add test for updating azmi

--- catalog/update.go
+++ catalog/update.go
@@ -33,6 +33,16 @@ func updateFunctionFactory(pathPrefix string, updatable []string) func(context.C
 			if !d.HasChange(field) {
 				continue
 			}
+
+			if contains([]string{
+				"aws_iam_role",
+				"azure_service_principal",
+				"azure_managed_identity",
+			}, field) {
+				patch[field] = d.Get(field).([]interface{})[0]
+				continue
+			}
+
 			if field == "delta_sharing_scope" && old != new && new == "INTERNAL_AND_EXTERNAL" &&
 				!d.HasChange("delta_sharing_recipient_token_lifetime_in_seconds") {
 				patch["delta_sharing_recipient_token_lifetime_in_seconds"] =

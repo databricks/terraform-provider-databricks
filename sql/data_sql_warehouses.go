2454acb3eadbdef2f602aec7580758d7e84e7c9a
vuong-nguyen <44292934+nkvuong@users.noreply.github.com>
2022-07-20 01:56:28 -0700

    Added `databricks_sql_warehouses`, `databricks_sql_warehouse`, and `databricks_cluster` data resources (#1460)
    Follow-ups: #1480 and #1481

---
+++ sql/data_sql_warehouses.go
@@ -0,0 +1,36 @@
+package sql
+
+import (
+	"context"
+	"sort"
+	"strings"
+
+	"github.com/databricks/terraform-provider-databricks/common"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func DataSourceWarehouses() *schema.Resource {
+	type warehousesData struct {
+		WarehouseNameContains string   `json:"warehouse_name_contains,omitempty"`
+		Ids                   []string `json:"ids,omitempty" tf:"computed,slice_set"`
+	}
+	return common.DataResource(warehousesData{}, func(ctx context.Context, e interface{}, c *common.DatabricksClient) error {
+		data := e.(*warehousesData)
+		a := NewSQLEndpointsAPI(ctx, c)
+		list, err := a.List()
+		if err != nil {
+			return err
+		}
+		name_contains := (*data).WarehouseNameContains
+		for _, e := range list.Endpoints {
+			match_name := strings.Contains(strings.ToLower(e.Name), name_contains)
+			if name_contains != "" && !match_name {
+				continue
+			}
+			data.Ids = append(data.Ids, e.ID)
+		}
+
+		sort.Strings(data.Ids)
+		return nil
+	})
+}

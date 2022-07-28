9298ffd99fba270bbdb2587ef47903e4b1eb4c92
Serge Smertin <serge.smertin@databricks.com>
2022-07-28 21:33:46 +0200

    add tests

---
+++ mws/acceptance/mws_permissionassignments_test.go
@@ -0,0 +1,64 @@
+package acceptance
+
+import (
+	"testing"
+
+	"github.com/databricks/terraform-provider-databricks/internal/acceptance"
+	"github.com/databricks/terraform-provider-databricks/qa"
+)
+
+func TestAccAssignGroupToWorkspace(t *testing.T) {
+	qa.RequireCloudEnv(t, "unity-catalog-account")
+	acceptance.Test(t, []acceptance.Step{
+		{
+			Template: `
+			resource "databricks_group" "this" {
+				display_name = "TF {var.RANDOM}"
+			}
+			resource "databricks_mws_permissionassignment" "this" {
+				workspace_id = {env.TEST_UC_WORKSPACE_ID}
+				principal_id = databricks_group.this.id
+				permissions  = ["USER"]
+			}`,
+		},
+		{
+			Template: `
+			resource "databricks_group" "this" {
+				display_name = "TF {var.RANDOM}"
+			}
+			resource "databricks_mws_permissionassignment" "this" {
+				workspace_id = {env.TEST_UC_WORKSPACE_ID}
+				principal_id = databricks_group.this.id
+				permissions  = ["ADMIN"]
+			}`,
+		},
+		{
+			Template: `
+			resource "databricks_group" "this" {
+				display_name = "TF {var.RANDOM}"
+			}
+			resource "databricks_mws_permissionassignment" "this" {
+				workspace_id = {env.TEST_UC_WORKSPACE_ID}
+				principal_id = databricks_group.this.id
+				permissions  = ["USER"]
+			}`,
+		},
+	})
+}
+
+func TestAccAssignSpnToWorkspace(t *testing.T) {
+	qa.RequireCloudEnv(t, "unity-catalog-account")
+	acceptance.Test(t, []acceptance.Step{
+		{
+			Template: `
+			resource "databricks_service_principal" "this" {
+				display_name = "TF {var.RANDOM}"
+			}
+			resource "databricks_mws_permissionassignment" "this" {
+				workspace_id = {env.TEST_UC_WORKSPACE_ID}
+				principal_id = databricks_service_principal.this.id
+				permissions  = ["USER"]
+			}`,
+		},
+	})
+}

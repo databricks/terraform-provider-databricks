b890cc3dd378cf9c45b616289669f2c6745c7770
Edmondo Porcu <edmondo.porcu@gmail.com>
2022-08-09 19:39:20 -0700

    Fixing serialization/deserialization of job id

--- jobs/data_queryable_job.go
+++ jobs/data_queryable_job.go
@@ -9,7 +9,7 @@ import (
 func DataSourceQueryableJob() *schema.Resource {
 	type queryableJobData struct {
 		Ids  []string `json:"ids,omitempty" tf:"computed,slice_set"`
-		Id   string   `json:"id,omitempty"`
+		Id   string   `json:"job_id,omitempty"`
 		Name string   `json:"job_name,omitempty"`
 	}
 	return common.DataResource(queryableJobData{}, func(ctx context.Context, e any, c *common.DatabricksClient) error {

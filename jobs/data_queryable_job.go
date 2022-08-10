cf6d39d2d266869e2a76a98d58a77956f6e161d0
Edmondo Porcu <edmondo.porcu@gmail.com>
2022-08-09 19:27:24 -0700

    Implementing suggestions from the PR comments

--- jobs/data_queryable_job.go
+++ jobs/data_queryable_job.go
@@ -2,20 +2,30 @@ package jobs
 
 import (
 	"context"
-
 	"github.com/databricks/terraform-provider-databricks/common"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func DataSourceQueryableJob() *schema.Resource {
 	type queryableJobData struct {
-		// data resources output fields annotated with tf:"computed"
-		Ids []string `json:"ids,omitempty" tf:"computed,slice_set"`
+		Ids  []string `json:"ids,omitempty" tf:"computed,slice_set"`
+		Id   string   `json:"id,omitempty"`
+		Name string   `json:"job_name,omitempty"`
 	}
 	return common.DataResource(queryableJobData{}, func(ctx context.Context, e any, c *common.DatabricksClient) error {
 		data := e.(*queryableJobData)
-		// TODO: implement me
-		data.Ids = append(data.Ids, "..") // replace
+		jobsAPI := NewJobsAPI(ctx, c)
+		list, err := jobsAPI.List()
+		if err != nil {
+			return err
+		}
+		for _, currentJob := range list.Jobs {
+			name := currentJob.Settings.Name
+			id := currentJob.ID()
+			if id == data.Id || name == data.Name {
+				data.Ids = append(data.Ids, currentJob.ID())
+			}
+		}
 		return nil
 	})
 }
--- jobs/data_queryable_job_test.go
+++ jobs/data_queryable_job_test.go
@@ -1,29 +1,85 @@
 package jobs
 
 import (
-	"testing"
-
 	"github.com/databricks/terraform-provider-databricks/qa"
+	"testing"
 )
 
-func TestDataSourceQueryableJob(t *testing.T) {
-	qa.ResourceFixture{
-		Fixtures: []qa.HTTPFixture{
-			// TODO: run this test to get fixtures
+var fixtures = []qa.HTTPFixture{
+	{
+		Method:   "GET",
+		Resource: "/api/2.1/jobs/list",
+		Response: JobList{
+			Jobs: []Job{
+				{
+					JobID: 123,
+					Settings: &JobSettings{
+						Name: "First",
+					},
+				},
+				{
+					JobID: 234,
+					Settings: &JobSettings{
+						Name: "Second",
+					},
+				},
+			},
 		},
-		Resource:    DataSourceQueryableJob(),
-		Read:        true,
-		NonWritable: true,
-		ID:          "_",
-	}.ApplyNoError(t)
+	},
 }
 
-func TestCatalogsData_Error(t *testing.T) {
+func TestDataSourceQueryableJobMatchesId(t *testing.T) {
 	qa.ResourceFixture{
-		Fixtures:    qa.HTTPFailures,
+		Fixtures:    fixtures,
 		Resource:    DataSourceQueryableJob(),
 		Read:        true,
 		NonWritable: true,
+		HCL:         `job_id = "234"`,
 		ID:          "_",
-	}.ExpectError(t, "I'm a teapot")
+	}.ApplyAndExpectData(t, map[string]any{
+		"ids": []string{
+			"234",
+		},
+	})
+}
+
+func TestDataSourceQueryableJobMatchesName(t *testing.T) {
+	qa.ResourceFixture{
+		Fixtures:    fixtures,
+		Resource:    DataSourceQueryableJob(),
+		Read:        true,
+		NonWritable: true,
+		HCL:         `job_name= "First"`,
+		ID:          "_",
+	}.ApplyAndExpectData(t, map[string]any{
+		"ids": []string{
+			"123",
+		},
+	})
+}
+
+func TestDataSourceQueryableJobNoMatchName(t *testing.T) {
+	qa.ResourceFixture{
+		Fixtures:    fixtures,
+		Resource:    DataSourceQueryableJob(),
+		Read:        true,
+		NonWritable: true,
+		HCL:         `job_name= "Third"`,
+		ID:          "_",
+	}.ApplyAndExpectData(t, map[string]any{
+		"ids": []string{},
+	})
+}
+
+func TestDataSourceQueryableJobNoMatchId(t *testing.T) {
+	qa.ResourceFixture{
+		Fixtures:    fixtures,
+		Resource:    DataSourceQueryableJob(),
+		Read:        true,
+		NonWritable: true,
+		HCL:         `job_id= "567"`,
+		ID:          "_",
+	}.ApplyAndExpectData(t, map[string]any{
+		"ids": []string{},
+	})
 }

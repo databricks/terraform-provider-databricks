aba7ed54ea57df3e00bda2d37fd708af8b3151a3
Shreyas Goenka <shreyas.goenka@databricks.com>
2022-08-22 15:29:53 +0200

    test correction for changes to error message on resize api

--- clusters/clusters_api_test.go
+++ clusters/clusters_api_test.go
@@ -342,7 +342,7 @@ func TestResizeCluster_FailsForNonRunningCluster(t *testing.T) {
 				NumWorkers: 10,
 			})
 			require.Error(t, err)
-			assert.Contains(t, err.Error(), "Resize API can only be called on clusters in RUNNING state. Cluster abc is in "+clusterState+" state")
+			assert.Contains(t, err.Error(), "resize: Cluster abc is in " + clusterState + " state. RUNNING state required to use resize API")
 			server.Close()
 		})
 	}

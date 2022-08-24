b96c0a4f2688e4619410e1ba60c19e2a573e2078
shreyas-goenka <88374338+shreyas-goenka@users.noreply.github.com>
2022-08-24 22:47:02 +0200

    Use resize API to scale running clusters   (#1541)

--- clusters/acceptance/clusters_api_test.go
+++ clusters/acceptance/clusters_api_test.go
@@ -64,3 +64,64 @@ func TestAccListClustersIntegration(t *testing.T) {
 	assert.NoError(t, err, err)
 	assert.True(t, clusterReadInfo.State == clusters.ClusterStateRunning)
 }
+
+func TestAccListClustersResizeIntegrationTest(t *testing.T) {
+	qa.RequireAnyCloudEnv(t)
+	t.Parallel()
+	client := common.CommonEnvironmentClient()
+	ctx := context.Background()
+	clustersAPI := clusters.NewClustersAPI(ctx, client)
+	randomName := qa.RandomName()
+
+	cluster := clusters.Cluster{
+		NumWorkers:  1,
+		ClusterName: "Terraform Integration Test " + randomName,
+		SparkVersion: clustersAPI.LatestSparkVersionOrDefault(
+			clusters.SparkVersionRequest{
+				Latest:          true,
+				LongTermSupport: true,
+			}),
+		InstancePoolID:         compute.CommonInstancePoolID(),
+		IdempotencyToken:       "acc-list-" + randomName,
+		AutoterminationMinutes: 15,
+	}
+
+	clusterReadInfo, err := clustersAPI.Create(cluster)
+	require.NoError(t, err, err)
+	assert.True(t, clusterReadInfo.NumWorkers == cluster.NumWorkers)
+	assert.True(t, clusterReadInfo.ClusterName == cluster.ClusterName)
+	assert.True(t, reflect.DeepEqual(clusterReadInfo.SparkEnvVars, cluster.SparkEnvVars))
+	assert.True(t, clusterReadInfo.SparkVersion == cluster.SparkVersion)
+	assert.True(t, clusterReadInfo.AutoterminationMinutes == cluster.AutoterminationMinutes)
+	assert.True(t, clusterReadInfo.State == clusters.ClusterStateRunning)
+
+	// Resize num workers
+	clusterReadInfo, err = clustersAPI.Resize(clusters.ResizeRequest{ClusterID: clusterReadInfo.ClusterID, NumWorkers: 2})
+	require.NoError(t, err, err)
+	assert.True(t, clusterReadInfo.NumWorkers == 2)
+	assert.True(t, clusterReadInfo.State == clusters.ClusterStateRunning)
+
+	// Resize cluster to become autoscaling
+	clusterReadInfo, err = clustersAPI.Resize(clusters.ResizeRequest{ClusterID: clusterReadInfo.ClusterID, AutoScale: &clusters.AutoScale{
+		MinWorkers: 1,
+		MaxWorkers: 2,
+	}})
+	require.NoError(t, err, err)
+	assert.True(t, clusterReadInfo.AutoScale.MinWorkers == 1)
+	assert.True(t, clusterReadInfo.AutoScale.MaxWorkers == 2)
+	assert.True(t, clusterReadInfo.State == clusters.ClusterStateRunning)
+
+	// cleanup
+	err = clustersAPI.Terminate(clusterReadInfo.ClusterID)
+	assert.NoError(t, err, err)
+
+	clusterReadInfo, err = clustersAPI.Get(clusterReadInfo.ClusterID)
+	assert.NoError(t, err, err)
+	assert.True(t, clusterReadInfo.State == clusters.ClusterStateTerminated)
+
+	err = clustersAPI.Unpin(clusterReadInfo.ClusterID)
+	assert.NoError(t, err, err)
+
+	err = clustersAPI.PermanentDelete(clusterReadInfo.ClusterID)
+	assert.NoError(t, err, err)
+}

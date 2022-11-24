a272129a20ef44348df8eac66c4b2350d974a761
Alex Ott <alexey.ott@databricks.com>
2022-11-24 14:08:26 +0100

    Added `databricks_cluster_policy` data source (#1792)

---
+++ policies/data_cluster_policy_test.go
@@ -0,0 +1,65 @@
+package policies
+
+import (
+	"context"
+	"testing"
+
+	"github.com/databricks/terraform-provider-databricks/common"
+	"github.com/databricks/terraform-provider-databricks/qa"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDataSourceClusterPolicy(t *testing.T) {
+	d, err := qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "GET",
+				Resource: "/api/2.0/policies/clusters/list",
+				Response: ClusterPolicyList{
+					Policies: []ClusterPolicy{
+						{
+							PolicyID: "abc",
+							Name:     "policy",
+						},
+					},
+				},
+			},
+		},
+		Read:        true,
+		NonWritable: true,
+		Resource:    DataSourceClusterPolicy(),
+		ID:          ".",
+		State: map[string]any{
+			"name": "policy",
+		},
+	}.Apply(t)
+	require.NoError(t, err)
+	assert.Equal(t, "abc", d.Id())
+}
+
+func TestDataSourceClusterPolicyNotFound(t *testing.T) {
+	qa.HTTPFixturesApply(t, []qa.HTTPFixture{
+		{
+			Method:   "GET",
+			Resource: "/api/2.0/policies/clusters/list",
+			Status:   404,
+			Response: common.APIError{
+				Message: "searching_error",
+			},
+		},
+		{
+			Method:   "GET",
+			Resource: "/api/2.0/policies/clusters/list",
+			Response: ClusterPolicyList{},
+		},
+	}, func(ctx context.Context, client *common.DatabricksClient) {
+		poolsAPI := NewClusterPoliciesAPI(ctx, client)
+
+		_, err := getPolicy(poolsAPI, "searching_error")
+		assert.EqualError(t, err, "searching_error")
+
+		_, err = getPolicy(poolsAPI, "unknown")
+		assert.EqualError(t, err, "cluster policy 'unknown' wasn't found")
+	})
+}

de8c11c9b8694637ef251e1431af1d0e4123daca
Alex Ott <alexott@gmail.com>
2023-01-04 09:52:14 +0100

    add search by name for `databricks_cluster` data source
    Also expose `id` for `databricks_cluster` & `databricks_job` data sources so it's easier
    to use results.
    
    This is a follow up for #1885

--- clusters/data_cluster.go
+++ clusters/data_cluster.go
@@ -2,6 +2,7 @@ package clusters
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/databricks/terraform-provider-databricks/common"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -9,18 +10,40 @@ import (
 
 func DataSourceCluster() *schema.Resource {
 	type clusterData struct {
-		ClusterId   string       `json:"cluster_id"`
+		Id          string       `json:"id,omitempty" tf:"computed"`
+		ClusterId   string       `json:"cluster_id,omitempty" tf:"computed"`
+		Name        string       `json:"name,omitempty" tf:"computed"`
 		ClusterInfo *ClusterInfo `json:"cluster_info,omitempty" tf:"computed"`
 	}
 	return common.DataResource(clusterData{}, func(ctx context.Context, e interface{}, c *common.DatabricksClient) error {
 		data := e.(*clusterData)
 		clusterAPI := NewClustersAPI(ctx, c)
-		clusterInfo, err := clusterAPI.Get(data.ClusterId)
-
-		data.ClusterInfo = &clusterInfo
-		if err != nil {
-			return err
+		if data.Name != "" {
+			clusters, err := clusterAPI.List()
+			if err != nil {
+				return err
+			}
+			for _, clst := range clusters {
+				cluster := clst
+				if cluster.ClusterName == data.Name {
+					data.ClusterInfo = &cluster
+					break
+				}
+			}
+			if data.ClusterInfo == nil {
+				return fmt.Errorf("there is no cluster with name '%s'", data.Name)
+			}
+		} else if data.ClusterId != "" {
+			cls, err := clusterAPI.Get(data.ClusterId)
+			if err != nil {
+				return err
+			}
+			data.ClusterInfo = &cls
+		} else {
+			return fmt.Errorf("you need to specify either `name` or `cluster_id`")
 		}
+		data.Id = data.ClusterInfo.ClusterID
+		data.ClusterId = data.ClusterInfo.ClusterID
 
 		return nil
 	})

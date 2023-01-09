49b3f4ffecb7f1eeed843225436f1d7bf4c607de
Alex Ott <alexey.ott@databricks.com>
2023-01-09 19:29:42 +0100

    Exporter: use `databricks_group_role` instead of deprecated `databricks_group_instance_profile` (#1905)
    * Exporter: use `databricks_group_role` instead of deprecated `databricks_group_instance_profile`
    
    * explicitly refer to ARN attribute

--- exporter/importables.go
+++ exporter/importables.go
@@ -190,11 +190,11 @@ var resourcesMap map[string]importable = map[string]importable{
 			return splits[len(splits)-1]
 		},
 	},
-	"databricks_group_instance_profile": {
+	"databricks_group_role": {
 		Service: "access",
 		Depends: []reference{
 			{Path: "group_id", Resource: "databricks_group"},
-			{Path: "instance_profile_id", Resource: "databricks_instance_profile"},
+			{Path: "role", Resource: "databricks_instance_profile", Match: "instance_profile_arn"},
 		},
 	},
 	"databricks_cluster": {
@@ -572,7 +572,7 @@ var resourcesMap map[string]importable = map[string]importable{
 						ID:       instanceProfile.Value,
 					})
 					ic.Emit(&resource{
-						Resource: "databricks_group_instance_profile",
+						Resource: "databricks_group_role",
 						ID:       fmt.Sprintf("%s|%s", g.ID, instanceProfile.Value),
 					})
 				}

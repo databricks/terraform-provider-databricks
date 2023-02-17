9c8eab6e1a6bf5125c10d5aba24fae7f59e12b19
Serge Smertin <serge.smertin@databricks.com>
2023-02-17 20:02:20 +0100

    bring back table acl test

--- access/acceptance/sql_permissions_test.go
+++ access/acceptance/sql_permissions_test.go
@@ -1,47 +1,53 @@
 package acceptance
 
 import (
+	"context"
+	"fmt"
+	"os"
 	"testing"
+
+	"github.com/databricks/databricks-sdk-go"
+	"github.com/databricks/terraform-provider-databricks/internal/acceptance"
+	"github.com/databricks/terraform-provider-databricks/qa"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestAccTableACL(t *testing.T) {
-	// cloudEnv := os.Getenv("CLOUD_ENV")
-	// if cloudEnv == "" {
-	// 	t.Skip("Acceptance tests skipped unless env 'CLOUD_ENV' is set")
-	// }
-	// t.Parallel()
-	// client := common.CommonEnvironmentClient()
-	// client.WithCommandExecutor(func(ctx context.Context,
-	// 	dc *common.DatabricksClient) common.CommandExecutor {
-	// 	return commands.NewCommandsAPI(ctx, dc)
-	// })
+	cloudEnv := os.Getenv("CLOUD_ENV")
+	if cloudEnv == "" {
+		t.Skip("Acceptance tests skipped unless env 'CLOUD_ENV' is set")
+	}
+	t.Parallel()
 
-	// shell := client.CommandExecutor(context.Background())
-	// clusterInfo := compute.NewTinyClusterInCommonPoolPossiblyReused()
-	// talbeName := qa.RandomName("table_acl_")
+	ctx := context.Background()
+	w := databricks.Must(databricks.NewWorkspaceClient())
+	info, err := w.Clusters.GetOrCreateRunningCluster(ctx, "tf-dummy")
+	require.NoError(t, err)
 
-	// cr := shell.Execute(clusterInfo.ClusterID, "python",
-	// 	fmt.Sprintf("spark.range(10).write.saveAsTable('%s')",
-	// 		talbeName))
-	// require.False(t, cr.Failed(), cr.Error())
-	// os.Setenv("TABLE_ACL_TEST_TABLE", talbeName)
-	// defer func() {
-	// 	cr := shell.Execute(clusterInfo.ClusterID, "sql",
-	// 		fmt.Sprintf("DROP TABLE %s", talbeName))
-	// 	assert.False(t, cr.Failed(), cr.Error())
-	// }()
+	talbeName := qa.RandomName("table_acl_")
+	cr := w.CommandExecutor.Execute(ctx, info.ClusterId, "python",
+		fmt.Sprintf("spark.range(10).write.saveAsTable('%s')",
+			talbeName))
+	require.False(t, cr.Failed(), cr.Error())
+	os.Setenv("TABLE_ACL_TEST_TABLE", talbeName)
+	defer func() {
+		cr := w.CommandExecutor.Execute(ctx, info.ClusterId, "sql",
+			fmt.Sprintf("DROP TABLE %s", talbeName))
+		assert.False(t, cr.Failed(), cr.Error())
+	}()
 
-	// acceptance.Test(t, []acceptance.Step{
-	// 	{
-	// 		Template: `
-	// 		resource "databricks_sql_permissions" "this" {
-	// 			table = "{env.TABLE_ACL_TEST_TABLE}"
+	acceptance.Test(t, []acceptance.Step{
+		{
+			Template: `
+			resource "databricks_sql_permissions" "this" {
+				table = "{env.TABLE_ACL_TEST_TABLE}"
 
-	// 			privilege_assignments {
-	// 				principal = "users"
-	// 				privileges = ["SELECT"]
-	// 			}
-	// 		}`,
-	// 	},
-	// })
+				privilege_assignments {
+					principal = "users"
+					privileges = ["SELECT"]
+				}
+			}`,
+		},
+	})
 }

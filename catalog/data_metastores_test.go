857d28eef436926636484599b93061ab5307bdf9
guillesd <guillermo.sanchez@ah.nl>
2023-02-20 13:53:54 +0100

    add new data test

--- catalog/data_metastores_test.go
+++ catalog/data_metastores_test.go
@@ -15,21 +15,21 @@ func TestMetastoresData(t *testing.T) {
 				Response: Metastores{
 					Metastores: []MetastoreInfo{
 						{
-							Name: "a",
-							StorageRoot: "",
-							DefaultDacID: "",
-							Owner: "",
-							MetastoreID: "",
-							Region: "",
-							Cloud: "",
+							Name:              "a",
+							StorageRoot:       "",
+							DefaultDacID:      "sth",
+							Owner:             "John.Doe@example.com",
+							MetastoreID:       "abc",
+							Region:            "",
+							Cloud:             "",
 							GlobalMetastoreId: "",
-							CreatedAt: 0,
-							CreatedBy: "",
-							UpdatedAt: 0,
-							UpdatedBy: "",
+							CreatedAt:         0,
+							CreatedBy:         "",
+							UpdatedAt:         0,
+							UpdatedBy:         "",
 							DeltaSharingScope: "",
 							DeltaSharingRecipientTokenLifetimeInSeconds: 0,
-							DeltaSharingOrganizationName: "",
+							DeltaSharingOrganizationName:                "",
 						},
 					},
 				},
@@ -40,6 +40,45 @@ func TestMetastoresData(t *testing.T) {
 		NonWritable: true,
 		ID:          "_",
 	}.ApplyNoError(t)
+}
+
+func TestMetastoresDataContainsName(t *testing.T) {
+	qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "GET",
+				Resource: "/api/2.1/unity-catalog/metastores",
+				Response: Metastores{
+					Metastores: []MetastoreInfo{
+						{
+							Name:         "a",
+							StorageRoot:  "abc",
+							DefaultDacID: "sth",
+							Owner:        "John.Doe@example.com",
+							MetastoreID:  "abc",
+						},
+						{
+							Name:         "b",
+							StorageRoot:  "dcw",
+							DefaultDacID: "sth",
+							Owner:        "John.Doe@example.com",
+							MetastoreID:  "ded",
+						},
+					},
+				},
+			},
+		},
+		Resource:    DataSourceMetastores(),
+		Read:        true,
+		NonWritable: true,
+		ID:          "_",
+		HCL: `metastores {
+			name = "a"
+			storage_root = "abc"
+		}`,
+	}.ApplyAndExpectData(t, map[string]any{
+		"metastores.0.name": "a",
+	})
 }
 
 func TestMetastoresData_Error(t *testing.T) {

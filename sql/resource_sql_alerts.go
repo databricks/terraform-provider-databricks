ebe7e337d53cdb354f1d91dbef3c6625198e238b
kartikgupta-db <kartik.gupta@databricks.com>
2023-03-02 13:57:33 +0000

    switch to go sdk

--- sql/resource_sql_alerts.go
+++ sql/resource_sql_alerts.go
@@ -2,10 +2,9 @@ package sql
 
 import (
 	"context"
-	"fmt"
 
+	"github.com/databricks/databricks-sdk-go/service/sql"
 	"github.com/databricks/terraform-provider-databricks/common"
-	"github.com/databricks/terraform-provider-databricks/sql/api"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
@@ -27,15 +26,15 @@ type AlertEntity struct {
 	Parent  string        `json:"parent,omitempty" tf:"suppress_diff,force_new"`
 }
 
-func (a *AlertEntity) toCreateAlertApiObject(s map[string]*schema.Schema, data *schema.ResourceData) (*api.CreateAlert, error) {
+func (a *AlertEntity) toCreateAlertApiObject(s map[string]*schema.Schema, data *schema.ResourceData) (sql.CreateAlert, error) {
 	common.DataToStructPointer(data, s, a)
 
-	var ca api.CreateAlert
+	var ca sql.CreateAlert
 	ca.Name = a.Name
 	ca.Parent = a.Parent
 	ca.QueryId = a.QueryId
 	ca.Rearm = a.Rearm
-	ca.Options = &api.AlertOptions{
+	ca.Options = sql.AlertOptions{
 		Column:        a.Options.Column,
 		CustomBody:    a.Options.CustomBody,
 		CustomSubject: a.Options.CustomSubject,
@@ -44,13 +43,32 @@ func (a *AlertEntity) toCreateAlertApiObject(s map[string]*schema.Schema, data *
 		Value:         a.Options.Value,
 	}
 
-	return &ca, nil
+	return ca, nil
 }
 
-func (a *AlertEntity) fromAPIObject(apiAlert *api.Alert, s map[string]*schema.Schema, data *schema.ResourceData) error {
+func (a *AlertEntity) toEditAlertApiObject(s map[string]*schema.Schema, data *schema.ResourceData) (sql.EditAlert, error) {
+	common.DataToStructPointer(data, s, a)
+
+	return sql.EditAlert{
+		AlertId: data.Id(),
+		Name:    a.Name,
+		Options: sql.AlertOptions{
+			Column:        a.Options.Column,
+			CustomBody:    a.Options.CustomBody,
+			CustomSubject: a.Options.CustomSubject,
+			Muted:         a.Options.Muted,
+			Op:            a.Options.Op,
+			Value:         a.Options.Value,
+		},
+		QueryId: a.QueryId,
+		Rearm:   a.Rearm,
+	}, nil
+}
+
+func (a *AlertEntity) fromAPIObject(apiAlert *sql.Alert, s map[string]*schema.Schema, data *schema.ResourceData) error {
 	a.Name = apiAlert.Name
 	a.Parent = apiAlert.Parent
-	a.QueryId = apiAlert.Query.ID
+	a.QueryId = apiAlert.Query.Id
 	a.Rearm = apiAlert.Rearm
 	a.Options = &AlertOptions{
 		Column:        apiAlert.Options.Column,
@@ -63,45 +81,6 @@ func (a *AlertEntity) fromAPIObject(apiAlert *api.Alert, s map[string]*schema.Sc
 
 	return common.StructToData(a, s, data)
 }
-func NewAlertApi(ctx context.Context, m any) AlertApi {
-	return AlertApi{m.(*common.DatabricksClient), ctx}
-}
-
-type AlertApi struct {
-	client  *common.DatabricksClient
-	context context.Context
-}
-
-func (a AlertApi) Create(q *api.CreateAlert) (*api.Alert, error) {
-	var r api.Alert
-	err := a.client.Post(a.context, "/preview/sql/alerts", q, &r)
-	if err != nil {
-		return nil, err
-	}
-
-	return &r, nil
-}
-
-func (a AlertApi) Get(id string) (*api.Alert, error) {
-	var alert api.Alert
-	path := fmt.Sprintf("/preview/sql/alerts/%v", id)
-	err := a.client.Get(a.context, path, nil, &alert)
-	if err != nil {
-		return nil, err
-	}
-
-	return &alert, nil
-}
-
-func (a AlertApi) Delete(id string) error {
-	path := fmt.Sprintf("/preview/sql/alerts/%v", id)
-	return a.client.Get(a.context, path, nil, nil)
-}
-
-func (a AlertApi) Update(id string, request *api.CreateAlert) error {
-	path := fmt.Sprintf("/preview/sql/alerts/%v", id)
-	return a.client.Put(a.context, path, request)
-}
 
 func ResourceSqlAlert() *schema.Resource {
 	s := common.StructToSchema(AlertEntity{}, func(m map[string]*schema.Schema) map[string]*schema.Schema {
@@ -112,40 +91,52 @@ func ResourceSqlAlert() *schema.Resource {
 
 	return common.Resource{
 		Create: func(ctx context.Context, data *schema.ResourceData, c *common.DatabricksClient) error {
+			w, err := c.WorkspaceClient()
+			if err != nil {
+				return err
+			}
 			var a AlertEntity
 			ca, err := a.toCreateAlertApiObject(s, data)
 			if err != nil {
 				return err
 			}
-
-			apiAlert, err := NewAlertApi(ctx, c).Create(ca)
+			apiAlert, err := w.Alerts.Create(ctx, ca)
 			if err != nil {
 				return err
 			}
-
 			data.SetId(apiAlert.Id)
 			return nil
 		},
 		Read: func(ctx context.Context, data *schema.ResourceData, c *common.DatabricksClient) error {
-			apiAlert, err := NewAlertApi(ctx, c).Get(data.Id())
+			w, err := c.WorkspaceClient()
 			if err != nil {
 				return err
 			}
-
+			apiAlert, err := w.Alerts.GetByAlertId(ctx, data.Id())
+			if err != nil {
+				return err
+			}
 			var a AlertEntity
 			return a.fromAPIObject(apiAlert, s, data)
 		},
 		Update: func(ctx context.Context, data *schema.ResourceData, c *common.DatabricksClient) error {
-			var a AlertEntity
-			ca, err := a.toCreateAlertApiObject(s, data)
+			w, err := c.WorkspaceClient()
 			if err != nil {
 				return err
 			}
-
-			return NewAlertApi(ctx, c).Update(data.Id(), ca)
+			var a AlertEntity
+			ca, err := a.toEditAlertApiObject(s, data)
+			if err != nil {
+				return err
+			}
+			return w.Alerts.Update(ctx, ca)
 		},
 		Delete: func(ctx context.Context, data *schema.ResourceData, c *common.DatabricksClient) error {
-			return NewAlertApi(ctx, c).Delete(data.Id())
+			w, err := c.WorkspaceClient()
+			if err != nil {
+				return err
+			}
+			return w.Alerts.DeleteByAlertId(ctx, data.Id())
 		},
 		Schema: s,
 	}.ToResource()

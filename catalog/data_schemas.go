bcdd93d53f0e2f6ac4b39160f7fe028f4b3dd646
Vuong Nguyen <vuong.nguyen@databricks.com>
2023-03-03 17:18:26 +0000

    Migrate `databricks_schemas` data to Go SDK

--- catalog/data_schemas.go
+++ catalog/data_schemas.go
@@ -3,24 +3,23 @@ package catalog
 import (
 	"context"
 
+	"github.com/databricks/databricks-sdk-go"
+	"github.com/databricks/databricks-sdk-go/service/unitycatalog"
 	"github.com/databricks/terraform-provider-databricks/common"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func DataSourceSchemas() *schema.Resource {
-	type schemasData struct {
+	return common.WorkspaceData(func(ctx context.Context, data *struct {
 		CatalogName string   `json:"catalog_name"`
 		Ids         []string `json:"ids,omitempty" tf:"computed,slice_set"`
-	}
-	return common.DataResource(schemasData{}, func(ctx context.Context, e any, c *common.DatabricksClient) error {
-		data := e.(*schemasData)
-		schemasAPI := NewSchemasAPI(ctx, c)
-		schemas, err := schemasAPI.listByCatalog(data.CatalogName)
+	}, w *databricks.WorkspaceClient) error {
+		schemas, err := w.Schemas.ListAll(ctx, unitycatalog.ListSchemasRequest{CatalogName: data.CatalogName})
 		if err != nil {
 			return err
 		}
-		for _, v := range schemas.Schemas {
-			data.Ids = append(data.Ids, v.FullName)
+		for _, v := range schemas {
+			data.Ids = append(data.Ids, v.Name)
 		}
 		return nil
 	})

0693e4c31f4fa84f2e01d5b0f75271f4c0b46fd8
vuong-nguyen <44292934+nkvuong@users.noreply.github.com>
2023-03-07 15:57:24 +0000

    Migrated `databricks_shares` data source to Go SDK (#2072)

--- catalog/data_shares.go
+++ catalog/data_shares.go
@@ -3,22 +3,21 @@ package catalog
 import (
 	"context"
 
+	"github.com/databricks/databricks-sdk-go"
 	"github.com/databricks/terraform-provider-databricks/common"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func DataSourceShares() *schema.Resource {
-	type sharesData struct {
+	return common.WorkspaceData(func(ctx context.Context, data *struct {
 		Shares []string `json:"shares,omitempty" tf:"computed,slice_set"`
-	}
-	return common.DataResource(sharesData{}, func(ctx context.Context, e any, c *common.DatabricksClient) error {
-		data := e.(*sharesData)
-		sharesAPI := NewSharesAPI(ctx, c)
-		shares, err := sharesAPI.list()
+	}, w *databricks.WorkspaceClient) error {
+
+		shares, err := w.Shares.ListAll(ctx)
 		if err != nil {
 			return err
 		}
-		for _, share := range shares.Shares {
+		for _, share := range shares {
 			data.Shares = append(data.Shares, share.Name)
 		}
 		return nil

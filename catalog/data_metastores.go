53cbf83089e12b1b6e988bdf60f2a718bd9ce4f6
guillesd <guillermo.sanchez@ah.nl>
2023-03-17 14:31:23 +0100

    use databricks-go-sdk for data_metastore.go

--- catalog/data_metastores.go
+++ catalog/data_metastores.go
@@ -3,22 +3,22 @@ package catalog
 import (
 	"context"
 
+	"github.com/databricks/databricks-sdk-go"
+	"github.com/databricks/databricks-sdk-go/service/unitycatalog"
 	"github.com/databricks/terraform-provider-databricks/common"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func DataSourceMetastores() *schema.Resource {
 	type MetastoresData struct {
-		Metastores []MetastoreInfo `json:"metastores,omitempty" tf:"computed"`
+		Metastores []unitycatalog.MetastoreInfo `json:"metastores,omitempty" tf:"computed"`
 	}
-	return common.DataResource(MetastoresData{}, func(ctx context.Context, e any, c *common.DatabricksClient) error {
-		data := e.(*MetastoresData)
-		metastoresAPI := NewMetastoresAPI(ctx, c)
-		metastores, err := metastoresAPI.listMetastores()
+	return common.WorkspaceData(func(ctx context.Context, data *MetastoresData, w *databricks.WorkspaceClient) error {
+		metastores, err := w.Metastores.ListAll(ctx)
 		if err != nil {
 			return err
 		}
-		data.Metastores = append(data.Metastores, metastores.Metastores...)
+		data.Metastores = metastores
 
 		return nil
 	})

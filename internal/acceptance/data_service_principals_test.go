bca50cd2fccbb4b93da570305902b9d48f5106c4
Vuong Nguyen <vuong.nguyen@databricks.com>
2023-03-31 21:41:02 +0100

    fix acc tests

--- internal/acceptance/data_service_principals_test.go
+++ internal/acceptance/data_service_principals_test.go
@@ -4,11 +4,45 @@ import (
 	"testing"
 )
 
-func TestAccDataSourceSPNs(t *testing.T) {
+const spns = `
+resource "databricks_service_principal" "this" {
+	display_name = "SPN {var.RANDOM}"
+}
+
+data databricks_service_principals "this" {
+	display_name_contains = ""
+	depends_on = [databricks_service_principal.this]
+}`
+
+const azureSpns = `
+resource "databricks_service_principal" "this" {
+	application_id = "{var.RANDOM_UUID}"
+	display_name = "SPN {var.RANDOM}"
+	force = true
+}
+
+data databricks_service_principals "this" {
+	display_name_contains = ""
+	depends_on = [databricks_service_principal.this]
+}`
+
+func TestAccDataSourceSPNsOnAWS(t *testing.T) {
+	GetEnvOrSkipTest(t, "TEST_EC2_INSTANCE_PROFILE")
 	workspaceLevel(t, step{
-		Template: `
-		data databricks_service_principals "this" {
-			display_name_contains = ""
-		}`,
+		Template: spns,
+	})
+}
+
+func TestAccDataSourceSPNsOnGCP(t *testing.T) {
+	GetEnvOrSkipTest(t, "GOOGLE_CREDENTIALS")
+	workspaceLevel(t, step{
+		Template: spns,
+	})
+}
+
+func TestAccDataSourceSPNsOnAzure(t *testing.T) {
+	GetEnvOrSkipTest(t, "ARM_CLIENT_ID")
+	workspaceLevel(t, step{
+		Template: azureSpns,
 	})
 }

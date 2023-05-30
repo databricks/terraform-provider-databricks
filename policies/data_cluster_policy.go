fb02e3b32801dfaf8105404314a03bf94b4e243e
Cedric <23346008+Cedric98@users.noreply.github.com>
2023-05-30 16:02:28 +0200

    Add more attributes to cluster policy

--- policies/data_cluster_policy.go
+++ policies/data_cluster_policy.go
@@ -11,10 +11,14 @@ import (
 // DataSourceClusterPolicy returns information about cluster policy specified by name
 func DataSourceClusterPolicy() *schema.Resource {
 	return common.WorkspaceData(func(ctx context.Context, data *struct {
-		Id                 string `json:"id,omitempty" tf:"computed"`
-		Name               string `json:"name,omitempty" tf:"computed"`
-		Definition         string `json:"definition,omitempty" tf:"computed"`
-		MaxClustersPerUser int    `json:"max_clusters_per_user,omitempty" tf:"computed"`
+		Id                        string `json:"id,omitempty" tf:"computed"`
+		Name                      string `json:"name,omitempty" tf:"computed"`
+		Definition                string `json:"definition,omitempty" tf:"computed"`
+		Description               string `json:"description,omitempty" tf:"computed"`
+		FamilyId                  string `json:"family_id,omitempty" tf:"computed"`
+		FamilyDefinitionOverrides string `json:"family_definition_overrides,omitempty" tf:"computed"`
+		IsDefault                 bool   `json:"is_default,omitempty" tf:"computed"`
+		MaxClustersPerUser        int    `json:"max_clusters_per_user,omitempty" tf:"computed"`
 	}, w *databricks.WorkspaceClient) error {
 		policy, err := w.ClusterPolicies.GetByName(ctx, data.Name)
 		if err != nil {
@@ -22,6 +26,10 @@ func DataSourceClusterPolicy() *schema.Resource {
 		}
 		data.Id = policy.PolicyId
 		data.Definition = policy.Definition
+		data.Description = policy.Description
+		data.FamilyId = policy.PolicyFamilyId
+		data.FamilyDefinitionOverrides = policy.PolicyFamilyDefinitionOverrides
+		data.IsDefault = policy.IsDefault
 		data.MaxClustersPerUser = int(policy.MaxClustersPerUser)
 		return nil
 	})

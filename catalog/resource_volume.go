fd509fb13424eb580af429b94572973f7138303b
Tanmay Rustagi <88379306+tanmay-db@users.noreply.github.com>
2023-05-31 11:45:57 +0200

    Added support for Unity Catalog `databricks_volume` resource (#2324)
    Support is added for UC Volumes CRUD APIs.

---
+++ catalog/resource_volume.go
@@ -0,0 +1,100 @@
+package catalog
+
+import (
+	"context"
+
+	"github.com/databricks/databricks-sdk-go/service/catalog"
+	"github.com/databricks/terraform-provider-databricks/common"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+// This structure contains the fields of catalog.UpdateVolumeRequestContent and catalog.CreateVolumeRequestContent
+// We need to create this because we need Owner, FullNameArg, SchemaName and CatalogName which aren't present in a single of them.
+// We also need to annotate tf:"computed" for the Owner field.
+type VolumeInfo struct {
+	// The name of the catalog where the schema and the volume are
+	CatalogName string `json:"catalog_name"`
+	// The comment attached to the volume
+	Comment string `json:"comment,omitempty"`
+	// The name of the schema where the volume is
+	SchemaName  string `json:"schema_name"`
+	FullNameArg string `json:"-" url:"-"`
+	// The name of the volume
+	Name string `json:"name"`
+	// The identifier of the user who owns the volume
+	Owner string `json:"owner,omitempty" tf:"computed"`
+	// The storage location on the cloud
+	StorageLocation string             `json:"storage_location,omitempty"`
+	VolumeType      catalog.VolumeType `json:"volume_type"`
+}
+
+func ResourceVolume() *schema.Resource {
+	s := common.StructToSchema(VolumeInfo{},
+		func(m map[string]*schema.Schema) map[string]*schema.Schema {
+			return m
+		})
+	return common.Resource{
+		Schema: s,
+		Create: func(ctx context.Context, d *schema.ResourceData, c *common.DatabricksClient) error {
+			w, err := c.WorkspaceClient()
+			if err != nil {
+				return err
+			}
+			var createVolumeRequestContent catalog.CreateVolumeRequestContent
+			common.DataToStructPointer(d, s, &createVolumeRequestContent)
+			v, err := w.Volumes.Create(ctx, createVolumeRequestContent)
+			if err != nil {
+				return err
+			}
+			d.SetId(v.FullName)
+
+			// Update owner if it is provided
+			if d.Get("owner") != "" {
+				var updateVolumeRequestContent catalog.UpdateVolumeRequestContent
+				common.DataToStructPointer(d, s, &updateVolumeRequestContent)
+				updateVolumeRequestContent.FullNameArg = d.Id()
+				_, err = w.Volumes.Update(ctx, updateVolumeRequestContent)
+				if err != nil {
+					return err
+				}
+			}
+
+			return nil
+		},
+		Read: func(ctx context.Context, d *schema.ResourceData, c *common.DatabricksClient) error {
+			w, err := c.WorkspaceClient()
+			if err != nil {
+				return err
+			}
+			v, err := w.Volumes.ReadByFullNameArg(ctx, d.Id())
+			if err != nil {
+				return err
+			}
+			return common.StructToData(v, s, d)
+		},
+		Update: func(ctx context.Context, d *schema.ResourceData, c *common.DatabricksClient) error {
+			w, err := c.WorkspaceClient()
+			if err != nil {
+				return err
+			}
+			var updateVolumeRequestContent catalog.UpdateVolumeRequestContent
+			common.DataToStructPointer(d, s, &updateVolumeRequestContent)
+			updateVolumeRequestContent.FullNameArg = d.Id()
+			v, err := w.Volumes.Update(ctx, updateVolumeRequestContent)
+			// We need to update the resource data because Name is updatable and FullName consists of Name,
+			// So if we don't update the field then the requests would be made to old FullName which doesn't exists.
+			d.SetId(v.FullName)
+			if err != nil {
+				return err
+			}
+			return nil
+		},
+		Delete: func(ctx context.Context, d *schema.ResourceData, c *common.DatabricksClient) error {
+			w, err := c.WorkspaceClient()
+			if err != nil {
+				return err
+			}
+			return w.Volumes.DeleteByFullNameArg(ctx, d.Id())
+		},
+	}.ToResource()
+}
---
+++ internal/acceptance/volume_test.go
@@ -0,0 +1,143 @@
+package acceptance
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+)
+
+func TestUcAccVolumesResourceWithoutInitialOwnerAWSFullLifecycle(t *testing.T) {
+	GetEnvOrSkipTest(t, "TEST_BUCKET")
+	randomName := acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
+	unityWorkspaceLevel(t, step{
+		Template: fmt.Sprintf(`
+		resource "databricks_schema" "this" {
+			name 		 = "schema-%[1]s"
+			catalog_name = "main"
+		}
+
+		resource "databricks_storage_credential" "external" {
+			name = "cred-%[1]s"
+			aws_iam_role {
+				role_arn = "{env.TEST_METASTORE_DATA_ACCESS_ARN}"
+			}
+			comment = "Managed by TF"
+		}
+
+		resource "databricks_external_location" "some" {
+			name            = "external-%[1]s"
+			url             = "s3://{env.TEST_BUCKET}/somepath-%[1]s"
+			credential_name = databricks_storage_credential.external.id
+			comment         = "Managed by TF"
+		}
+
+		resource "databricks_volume" "this" {
+			name = "name-abc"
+			comment = "comment-abc"
+			catalog_name = "main"
+			schema_name = databricks_schema.this.name 
+			volume_type = "EXTERNAL"
+			storage_location   = databricks_external_location.some.url
+		}`, randomName),
+	}, step{
+		Template: fmt.Sprintf(`
+		resource "databricks_schema" "this" {
+			name 		 = "schema-%[1]s"
+			catalog_name = "main"
+		}
+
+		resource "databricks_storage_credential" "external" {
+			name = "cred-%[1]s"
+			aws_iam_role {
+				role_arn = "{env.TEST_METASTORE_DATA_ACCESS_ARN}"
+			}
+			comment = "Managed by TF"
+		}
+
+		resource "databricks_external_location" "some" {
+			name            = "external-%[1]s"
+			url             = "s3://{env.TEST_BUCKET}/somepath-%[1]s"
+			credential_name = databricks_storage_credential.external.id
+			comment         = "Managed by TF"
+		}
+
+		resource "databricks_volume" "this" {
+			name = "name-def"
+			comment = "comment-def"
+			owner = "account users"
+			catalog_name = "main"
+			schema_name = databricks_schema.this.name 
+			volume_type = "EXTERNAL"
+			storage_location   = databricks_external_location.some.url
+		}`, randomName),
+	})
+}
+
+func TestUcAccVolumesResourceWithInitialOnwerAWSFullLifecycle(t *testing.T) {
+	GetEnvOrSkipTest(t, "TEST_BUCKET")
+	randomName := acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
+	unityWorkspaceLevel(t, step{
+		Template: fmt.Sprintf(`
+		resource "databricks_schema" "this" {
+			name 		 = "schema-%[1]s"
+			catalog_name = "main"
+		}
+
+		resource "databricks_storage_credential" "external" {
+			name = "cred-%[1]s"
+			aws_iam_role {
+				role_arn = "{env.TEST_METASTORE_DATA_ACCESS_ARN}"
+			}
+			comment = "Managed by TF"
+		}
+
+		resource "databricks_external_location" "some" {
+			name            = "external-%[1]s"
+			url             = "s3://{env.TEST_BUCKET}/somepath-%[1]s"
+			credential_name = databricks_storage_credential.external.id
+			comment         = "Managed by TF"
+		}
+
+		resource "databricks_volume" "this" {
+			name = "name-abc"
+			comment = "comment-abc"
+			owner = "account users"
+			catalog_name = "main"
+			schema_name = databricks_schema.this.name 
+			volume_type = "EXTERNAL"
+			storage_location   = databricks_external_location.some.url
+		}`, randomName),
+	}, step{
+		Template: fmt.Sprintf(`
+		resource "databricks_schema" "this" {
+			name 		 = "schema-%[1]s"
+			catalog_name = "main"
+		}
+
+		resource "databricks_storage_credential" "external" {
+			name = "cred-%[1]s"
+			aws_iam_role {
+				role_arn = "{env.TEST_METASTORE_DATA_ACCESS_ARN}"
+			}
+			comment = "Managed by TF"
+		}
+
+		resource "databricks_external_location" "some" {
+			name            = "external-%[1]s"
+			url             = "s3://{env.TEST_BUCKET}/somepath-%[1]s"
+			credential_name = databricks_storage_credential.external.id
+			comment         = "Managed by TF"
+		}
+
+		resource "databricks_volume" "this" {
+			name = "name-def"
+			comment = "comment-def"
+			owner = "{env.TEST_DATA_ENG_GROUP}"
+			catalog_name = "main"
+			schema_name = databricks_schema.this.name 
+			volume_type = "EXTERNAL"
+			storage_location   = databricks_external_location.some.url
+		}`, randomName),
+	})
+}

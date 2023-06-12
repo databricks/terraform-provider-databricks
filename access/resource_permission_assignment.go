5751fc963240735da761fc96296494e72c5f9af9
RicNord <109558073+RicNord@users.noreply.github.com>
2023-06-12 16:57:11 +0200

    Explicit conversion of strings to int64. (#2346)
    Instead of machine dependant conversion when using integer type int.
    Will throw "value out of range" error for platforms that infer int as int32,
    when larger integers than int32 is used.

--- access/resource_permission_assignment.go
+++ access/resource_permission_assignment.go
@@ -66,7 +66,7 @@ func (a PermissionAssignmentAPI) List() (list PermissionAssignmentList, err erro
 }
 
 func mustInt64(s string) int64 {
-	n, err := strconv.ParseInt(s, 10, 0)
+	n, err := strconv.ParseInt(s, 10, 64)
 	if err != nil {
 		panic(err)
 	}

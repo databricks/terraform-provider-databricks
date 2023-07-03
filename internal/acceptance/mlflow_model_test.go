37e1d476a09cc158e1511bacd0ce945d77038f93
Michael Berk <michael.berk@databricks.com>
2023-07-03 17:56:02 -0400

    remove hard-coded test

--- internal/acceptance/mlflow_model_test.go
+++ internal/acceptance/mlflow_model_test.go
@@ -1,12 +1,10 @@
 package acceptance
 
 import (
-	"os"
 	"testing"
 )
 
 func TestAccMLflowModel(t *testing.T) {
-	os.Setenv("CLOUD_ENV", "AZURE")
 	workspaceLevel(t, step{
 		Template: `
 		resource "databricks_mlflow_model" "m1" {

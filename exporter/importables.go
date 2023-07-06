705d668ba7f4c4987d701ee22f34aa1702277fca
Krishna Swaroop K <krishna.swaroop@databricks.com>
2023-07-06 16:17:13 +0200

    Remove JobParameter emit

--- exporter/importables.go
+++ exporter/importables.go
@@ -458,14 +458,8 @@ var resourcesMap map[string]importable = map[string]importable{
 				if task.RunJobTask != nil {
 					if task.RunJobTask.JobID != "" {
 						ic.Emit(&resource{
-							Resource: "databricks_sql_endpoint",
+							Resource: "databricks_job",
 							ID:       task.RunJobTask.JobID,
-						})
-					}
-					if task.RunJobTask.JobParameters != "" {
-						ic.Emit(&resource{
-							Resource: "databricks_sql_endpoint",
-							ID:       task.RunJobTask.JobParameters,
 						})
 					}
 				}

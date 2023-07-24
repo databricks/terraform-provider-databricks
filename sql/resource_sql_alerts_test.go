0273661263eb8c32402a8e81ffca982dc3e209de
Fabian Jakobs <fabian.jakobs@databricks.com>
2023-07-24 12:31:32 +0200

    Supported boolean values in `databricks_sql_alert` alerts (#2506)

--- sql/resource_sql_alerts_test.go
+++ sql/resource_sql_alerts_test.go
@@ -69,3 +69,34 @@ func TestSqlAlertReadNumberValue(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, "xyz", d.Id(), "Resource ID should not be empty")
 }
+
+func TestSqlAlertReadBoolValue(t *testing.T) {
+	d, err := qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "GET",
+				Resource: "/api/2.0/preview/sql/alerts/xyz?",
+				Response: sql.Alert{
+					CreatedAt: "2020-01-01T00:00:00.000Z",
+					Id:        "xyz",
+					Parent:    "abc",
+					Name:      "Alert name",
+					Query: &sql.AlertQuery{
+						Id: "abc",
+					},
+					Options: &sql.AlertOptions{
+						Column: "col1",
+						Op:     "==",
+						Value:  true,
+					},
+				},
+			},
+		},
+		Resource: ResourceSqlAlert(),
+		Read:     true,
+		ID:       "xyz",
+	}.Apply(t)
+
+	assert.NoError(t, err)
+	assert.Equal(t, "xyz", d.Id(), "Resource ID should not be empty")
+}

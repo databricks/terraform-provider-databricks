bcb7c87468c3bca7bd361f9c2967378c791c8e5f
Vuong <vuong.nguyen@databricks.com>
2023-08-23 16:18:47 +0100

    fix test

--- internal/acceptance/metastore_data_access_test.go
+++ internal/acceptance/metastore_data_access_test.go
@@ -28,7 +28,7 @@ func TestUcAccMetastoreDataAccessOnAws(t *testing.T) {
 	unityWorkspaceLevel(t, step{
 		Template: `
 		resource "databricks_metastore_data_access" "this" {
-			metastore_id = "{env.TEST_METASTORE_DATA_ACCESS_ARN}"
+			metastore_id = "{env.TEST_METASTORE_ID}"
 			name         = "{var.RANDOM}"
 			aws_iam_role {
 				role_arn = "{env.TEST_METASTORE_DATA_ACCESS_ARN}"

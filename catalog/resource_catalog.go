62ee1bf970257ea0272bc5557ccfc916c9aa0ae4
Andrew Li <andrew.li@databricks.com>
2023-08-27 18:06:55 -0700

    Done

--- catalog/resource_catalog.go
+++ catalog/resource_catalog.go
@@ -33,7 +33,7 @@ type CatalogInfo struct {
 	ProviderName   string            `json:"provider_name,omitempty" tf:"force_new,conflicts:storage_root"`
 	ShareName      string            `json:"share_name,omitempty" tf:"force_new,conflicts:storage_root"`
 	ConnectionName string            `json:"connection_name,omitempty" tf:"force_new,conflicts:storage_root"`
-	Options        map[string]string `json:"options,omitempty" tf:"force_new,conflicts:storage_root"`
+	Options        map[string]string `json:"options,omitempty" tf:"force_new"`
 	Properties     map[string]string `json:"properties,omitempty"`
 	Owner          string            `json:"owner,omitempty" tf:"computed"`
 	IsolationMode  string            `json:"isolation_mode,omitempty" tf:"computed"`

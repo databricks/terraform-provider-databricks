bd5099e840736f513e8a98f7ed1758d6924e2191
monalisa <shreyas.goenka@databricks.com>
2023-08-30 16:57:04 +0200

    Fix mount read failing if cluster no longer exits

--- storage/resource_mount.go
+++ storage/resource_mount.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"time"
 
+	"github.com/databricks/databricks-sdk-go/apierr"
+	"github.com/databricks/terraform-provider-databricks/clusters"
 	"github.com/databricks/terraform-provider-databricks/common"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -37,6 +39,34 @@ func (cb mountCallback) preProcess(r *schema.Resource) func(
 	}
 }
 
+// If a cluster_id is specified for the mount, this function validates that the
+// cluster actually exists. If the cluster does not exist, we assume (incorrectly)
+// that the mount also does not exist.
+//
+// In cases of update requests this would lead to an attempted recreation of the
+// mount
+// In cases of delete, this could orphan the mount
+//
+// This is needed to resolve issue: https://github.com/databricks/terraform-provider-databricks/issues/1864
+// because without a running cluster we do not have a way to do CRUD on mounts.
+// The argument being that possibly orphaning mounts is a better experience than
+// requiring manual edits to tfstate.
+func validateCluster(cb func(context.Context, *schema.ResourceData, any) diag.Diagnostics) func(context.Context, *schema.ResourceData, any) diag.Diagnostics {
+	return func(ctx context.Context, d *schema.ResourceData, m any) diag.Diagnostics {
+		clusterId := d.Get("cluster_id").(string)
+		if clusterId != "" {
+			clustersAPI := clusters.NewClustersAPI(ctx, m)
+			_, err := clustersAPI.Get(clusterId)
+			if apierr.IsMissing(err) {
+				// Return with empty ID to indicate the resource no longer exists.
+				d.SetId("")
+				return nil
+			}
+		}
+		return cb(ctx, d, m)
+	}
+}
+
 func ResourceDatabricksMountSchema() map[string]*schema.Schema {
 	tpl := GenericMount{}
 	scm := common.StructToSchema(tpl, func(s map[string]*schema.Schema) map[string]*schema.Schema {
@@ -62,8 +92,8 @@ func ResourceMount() *schema.Resource {
 	tpl := GenericMount{}
 	r := commonMountResource(tpl, ResourceDatabricksMountSchema())
 	r.CreateContext = mountCallback(mountCreate).preProcess(r)
-	r.ReadContext = mountCallback(mountRead).preProcess(r)
-	r.DeleteContext = mountCallback(mountDelete).preProcess(r)
+	r.ReadContext = validateCluster(mountCallback(mountRead).preProcess(r))
+	r.DeleteContext = validateCluster(mountCallback(mountDelete).preProcess(r))
 	r.Importer = nil
 	r.Timeouts = &schema.ResourceTimeout{
 		Default: schema.DefaultTimeout(20 * time.Minute),

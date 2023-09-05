f89e5a57bc4ad75db3c3d3cd811c6f454fe8e7a6
Cedric <23346008+840@users.noreply.github.com>
2023-09-05 10:55:31 +0200

    Divide acceptance test for `data_mlflow_model` in 2 steps

--- internal/acceptance/data_mlflow_model_test.go
+++ internal/acceptance/data_mlflow_model_test.go
@@ -5,30 +5,33 @@ import (
 )
 
 func TestAccDataSourceMlflowModel(t *testing.T) {
-	workspaceLevel(t, step{
-		Template: `
-		resource "databricks_mlflow_model" "this" {
-		  name = "My MLflow Model"
-		
-		  description = "My MLflow model description"
-		
-		  tags {
-			key   = "key1"
-			value = "value1"
-		  }
-		  tags {
-			key   = "key2"
-			value = "value2"
-		  }
-		}
-		
-		data "databricks_mlflow_model" "this" {
-		  depends_on = [databricks_mlflow_model.this]
-		  name       = "My MLflow Model"
-		}
-		
-		output "model" {
-		  value = data.databricks_mlflow_model.this
-		}`,
-	})
+	workspaceLevel(t,
+		step{
+			Template: `
+			resource "databricks_mlflow_model" "this" {
+			  name = "My MLflow Model"
+			
+			  description = "My MLflow model description"
+			
+			  tags {
+				key   = "key1"
+				value = "value1"
+			  }
+			  tags {
+				key   = "key2"
+				value = "value2"
+			  }
+			}`,
+		},
+		step{
+			Template: `
+			data "databricks_mlflow_model" "this" {
+			  depends_on = [databricks_mlflow_model.this]
+			  name       = "My MLflow Model"
+			}
+			
+			output "model" {
+			  value = data.databricks_mlflow_model.this
+			}`,
+		})
 }

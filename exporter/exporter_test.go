5aee89383fd3f1d37f7cb9f830edde7525ee67dc
Alex Ott <alexott@gmail.com>
2023-10-02 10:44:29 +0200

    fixing flacky test

--- exporter/exporter_test.go
+++ exporter/exporter_test.go
@@ -675,7 +675,7 @@ func TestImportingClusters(t *testing.T) {
 				Method:   "POST",
 				Resource: "/api/2.0/clusters/events",
 				ExpectedRequest: clusters.EventsRequest{
-					ClusterID:  "test1",
+					ClusterID:  "test2",
 					Order:      "DESC",
 					EventTypes: []clusters.ClusterEventType{"PINNED", "UNPINNED"},
 					Limit:      1,
@@ -753,6 +753,7 @@ func TestImportingClusters(t *testing.T) {
 			},
 		},
 		func(ctx context.Context, client *common.DatabricksClient) {
+			os.Setenv("EXPORTER_PARALLELISM_databricks_cluster", "1")
 			tmpDir := fmt.Sprintf("/tmp/tf-%s", qa.RandomName())
 			defer os.RemoveAll(tmpDir)
 

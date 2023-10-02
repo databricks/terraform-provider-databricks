9ef8366f40ad952a2b9bf7640d0883e107578016
Tanmay Rustagi <88379306+tanmay-db@users.noreply.github.com>
2023-10-02 14:26:51 +0200

    Added logging package and fixed issue with API calls not being shown in DEBUG or lower log levels (#2747)
    * add logger
    
    * fix format strings
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -
    
    * -

--- provider/provider.go
+++ provider/provider.go
@@ -21,6 +21,7 @@ import (
 	"github.com/databricks/terraform-provider-databricks/commands"
 	"github.com/databricks/terraform-provider-databricks/common"
 	"github.com/databricks/terraform-provider-databricks/jobs"
+	tflogger "github.com/databricks/terraform-provider-databricks/logger"
 	"github.com/databricks/terraform-provider-databricks/mlflow"
 	"github.com/databricks/terraform-provider-databricks/mws"
 	"github.com/databricks/terraform-provider-databricks/permissions"
@@ -169,6 +170,7 @@ func DatabricksProvider() *schema.Provider {
 		if p.TerraformVersion != "" {
 			useragent.WithUserAgentExtra("terraform", p.TerraformVersion)
 		}
+		tflogger.SetLogger()
 		return configureDatabricksClient(ctx, d)
 	}
 	common.AddContextToAllResources(p, "databricks")

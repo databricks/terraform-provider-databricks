89d5375dad9f6b7eb696f04950ac900cecab609c
Vuong <vuong.nguyen@databricks.com>
2023-10-14 19:12:10 +0100

    fix test

--- internal/acceptance/ip_access_list_test.go
+++ internal/acceptance/ip_access_list_test.go
@@ -1,11 +1,7 @@
 package acceptance
 
 import (
-	"context"
 	"testing"
-	"time"
-
-	"github.com/databricks/terraform-provider-databricks/common"
 )
 
 func TestAccIPACLListsResourceFullLifecycle(t *testing.T) {
@@ -33,12 +29,6 @@ func TestAccIPACLListsResourceFullLifecycle(t *testing.T) {
 }
 
 func TestMwsAccIPACLListsResourceFullLifecycle(t *testing.T) {
-	// need to wait for internal state to stabilise, otherwise will get 409 Resource Conflict
-	waitForState := func(ctx context.Context, client *common.DatabricksClient, id string) error {
-		// sleep for 3 seconds
-		time.Sleep(3 * time.Second)
-		return nil
-	}
 	accountLevel(t, step{
 		Template: `
 		resource "databricks_ip_access_list" "this" {
@@ -49,17 +39,5 @@ func TestMwsAccIPACLListsResourceFullLifecycle(t *testing.T) {
 				"10.0.10.0/24"
 			]
 		}`,
-		Check: resourceCheck("databricks_ip_access_list.this", waitForState),
-	}, step{
-		Template: `
-		resource "databricks_ip_access_list" "this" {
-			label = "tf-{var.RANDOM}"
-			list_type = "BLOCK"
-			ip_addresses = [
-				"10.0.11.25",
-				"10.0.11.0/24"
-			]
-		}`,
-		Check: resourceCheck("databricks_ip_access_list.this", waitForState),
 	})
 }

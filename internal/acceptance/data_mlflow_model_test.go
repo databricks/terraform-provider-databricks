5a179865b90a94341b07b379c1c513b4b5e53d4f
Cedric <23346008+840@users.noreply.github.com>
2023-10-16 17:12:26 +0200

    Check on acceptance test for `id` and `description` content

--- internal/acceptance/data_mlflow_model_test.go
+++ internal/acceptance/data_mlflow_model_test.go
@@ -1,7 +1,10 @@
 package acceptance
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func TestAccDataMlflowModel(t *testing.T) {
@@ -43,10 +46,23 @@ func TestAccDataMlflowModel(t *testing.T) {
 			data "databricks_mlflow_model" "this" {
 			  depends_on = [databricks_mlflow_model.this]
 			  name       = databricks_mlflow_model.this.name
-			}
-			
-			output "model" {
-			  value = data.databricks_mlflow_model.this
 			}`,
+			Check: func(s *terraform.State) error {
+				r, ok := s.RootModule().Resources["data.databricks_mlflow_model.this"]
+				if !ok {
+					return fmt.Errorf("data not found in state")
+				}
+				id := r.Primary.Attributes["id"]
+				if id == "" {
+					return fmt.Errorf("id is empty: %v", r.Primary.Attributes)
+				}
+				expect := "My MLflow model description"
+				description := r.Primary.Attributes["description"]
+				if description != expect {
+					return fmt.Errorf("incorrect description. expected: %v, received: %v",
+						expect, description)
+				}
+				return nil
+			},
 		})
 }

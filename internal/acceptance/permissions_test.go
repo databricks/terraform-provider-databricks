bb24da18b0baf79d4d2c6faed58fdf47a95af95a
monalisa <shreyas.goenka@databricks.com>
2023-10-26 16:18:38 +0200

    Add integration test for SQL warehouse permissions

--- internal/acceptance/permissions_test.go
+++ internal/acceptance/permissions_test.go
@@ -121,3 +121,93 @@ func TestAccDatabricksReposPermissionsResourceFullLifecycle(t *testing.T) {
 		),
 	})
 }
+
+func TestAccDatabricksPermissionsForSqlWarehouses(t *testing.T) {
+	randomName := acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
+	workspaceLevel(t,
+		// Create a databricks_permissions resources, assigning a group CAN_MANAGE
+		// permission to the warehouse
+		step{
+			Template: fmt.Sprintf(`
+				resource "databricks_group" "one" {
+					display_name = "test-warehouse-permission-one-%s"
+				}
+				resource "databricks_permissions" "this" {
+					sql_endpoint_id = "{env.TEST_DEFAULT_WAREHOUSE_ID}"
+					access_control {
+						group_name = databricks_group.one.display_name
+						permission_level = "CAN_MANAGE"
+					}
+				}`, randomName),
+			Check: resource.ComposeTestCheckFunc(
+				// Validate object_type is set to warehouses
+				resource.TestCheckResourceAttr("databricks_permissions.this",
+					"object_type", "warehouses"),
+				resourceCheck("databricks_permissions.this",
+					func(ctx context.Context, client *common.DatabricksClient, id string) error {
+						warehousePermissions, err := permissions.NewPermissionsAPI(ctx, client).Read(id)
+						if err != nil {
+							return err
+						}
+						// Validate that the group has the expected permission
+						assert.Contains(t, warehousePermissions.AccessControlList, permissions.AccessControl{
+							GroupName: "test-warehouse-permission-one-" + randomName,
+							AllPermissions: []permissions.Permission{
+								{
+									PermissionLevel: "CAN_MANAGE",
+								},
+							},
+						})
+						return nil
+					}),
+			),
+		},
+		// Create another group and update permissions to CAN_USE for the second group
+		step{
+			Template: fmt.Sprintf(`			
+			resource "databricks_group" "one" {
+				display_name = "test-warehouse-permission-one-%[1]s"
+			}
+			resource "databricks_group" "two" {
+				display_name = "test-warehouse-permission-two-%[1]s"
+			}
+			resource "databricks_permissions" "this" {
+				sql_endpoint_id = "{env.TEST_DEFAULT_WAREHOUSE_ID}"
+				access_control {
+					group_name = databricks_group.one.display_name
+					permission_level = "CAN_MANAGE"
+				}
+				access_control {
+					group_name = databricks_group.two.display_name
+					permission_level = "CAN_USE"
+				}
+			}`, randomName),
+			Check: resourceCheck("databricks_permissions.this",
+				func(ctx context.Context, client *common.DatabricksClient, id string) error {
+					warehousePermissions, err := permissions.NewPermissionsAPI(ctx, client).Read(id)
+					if err != nil {
+						return err
+					}
+					// Validate that group one has the expected permission
+					assert.Contains(t, warehousePermissions.AccessControlList, permissions.AccessControl{
+						GroupName: "test-warehouse-permission-one-" + randomName,
+						AllPermissions: []permissions.Permission{
+							{
+								PermissionLevel: "CAN_MANAGE",
+							},
+						},
+					})
+					// Validate that group one has the expected permission
+					assert.Contains(t, warehousePermissions.AccessControlList, permissions.AccessControl{
+						GroupName: "test-warehouse-permission-two-" + randomName,
+						AllPermissions: []permissions.Permission{
+							{
+								PermissionLevel: "CAN_USE",
+							},
+						},
+					})
+					return nil
+				}),
+		},
+	)
+}

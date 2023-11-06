5e116a1e884db829ee7867dc3c51e90d3cfd5c90
Cedric <23346008+840@users.noreply.github.com>
2023-11-06 15:39:55 +0100

    Added `databricks_mlflow_model` data source (#2456)
    * Add data source `databricks_mlflow_model`
    
    * Add tests for `databricks_mlflow_model`
    
    * Add acceptance test for `databricks_mlflow_model`
    
    * Add docs for `databricks_mlflow_model`
    
    * Remove redundant check for `name` in favor of annotation
    
    * Add additional example for `databricks_mlflow_model`
    
    * Refactor `data_mlflow_model` to use `GetModel` rather than `GetModelVersion`
    
    * Divide acceptance test for `data_mlflow_model` in 2 steps
    
    * Fix acceptance tests in `data_mlflow_model_test.go`
    
    * Add example for the use of `databricks_mlflow_model` data source being used in a `databricks_model_serving` resource
    
    * Make sure `databricks_mlflow_model` data source references the same model
    
    * Define model in both steps
    
    * Run `make fmt`
    
    * Rename test to `TestAccDataMlflowModel`
    
    * Check on acceptance test for `id` and `description` content

--- provider/provider.go
+++ provider/provider.go
@@ -65,6 +65,7 @@ func DatabricksProvider() *schema.Provider {
 			"databricks_job":                     jobs.DataSourceJob(),
 			"databricks_metastore":               catalog.DataSourceMetastore(),
 			"databricks_metastores":              catalog.DataSourceMetastores(),
+			"databricks_mlflow_model":            mlflow.DataSourceModel(),
 			"databricks_mws_credentials":         mws.DataSourceMwsCredentials(),
 			"databricks_mws_workspaces":          mws.DataSourceMwsWorkspaces(),
 			"databricks_node_type":               clusters.DataSourceNodeType(),

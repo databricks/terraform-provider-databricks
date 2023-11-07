c24311db4128eaee19815ab8142e0d32907ce278
Vuong <vuong.nguyen@databricks.com>
2023-11-07 14:44:10 +0000

    add data source`databricks_current_config`

---
+++ mws/data_current_config.go
@@ -0,0 +1,37 @@
+package mws
+
+import (
+	"context"
+
+	"github.com/databricks/terraform-provider-databricks/common"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+type currentConfig struct {
+	IsAccount bool   `json:"is_account,omitempty" tf:"computed"`
+	AccountId string `json:"account_id,omitempty" tf:"computed"`
+	Host      string `json:"host,omitempty" tf:"computed"`
+	CloudType string `json:"cloud_type,omitempty" tf:"computed"`
+	AuthType  string `json:"auth_type,omitempty" tf:"computed"`
+}
+
+func DataSourceCurrentConfiguration() *schema.Resource {
+	return common.DataResource(currentConfig{}, func(ctx context.Context, e any, c *common.DatabricksClient) error {
+		data := e.(*currentConfig)
+		data.IsAccount = false
+		if c.Config.IsAccountClient() {
+			data.AccountId = c.Config.AccountID
+			data.IsAccount = true
+		}
+		data.Host = c.Config.Host
+		if c.Config.IsAws() {
+			data.CloudType = "aws"
+		} else if c.Config.IsAzure() {
+			data.CloudType = "azure"
+		} else {
+			data.CloudType = "gcp"
+		}
+		data.AuthType = c.Config.AuthType
+		return nil
+	})
+}

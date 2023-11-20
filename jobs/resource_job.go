040f6aa7dd4576e9aecec6aa598f76e2f6a96bcb
Philip Nord <philip.nord@databricks.com>
2023-11-20 10:49:25 +0100

    Update resource_job.go

--- jobs/resource_job.go
+++ jobs/resource_job.go
@@ -249,7 +249,7 @@ type Trigger struct {
 
 // JobSettings contains the information for configuring a job on databricks
 type JobSettings struct {
-	Name string `json:"name,omitempty" tf:"default:Untitled"`
+	Name        string `json:"name,omitempty" tf:"default:Untitled"`
 	Description string `json:"description,omitempty"`
 
 	// BEGIN Jobs API 2.0

b7c09d178e9a42921d4a04a05262fa23b7806157
ehab-eb <ehabelbadrawy@live.com>
2023-11-28 12:47:00 +0100

    Remove wait & resource creation in acceptance test

--- internal/acceptance/data_instance_profiles_test.go
+++ internal/acceptance/data_instance_profiles_test.go
@@ -8,22 +8,7 @@ func TestAccDataSourceInstanceProfiles(t *testing.T) {
 	GetEnvOrSkipTest(t, "TEST_EC2_INSTANCE_PROFILE")
 	workspaceLevel(t, step{
 		Template: `
-		resource "databricks_instance_profile" "this" {
-			instance_profile_arn = "{env.TEST_EC2_INSTANCE_PROFILE}"
-		}
-
-  		# Delay the data call, otherwise it will return null
-  		resource "time_sleep" "wait" {
-  			depends_on = [databricks_instance_profile.this]
-
-  			create_duration = "10s"
-		}
-
 		data "databricks_instance_profiles" "this" {
-			depends_on = [
-				databricks_instance_profile.this,
-    				time_sleep.wait,
-			]
 		}
 		
 		output "instance_profiles" {

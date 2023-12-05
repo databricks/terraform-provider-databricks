5794bee36e65c86096b935453a5b16346f3a8892
Alex Ott <alexott@gmail.com>
2023-12-05 11:35:22 +0100

    Add integration test for policy overrides

--- internal/acceptance/cluster_policy_test.go
+++ internal/acceptance/cluster_policy_test.go
@@ -28,3 +28,29 @@ func TestAccClusterPolicyResourceFullLifecycle(t *testing.T) {
 		}`,
 	})
 }
+
+func TestAccClusterPolicyResourceOverrideBuiltIn(t *testing.T) {
+	workspaceLevel(t, step{
+		Template: `resource "databricks_cluster_policy" "personal_vm" {
+			name = "Personal Compute"
+			policy_family_id = "personal-vm"
+			policy_family_definition_overrides = jsonencode({
+				"node_type_id": {
+				  "type": "fixed",
+				  "value": "Standard_DS3_v2"
+				}
+			  })
+		}
+		`,
+	})
+}
+
+func TestAccClusterPolicyResourceOverrideNew(t *testing.T) {
+	workspaceLevel(t, step{
+		Template: `resource "databricks_cluster_policy" "policyoverrideempty" {
+			policy_family_id = "personal-vm"
+			name             = "Policy Override {var.RANDOM}"
+		  }
+		  `,
+	})
+}

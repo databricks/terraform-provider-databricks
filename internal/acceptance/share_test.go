33d4e0e74660693f00f46e6641ef50b8a619dada
Tanmay Rustagi <tanmay.rustagi@databricks.com>
2023-12-14 16:39:07 +0100

    -

--- internal/acceptance/share_test.go
+++ internal/acceptance/share_test.go
@@ -93,3 +93,95 @@ func TestUcAccCreateShare(t *testing.T) {
 		`,
 	})
 }
+
+func TestUcAccCreateUpdate(t *testing.T) {
+	unityWorkspaceLevel(t, step{
+		Template: `
+		resource "databricks_catalog" "sandbox" {
+			name         = "sandbox{var.STICKY_RANDOM}"
+			comment      = "this catalog is managed by terraform"
+			properties = {
+				purpose = "testing"
+			}
+		}
+		
+		resource "databricks_schema" "things" {
+			catalog_name = databricks_catalog.sandbox.id
+			name         = "things{var.STICKY_RANDOM}"
+			comment      = "this database is managed by terraform"
+			properties = {
+				kind = "various"
+			}
+		}
+		
+		resource "databricks_table" "mytable" {
+			catalog_name = databricks_catalog.sandbox.id
+			schema_name = databricks_schema.things.name
+			name = "bar"
+			table_type = "MANAGED"
+			data_source_format = "DELTA"
+			
+			column {
+				name      = "id"
+				position  = 0
+				type_name = "INT"
+				type_text = "int"
+				type_json = "{\"name\":\"id\",\"type\":\"integer\",\"nullable\":true,\"metadata\":{}}"
+			}
+		}		
+		
+		resource "databricks_share" "myshare" {
+			name  = "{var.STICKY_RANDOM}-terraform-delta-share"
+			owner = "account users"
+			object {
+				name = databricks_table.mytable.id
+				comment = "c"
+				data_object_type = "TABLE"
+			}							
+		}`,
+	}, step{
+		Template: `
+		resource "databricks_catalog" "sandbox" {
+			name         = "sandbox{var.STICKY_RANDOM}"
+			comment      = "this catalog is managed by terraform"
+			properties = {
+				purpose = "testing"
+			}
+		}
+		
+		resource "databricks_schema" "things" {
+			catalog_name = databricks_catalog.sandbox.id
+			name         = "things{var.STICKY_RANDOM}"
+			comment      = "this database is managed by terraform"
+			properties = {
+				kind = "various"
+			}
+		}
+		
+		resource "databricks_table" "mytable" {
+			catalog_name = databricks_catalog.sandbox.id
+			schema_name = databricks_schema.things.name
+			name = "bar"
+			table_type = "MANAGED"
+			data_source_format = "DELTA"
+			
+			column {
+				name      = "id"
+				position  = 0
+				type_name = "INT"
+				type_text = "int"
+				type_json = "{\"name\":\"id\",\"type\":\"integer\",\"nullable\":true,\"metadata\":{}}"
+			}
+		}		
+		
+		resource "databricks_share" "myshare" {
+			name  = "{var.STICKY_RANDOM}-terraform-delta-share"
+			owner = "account users"
+			object {
+				name = databricks_table.mytable.id
+				comment = "d"
+				data_object_type = "TABLE"
+			}								
+		}`,
+	})
+}

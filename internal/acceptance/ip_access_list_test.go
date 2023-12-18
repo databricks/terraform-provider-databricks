dcad033f4d18a7c5806499bb29f12801e4487cb8
Tanmay Rustagi <tanmay.rustagi@databricks.com>
2023-12-18 20:16:40 +0100

    Revert "Add support for Databricks Account Console IP Access Lists (#2586)"
    This reverts commit 40c92d67d160de37ecd740d771ad3037ae398ab8.

--- internal/acceptance/ip_access_list_test.go
+++ internal/acceptance/ip_access_list_test.go
@@ -2,7 +2,6 @@ package acceptance
 
 import (
 	"testing"
-	"time"
 )
 
 func TestAccIPACLListsResourceFullLifecycle(t *testing.T) {
@@ -26,30 +25,5 @@ func TestAccIPACLListsResourceFullLifecycle(t *testing.T) {
 				"10.0.11.0/24"
 			]
 		}`,
-	})
-}
-
-func TestMwsAccIPACLListsResourceFullLifecycle(t *testing.T) {
-	accountLevel(t, step{
-		Template: `
-		resource "databricks_ip_access_list" "this" {
-			label = "tf-{var.RANDOM}"
-			list_type = "BLOCK"
-			ip_addresses = [
-				"10.0.10.25",
-				"10.0.10.0/24"
-			]
-		}`,
-	}, step{
-		PreConfig: func() { time.Sleep(1 * time.Second) },
-		Template: `
-			resource "databricks_ip_access_list" "this" {
-				label = "tf-{var.RANDOM}"
-				list_type = "BLOCK"
-				ip_addresses = [
-					"10.0.11.25",
-					"10.0.11.0/24"
-				]
-			}`,
 	})
 }

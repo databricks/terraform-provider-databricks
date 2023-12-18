16ac23e11e5119e4d27b2f344b330a2b17c10e56
Tanmay Rustagi <tanmay.rustagi@databricks.com>
2023-12-18 14:53:06 +0100

    -

--- internal/acceptance/share_test.go
+++ internal/acceptance/share_test.go
@@ -1,69 +1,74 @@
 package acceptance
 
 import (
+	"fmt"
 	"testing"
 )
 
-const preTestTemplate = `resource "databricks_catalog" "sandbox" {
-	name         = "sandbox{var.STICKY_RANDOM}"
-	comment      = "this catalog is managed by terraform"
-	properties = {
-		purpose = "testing"
+const preTestTemplate = `
+	resource "databricks_catalog" "sandbox" {
+		name         = "sandbox{var.STICKY_RANDOM}"
+		comment      = "this catalog is managed by terraform"
+		properties = {
+			purpose = "testing"
+		}
 	}
-}
 
-resource "databricks_schema" "things" {
-	catalog_name = databricks_catalog.sandbox.id
-	name         = "things{var.STICKY_RANDOM}"
-	comment      = "this database is managed by terraform"
-	properties = {
-		kind = "various"
+	resource "databricks_schema" "things" {
+		catalog_name = databricks_catalog.sandbox.id
+		name         = "things{var.STICKY_RANDOM}"
+		comment      = "this database is managed by terraform"
+		properties = {
+			kind = "various"
+		}
 	}
-}
 
-resource "databricks_table" "mytable" {
-	catalog_name = databricks_catalog.sandbox.id
-	schema_name = databricks_schema.things.name
-	name = "bar"
-	table_type = "MANAGED"
-	data_source_format = "DELTA"
-	
-	column {
-		name      = "id"
-		position  = 0
-		type_name = "INT"
-		type_text = "int"
-		type_json = "{\"name\":\"id\",\"type\":\"integer\",\"nullable\":true,\"metadata\":{}}"
+	resource "databricks_table" "mytable" {
+		catalog_name = databricks_catalog.sandbox.id
+		schema_name = databricks_schema.things.name
+		name = "bar"
+		table_type = "MANAGED"
+		data_source_format = "DELTA"
+		
+		column {
+			name      = "id"
+			position  = 0
+			type_name = "INT"
+			type_text = "int"
+			type_json = "{\"name\":\"id\",\"type\":\"integer\",\"nullable\":true,\"metadata\":{}}"
+		}
 	}
-}
 
-resource "databricks_table" "mytable_2" {
-	catalog_name = databricks_catalog.sandbox.id
-	schema_name = databricks_schema.things.name
-	name = "bar_2"
-	table_type = "MANAGED"
-	data_source_format = "DELTA"
-	
-	column {
-		name      = "id"
-		position  = 0
-		type_name = "INT"
-		type_text = "int"
-		type_json = "{\"name\":\"id\",\"type\":\"integer\",\"nullable\":true,\"metadata\":{}}"
+	resource "databricks_table" "mytable_2" {
+		catalog_name = databricks_catalog.sandbox.id
+		schema_name = databricks_schema.things.name
+		name = "bar_2"
+		table_type = "MANAGED"
+		data_source_format = "DELTA"
+		
+		column {
+			name      = "id"
+			position  = 0
+			type_name = "INT"
+			type_text = "int"
+			type_json = "{\"name\":\"id\",\"type\":\"integer\",\"nullable\":true,\"metadata\":{}}"
+		}
 	}
-}`
+`
 
-const preTestTemplateUpdate = `resource "databricks_grants" "some" {
-	catalog = databricks_catalog.sandbox.id
-	grant {
-		principal  = "account users"
-		privileges = ["ALL_PRIVILEGES"]
+const preTestTemplateUpdate = `
+	resource "databricks_grants" "some" {
+		catalog = databricks_catalog.sandbox.id
+		grant {
+			principal  = "account users"
+			privileges = ["ALL_PRIVILEGES"]
+		}
+		grant {
+			principal  = "{env.TEST_METASTORE_ADMIN_GROUP_NAME}"
+			privileges = ["ALL_PRIVILEGES"]
+		}
 	}
-	grant {
-		principal  = "{env.TEST_METASTORE_ADMIN_GROUP_NAME}"
-		privileges = ["ALL_PRIVILEGES"]
-	}
-}`
+`
 
 func TestUcAccCreateShare(t *testing.T) {
 	unityWorkspaceLevel(t, step{
@@ -106,42 +111,25 @@ func TestUcAccCreateShare(t *testing.T) {
 	})
 }
 
+func shareTemplateWithOwner(comment string, owner string) string {
+	return fmt.Sprintf(`
+		resource "databricks_share" "myshare" {
+			name  = "{var.STICKY_RANDOM}-terraform-delta-share"
+			owner = "%s"
+			object {
+				name = databricks_table.mytable.id
+				comment = "%s"
+				data_object_type = "TABLE"
+			}								
+		}`, owner, comment)
+}
+
 func TestUcAccUpdateShare(t *testing.T) {
 	unityWorkspaceLevel(t, step{
-		Template: preTestTemplate + `
-		` + preTestTemplateUpdate + `	
-		resource "databricks_share" "myshare" {
-			name  = "{var.STICKY_RANDOM}-terraform-delta-share"
-			owner = "account users"
-			object {
-				name = databricks_table.mytable.id
-				comment = "c"
-				data_object_type = "TABLE"
-			}								
-		}`,
+		Template: preTestTemplate + preTestTemplateUpdate + shareTemplateWithOwner("c", "account users"),
 	}, step{
-		Template: preTestTemplate + `
-		` + preTestTemplateUpdate + `				
-		resource "databricks_share" "myshare" {
-			name  = "{var.STICKY_RANDOM}-terraform-delta-share"
-			owner = "account users"
-			object {
-				name = databricks_table.mytable.id
-				comment = "e"
-				data_object_type = "TABLE"
-			}								
-		}`,
+		Template: preTestTemplate + preTestTemplateUpdate + shareTemplateWithOwner("e", "account users"),
 	}, step{
-		Template: preTestTemplate + `
-		` + preTestTemplateUpdate + `				
-		resource "databricks_share" "myshare" {
-			name  = "{var.STICKY_RANDOM}-terraform-delta-share"
-			owner = "{env.TEST_METASTORE_ADMIN_GROUP_NAME}"
-			object {
-				name = databricks_table.mytable.id
-				comment = "f"
-				data_object_type = "TABLE"
-			}								
-		}`,
+		Template: preTestTemplate + preTestTemplateUpdate + shareTemplateWithOwner("f", "{env.TEST_METASTORE_ADMIN_GROUP_NAME}"),
 	})
 }

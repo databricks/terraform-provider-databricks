1d41b591c0a7dd9d36390e6712a687c9eece39a7
Tanmay Rustagi <tanmay.rustagi@databricks.com>
2023-12-19 12:08:00 +0100

    modelServing

--- serving/resource_model_serving.go
+++ serving/resource_model_serving.go
@@ -12,51 +12,28 @@ import (
 
 const DefaultProvisionTimeout = 45 * time.Minute
 
-type CreateServingEndpoint struct {
-	// The core config of the serving endpoint.
-	Config EndpointCoreConfigInput `json:"config"`
-	// The name of the serving endpoint. This field is required and must be
-	// unique across a Databricks workspace. An endpoint name can consist of
-	// alphanumeric characters, dashes, and underscores.
-	Name string `json:"name"`
-	// Rate limits to be applied to the serving endpoint. NOTE: only external
-	// and foundation model endpoints are supported as of now.
-	RateLimits []serving.RateLimit `json:"rate_limits,omitempty"`
-	// Tags to be attached to the serving endpoint and automatically propagated
-	// to billing logs.
-	Tags []serving.EndpointTag `json:"tags,omitempty"`
-}
-
-type EndpointCoreConfigInput struct {
-	// Configuration for Inference Tables which automatically logs requests and
-	// responses to Unity Catalog.
-	AutoCaptureConfig *serving.AutoCaptureConfigInput `json:"auto_capture_config,omitempty"`
-	// The name of the serving endpoint to update. This field is required.
-	Name string `json:"-" url:"-"`
-	// A list of served models for the endpoint to serve.
-	// A serving endpoint can have up to 10 served models.
-	ServedModels []serving.ServedModelInput `json:"served_models,omitempty"`
-	// The traffic config defining how invocations to the serving endpoint
-	// should be routed.
-	TrafficConfig *serving.TrafficConfig `json:"traffic_config,omitempty"`
-}
-
 func ResourceModelServing() *schema.Resource {
 	s := common.StructToSchema(
-		CreateServingEndpoint{},
+		serving.CreateServingEndpoint{},
 		func(m map[string]*schema.Schema) map[string]*schema.Schema {
 			m["name"].ForceNew = true
-			common.MustSchemaPath(m, "config", "served_models", "scale_to_zero_enabled").Required = false
-			common.MustSchemaPath(m, "config", "served_models", "scale_to_zero_enabled").Optional = true
-			common.MustSchemaPath(m, "config", "served_models", "scale_to_zero_enabled").Default = true
-			common.MustSchemaPath(m, "config", "served_models", "name").Computed = true
 
-			common.MustSchemaPath(m, "config", "served_models", "workload_type").Default = "CPU"
-			// TODO: `config.served_models.workload_type` should be a `Optional+Computed` field. Also consider this for other similar fields.
-			// In this scenario, if a workspace does not have GPU serving, specifying `workload_type` = 'CPU' will get empty response from API.
-			common.MustSchemaPath(m, "config", "served_models", "workload_type").DiffSuppressFunc = func(k, old, new string, d *schema.ResourceData) bool {
-				return old == "" && new == "CPU"
+			for _, name := range []string{"served_models", "served_entities"} {
+				common.MustSchemaPath(m, "config", name).Computed = true
+				common.MustSchemaPath(m, "config", name, "name").Computed = true
+				common.MustSchemaPath(m, "config", name, "environment_vars").Computed = true
+				common.MustSchemaPath(m, "config", name, "scale_to_zero_enabled").Required = false
+				common.MustSchemaPath(m, "config", name, "scale_to_zero_enabled").Optional = true
+				common.MustSchemaPath(m, "config", name, "scale_to_zero_enabled").Default = true
+				common.MustSchemaPath(m, "config", name, "workload_type").Default = "CPU"
+				// TODO: `config.served_models.workload_type` should be a `Optional+Computed` field. Also consider this for other similar fields.
+				// In this scenario, if a workspace does not have GPU serving, specifying `workload_type` = 'CPU' will get empty response from API.
+				common.MustSchemaPath(m, "config", name, "workload_type").DiffSuppressFunc = func(k, old, new string, d *schema.ResourceData) bool {
+					return old == "" && new == "CPU"
+				}
 			}
+			common.MustSchemaPath(m, "config", "served_models").Deprecated = "Use `config.served_entities` instead."
+
 			common.MustSchemaPath(m, "config", "traffic_config").Computed = true
 
 			m["serving_endpoint_id"] = &schema.Schema{

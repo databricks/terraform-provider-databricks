97f0f43014a8a7140f00536e259bac478c9cf9cb
Miles Yucht <miles@databricks.com>
2023-12-20 09:36:15 +0100

    check in diff-schema scrip

---
+++ scripts/diff-schema.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+BASE_BRANCH="master"
+
+checkout_branch() {
+    local branch=$1
+    echo "Checking out branch: $branch"
+    git checkout $branch
+}
+
+# Function to generate provider schema
+generate_schema() {
+  local schema_name=$1
+  make install
+
+  echo "Generating provider schema for $branch..."
+  set -ex
+  cd /tmp
+  cat > main.tf <<EOF
+terraform {
+  required_providers {
+    databricks = {
+      source = "databricks/databricks"
+    }
+  }
+}
+EOF
+  terraform init
+  terraform providers schema -json > $schema_name.json
+  cd - # Go back to the original directory
+}
+
+if [ -n "$(git status --porcelain)" ]; then
+    echo "There are uncommitted changes. Please commit them before running this script."
+    exit 1
+fi
+
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+generate_schema "schema-new"
+checkout_branch $BASE_BRANCH
+generate_schema "schema-current"
+checkout_branch $CURRENT_BRANCH
+
+jd -color /tmp/schema-current.json /tmp/schema-new.json

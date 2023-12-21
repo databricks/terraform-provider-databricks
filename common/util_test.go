c68253b5eef9a99728933eac50ee263e4c077bbb
Alex Ott <alexey.ott@databricks.com>
2023-12-21 11:41:51 +0100

    Exporter: generate cluster libraries blocks directly in `databricks_cluster`, don't generate `databricks_library` resources (#2983)
    * Exporter: generate cluster libraries blocks directly in `databricks_cluster`, don't generate `databricks_library` resources
    
    Instead of generating separate `databricks_library` resources, cluster libraries are now
    generated inside of the `databricks_cluster` definition.  This decrease the number of the
    generated resources that is important for big workspaces as it decreases the state size.
    
    * Address review feedback
    
    * A bit more refactoring
    
    * Fix condition

--- common/util_test.go
+++ common/util_test.go
@@ -1,12 +1,27 @@
 package common
 
 import (
+	"context"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestStringIsUUID(t *testing.T) {
 	assert.True(t, StringIsUUID("3f670caf-9a4b-4479-8143-1a0878da8f57"))
 	assert.False(t, StringIsUUID("abc"))
+}
+
+func TestGetTerraformVersionFromContext(t *testing.T) {
+	assert.Equal(t, "unknown", GetTerraformVersionFromContext(context.Background()))
+
+	//
+	p := &schema.Provider{}
+	p.TerraformVersion = "exporter"
+	ctx := context.WithValue(context.Background(), Provider, p)
+	assert.Equal(t, "exporter", GetTerraformVersionFromContext(ctx))
+
+	//
+	assert.True(t, IsExporter(ctx))
 }

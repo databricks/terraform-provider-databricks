84ab1c41df1c9033d70c093744a050c83c974aae
edwardfeng-db <jinghao.feng@databricks.com>
2024-01-03 15:49:21 +0100

    update

--- clusters/resource_cluster.go
+++ clusters/resource_cluster.go
@@ -64,7 +64,8 @@ func (ClusterResourceProvider) UnderlyingType() Cluster {
 }
 
 func (ClusterResourceProvider) Aliases() map[string]string {
-	return map[string]string{"cluster_mount_infos": "cluster_mount_info"}
+	return map[string]string{}
+	// return map[string]string{"cluster_mount_infos": "cluster_mount_info"}
 }
 
 func (ClusterResourceProvider) TfOverlay() map[string]*schema.Schema {

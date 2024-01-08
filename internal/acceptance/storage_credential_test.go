eb3e3b987cade2adcc978e87218111a4e42a2c64
Vuong <vuong.nguyen@databricks.com>
2024-01-08 17:29:29 +0000

    remove azure test for now

--- internal/acceptance/storage_credential_test.go
+++ internal/acceptance/storage_credential_test.go
@@ -16,17 +16,6 @@ func TestUcAccStorageCredential(t *testing.T) {
 					aws_iam_role {
 						role_arn = "{env.TEST_METASTORE_DATA_ACCESS_ARN}"
 					}
-					comment = "Managed by TF"
-				}`,
-		})
-	case "azure":
-		unityWorkspaceLevel(t, step{
-			Template: `
-				resource "databricks_storage_credential" "external" {
-					name = "cred-{var.RANDOM}"
-					azure_managed_identity {
-						access_connector_id = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg-name/providers/Microsoft.Databricks/accessConnectors/connector-name"
-					}
 					skip_validation = true
 					comment = "Managed by TF"
 				}`,
@@ -37,6 +26,7 @@ func TestUcAccStorageCredential(t *testing.T) {
 				resource "databricks_storage_credential" "external" {
 					name = "cred-{var.RANDOM}"
 					databricks_gcp_service_account {}
+					skip_validation = true
 					comment = "Managed by TF"
 				}`,
 		})

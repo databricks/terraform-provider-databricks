da538a6af7f61c7e131d9a2f2a2c96db059e1bcf
Miles Yucht <miles@databricks.com>
2024-01-17 17:47:23 +0100

    add test back in

--- internal/acceptance/sql_endpoint_test.go
+++ internal/acceptance/sql_endpoint_test.go
@@ -12,12 +12,12 @@ import (
 
 func TestAccSQLEndpoint(t *testing.T) {
 	workspaceLevel(t, step{
-		// 	Template: `resource "databricks_sql_endpoint" "this" {
-		// 		name = "tf-{var.RANDOM}"
-		// 		cluster_size = "2X-Small"
-		// 		max_num_clusters = 1
-		// 	}`,
-		// }, step{
+		Template: `resource "databricks_sql_endpoint" "this" {
+				name = "tf-{var.RANDOM}"
+				cluster_size = "2X-Small"
+				max_num_clusters = 1
+			}`,
+	}, step{
 		Template: `
 		resource "databricks_sql_endpoint" "that" {
 			name = "tf-{var.RANDOM}"

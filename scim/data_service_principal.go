54810c473fcc3155965f6355f4c066d38b3dec3a
Karol Luszczek <karol.luszczek@reinsight.se>
2024-01-22 11:33:00 +0100

    service principal data source by sp_id

--- scim/data_service_principal.go
+++ scim/data_service_principal.go
@@ -8,7 +8,26 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-// DataSourceServicePrincipal returns information about the spn specified by the application_id
+// onlyOneNonEmpty checks if only one string in the slice is non-empty
+func onlyOneNonEmpty(strings []string) bool {
+	maxLen := 0
+	sumLen := 0
+
+	for _, s := range strings {
+		l := len(s)
+		if l > maxLen {
+			maxLen = l
+		}
+		sumLen += l
+	}
+	if sumLen != maxLen {
+		return false
+	} else {
+		return true
+	}
+}
+
+// DataSourceServicePrincipal returns information about the spn specified by the application_id, id, or display_name
 func DataSourceServicePrincipal() *schema.Resource {
 	type spnData struct {
 		ApplicationID  string `json:"application_id,omitempty" tf:"computed"`
@@ -26,27 +45,32 @@ func DataSourceServicePrincipal() *schema.Resource {
 		spnAPI := NewServicePrincipalsAPI(ctx, c)
 		var spList []User
 		var err error
-		if response.ApplicationID != "" && response.DisplayName != "" {
-			return fmt.Errorf("please specify only one of application_id or display_name")
+
+		if !onlyOneNonEmpty([]string{response.ApplicationID, response.DisplayName, response.SpID}) {
+			return fmt.Errorf("please specify only one of: application_id, sp_id, or display_name")
 		}
 		if response.ApplicationID != "" {
 			spList, err = spnAPI.Filter(fmt.Sprintf(`applicationId eq "%s"`, response.ApplicationID), true)
+		} else if response.SpID != "" {
+			spList, err = spnAPI.Filter(fmt.Sprintf(`id eq "%s"`, response.SpID), true)
 		} else if response.DisplayName != "" {
 			spList, err = spnAPI.Filter(fmt.Sprintf(`displayName eq "%s"`, response.DisplayName), true)
 		} else {
-			return fmt.Errorf("please specify either application_id or display_name")
+			return fmt.Errorf("please specify either application_id, display_name, or sp_id")
 		}
 		if err != nil {
 			return err
 		}
 		if len(spList) == 0 {
 			if response.ApplicationID != "" {
-				return fmt.Errorf("cannot find SP with ID %s", response.ApplicationID)
+				return fmt.Errorf("cannot find SP with an application ID %s", response.ApplicationID)
+			} else if response.SpID != "" {
+				return fmt.Errorf("cannot find SP with an ID %s", response.SpID)
 			} else {
 				return fmt.Errorf("cannot find SP with name %s", response.DisplayName)
 			}
 		} else if len(spList) > 1 {
-			return fmt.Errorf("there are more than 1 service principal with name %s", response.DisplayName)
+			return fmt.Errorf("there are more than %d Service Principals with name %s", len(spList), response.DisplayName)
 		}
 
 		sp := spList[0]

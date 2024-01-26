909e7b027f6c6fbdb2ac44f56caa2e28d6c67be5
James Davidheiser <jamesdavidheiser@asana.com>
2024-01-26 16:24:27 -0500

    initial prototype

---
+++ catalog/data_table.go
@@ -0,0 +1,24 @@
+package catalog
+
+import (
+	"context"
+
+	"github.com/databricks/databricks-sdk-go"
+	"github.com/databricks/databricks-sdk-go/service/catalog"
+	"github.com/databricks/terraform-provider-databricks/common"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+type tableParams struct {
+	FullName string `json:"full_name" tf:"computed,optional`
+}
+
+func DataSourceTable() *schema.Resource {
+	return common.WorkspaceDataWithParams(func(ctx context.Context, data tableParams, w *databricks.WorkspaceClient) (*catalog.TableInfo, error) {
+		table_info, err := w.Tables.Get(ctx, catalog.GetTableRequest{FullName: data.FullName})
+		if err != nil {
+			return nil, err
+		}
+		return table_info, nil
+	})
+}

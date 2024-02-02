4bf42eaac528c75de0076a9f1798beded6c75915
Tanmay Rustagi <tanmay.rustagi@databricks.com>
2024-02-02 11:20:15 +0100

    restore connection

--- catalog/resource_connection.go
+++ catalog/resource_connection.go
@@ -36,7 +36,7 @@ type ConnectionInfo struct {
 
 var sensitiveOptions = []string{"user", "password", "personalAccessToken", "access_token", "client_secret", "OAuthPvtKey", "GoogleServiceAccountKeyJson"}
 
-func ResourceConnection() common.Resource {
+func ResourceConnection() *schema.Resource {
 	s := common.StructToSchema(ConnectionInfo{},
 		common.NoCustomize)
 	pi := common.NewPairID("metastore_id", "name").Schema(
@@ -56,19 +56,20 @@ func ResourceConnection() common.Resource {
 			}
 			var createConnectionRequest catalog.CreateConnection
 			common.DataToStructPointer(d, s, &createConnectionRequest)
-			conn, err := w.Connections.Create(ctx, createConnectionRequest)
+			_, err = w.Connections.Create(ctx, createConnectionRequest)
 			if err != nil {
 				return err
 			}
 			// Update owner if it is provided
-			if d.Get("owner") != "" {
-				var updateConnectionRequest catalog.UpdateConnection
-				common.DataToStructPointer(d, s, &updateConnectionRequest)
-				updateConnectionRequest.NameArg = updateConnectionRequest.Name
-				conn, err = w.Connections.Update(ctx, updateConnectionRequest)
-				if err != nil {
-					return err
-				}
+			if d.Get("owner") == "" {
+				return nil
+			}
+			var updateConnectionRequest catalog.UpdateConnection
+			common.DataToStructPointer(d, s, &updateConnectionRequest)
+			updateConnectionRequest.NameArg = updateConnectionRequest.Name
+			conn, err := w.Connections.Update(ctx, updateConnectionRequest)
+			if err != nil {
+				return err
 			}
 			d.Set("metastore_id", conn.MetastoreId)
 			pi.Pack(d)
@@ -90,10 +91,6 @@ func ResourceConnection() common.Resource {
 			// We need to preserve original sensitive options as API doesn't return them
 			var cOrig catalog.CreateConnection
 			common.DataToStructPointer(d, s, &cOrig)
-			// If there are no options returned, need to initialize the map
-			if conn.Options == nil {
-				conn.Options = map[string]string{}
-			}
 			for key, element := range cOrig.Options {
 				if slices.Contains(sensitiveOptions, key) {
 					conn.Options[key] = element
@@ -120,16 +117,13 @@ func ResourceConnection() common.Resource {
 
 			if d.HasChange("owner") {
 				_, err = w.Connections.Update(ctx, catalog.UpdateConnection{
+					Name:    updateConnectionRequest.Name,
 					NameArg: updateConnectionRequest.Name,
 					Owner:   updateConnectionRequest.Owner,
 				})
 				if err != nil {
 					return err
 				}
-			}
-
-			if !d.HasChangeExcept("owner") {
-				return nil
 			}
 
 			updateConnectionRequest.Owner = ""
@@ -139,6 +133,7 @@ func ResourceConnection() common.Resource {
 					// Rollback
 					old, new := d.GetChange("owner")
 					_, rollbackErr := w.Connections.Update(ctx, catalog.UpdateConnection{
+						Name:    updateConnectionRequest.Name,
 						NameArg: updateConnectionRequest.Name,
 						Owner:   old.(string),
 					})
@@ -161,5 +156,5 @@ func ResourceConnection() common.Resource {
 			}
 			return w.Connections.DeleteByNameArg(ctx, connName)
 		},
-	}
+	}.ToResource()
 }

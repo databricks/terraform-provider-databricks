3376ba954eea07e10575798e5996c68b9cbf09fc
Donatas Mažionis <d_mazionis@flo.health>
2024-02-05 09:28:17 +0200

    Add acceptance test

--- internal/acceptance/storage_credential_test.go
+++ internal/acceptance/storage_credential_test.go
@@ -32,3 +32,22 @@ func TestUcAccStorageCredential(t *testing.T) {
 		})
 	}
 }
+
+func TestAccStorageCredentialOwner(t *testing.T) {
+	unityAccountLevel(t, step{
+		Template: `
+			resource "databricks_service_principal" "test_acc_storage_credential_owner" {
+				display_name = "test_acc_storage_credential_owner {var.RANDOM}"
+			}
+
+			resource "databricks_storage_credential" "test_acc_storage_credential_owner" {
+				name = "test_acc_storage_credential_owner-{var.RANDOM}"
+				owner = databricks_service_principal.test_acc_storage_credential_owner.application_id
+				metastore_id = "{env.TEST_METASTORE_ID}"
+				aws_iam_role {
+					role_arn = "{env.TEST_METASTORE_DATA_ACCESS_ARN}"
+				}
+			}
+		`,
+	})
+}

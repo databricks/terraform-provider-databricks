5221c30110c125531dfab53f3f72c3bdc05d418f
Cedric <23346008+840@users.noreply.github.com>
2024-02-06 18:34:24 +0100

    Correct indentiation for tests

--- aws/data_aws_unity_catalog_policy_test.go
+++ aws/data_aws_unity_catalog_policy_test.go
@@ -1,6 +1,7 @@
 package aws
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/databricks/terraform-provider-databricks/qa"
@@ -14,56 +15,56 @@ func TestDataAwsUnityCatalogPolicy(t *testing.T) {
 		NonWritable: true,
 		ID:          ".",
 		HCL: `
-		aws_account_id = "123456789098"
-		bucket_name = "databricks-bucket"
-		role_name = "databricks-role"
-		kms_name = "databricks-kms"
-		`,
+        aws_account_id = "123456789098"
+        bucket_name = "databricks-bucket"
+        role_name = "databricks-role"
+        kms_name = "databricks-kms"
+        `,
 	}.Apply(t)
 	assert.NoError(t, err)
-	j := d.Get("json")
-	assert.Equal(t, j, `{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:GetObject",
-        "s3:PutObject",
-        "s3:DeleteObject",
-        "s3:ListBucket",
-        "s3:GetBucketLocation",
-        "s3:GetLifecycleConfiguration",
-        "s3:PutLifecycleConfiguration"
-      ],
-      "Resource": [
-        "arn:aws:s3:::databricks-bucket/*",
-        "arn:aws:s3:::databricks-bucket"
-      ]
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "sts:AssumeRole"
-      ],
-      "Resource": [
-        "arn:aws:iam::123456789098:role/databricks-role"
-      ]
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "kms:Decrypt",
-        "kms:Encrypt",
-        "kms:GenerateDataKey*"
-      ],
-      "Resource": [
-        "arn:aws:kms:databricks-kms"
-      ]
-    }
-  ]
-}`,
-	)
+	j := strings.ReplaceAll(d.Get("json").(string), " ", "")
+	p := strings.ReplaceAll(`{
+          "Version": "2012-10-17",
+          "Statement": [
+            {
+              "Effect": "Allow",
+              "Action": [
+                "s3:GetObject",
+                "s3:PutObject",
+                "s3:DeleteObject",
+                "s3:ListBucket",
+                "s3:GetBucketLocation",
+                "s3:GetLifecycleConfiguration",
+                "s3:PutLifecycleConfiguration"
+              ],
+              "Resource": [
+                "arn:aws:s3:::databricks-bucket/*",
+                "arn:aws:s3:::databricks-bucket"
+              ]
+            },
+            {
+              "Effect": "Allow",
+              "Action": [
+                "sts:AssumeRole"
+              ],
+              "Resource": [
+                "arn:aws:iam::123456789098:role/databricks-role"
+              ]
+            },
+            {
+              "Effect": "Allow",
+              "Action": [
+                "kms:Decrypt",
+                "kms:Encrypt",
+                "kms:GenerateDataKey*"
+              ],
+              "Resource": [
+                "arn:aws:kms:databricks-kms"
+              ]
+            }
+          ]
+        }`, " ", "")
+	assert.EqualValues(t, j, p)
 }
 
 func TestDataAwsUnityCatalogPolicyWithoutKMS(t *testing.T) {
@@ -73,42 +74,42 @@ func TestDataAwsUnityCatalogPolicyWithoutKMS(t *testing.T) {
 		NonWritable: true,
 		ID:          ".",
 		HCL: `
-		aws_account_id = "123456789098"
-		bucket_name = "databricks-bucket"
-		role_name = "databricks-role"
-		`,
+        aws_account_id = "123456789098"
+        bucket_name = "databricks-bucket"
+        role_name = "databricks-role"
+        `,
 	}.Apply(t)
 	assert.NoError(t, err)
-	j := d.Get("json")
-	assert.Equal(t, j, `{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:GetObject",
-        "s3:PutObject",
-        "s3:DeleteObject",
-        "s3:ListBucket",
-        "s3:GetBucketLocation",
-        "s3:GetLifecycleConfiguration",
-        "s3:PutLifecycleConfiguration"
-      ],
-      "Resource": [
-        "arn:aws:s3:::databricks-bucket/*",
-        "arn:aws:s3:::databricks-bucket"
-      ]
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "sts:AssumeRole"
-      ],
-      "Resource": [
-        "arn:aws:iam::123456789098:role/databricks-role"
-      ]
-    }
-  ]
-}`,
-	)
+	j := strings.ReplaceAll(d.Get("json").(string), " ", "")
+	p := strings.ReplaceAll(`{
+          "Version": "2012-10-17",
+          "Statement": [
+            {
+              "Effect": "Allow",
+              "Action": [
+                "s3:GetObject",
+                "s3:PutObject",
+                "s3:DeleteObject",
+                "s3:ListBucket",
+                "s3:GetBucketLocation",
+                "s3:GetLifecycleConfiguration",
+                "s3:PutLifecycleConfiguration"
+              ],
+              "Resource": [
+                "arn:aws:s3:::databricks-bucket/*",
+                "arn:aws:s3:::databricks-bucket"
+              ]
+            },
+            {
+              "Effect": "Allow",
+              "Action": [
+                "sts:AssumeRole"
+              ],
+              "Resource": [
+                "arn:aws:iam::123456789098:role/databricks-role"
+              ]
+            }
+          ]
+        }`, " ", "")
+	assert.EqualValues(t, j, p)
 }

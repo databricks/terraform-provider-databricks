72abbed88d41f42e749c57416fea71fb7b95d2bf
Alex Ott <alexott@gmail.com>
2024-02-12 15:11:24 +0100

    Try to fix integration tests

--- internal/acceptance/lakehouse_monitor_test.go
+++ internal/acceptance/lakehouse_monitor_test.go
@@ -8,7 +8,7 @@ func TestUcAccLakehouseMonitor(t *testing.T) {
 	unityWorkspaceLevel(t, step{
 		Template: `
 			resource "databricks_catalog" "sandbox" {
-				name         = "sandbox{var.RANDOM}"
+				name         = "sandbox${var.RANDOM}"
 				comment      = "this catalog is managed by terraform"
 				properties = {
 					purpose = "testing"
@@ -17,47 +17,38 @@ func TestUcAccLakehouseMonitor(t *testing.T) {
 
 			resource "databricks_schema" "things" {
 				catalog_name = databricks_catalog.sandbox.id
-				name         = "things{var.RANDOM}"
+				name         = "things${var.RANDOM}"
 				comment      = "this database is managed by terraform"
 				properties = {
 					kind = "various"
 				}
 			}
 
-			resource "databricks_table" "mytable" {
+			resource "databricks_sql_table" "mytable" {
 				catalog_name = databricks_catalog.sandbox.id
 				schema_name = databricks_schema.things.name
-				name = "bar"
+				name = "bar${var.RANDOM}"
 				table_type = "MANAGED"
 				data_source_format = "DELTA"
 				
 				column {
-					name      = "model_id"
-					position  = 0
-					type_name = "INT"
-					type_text = "int"
-					type_json = "{\"name\":\"id\",\"type\":\"integer\",\"nullable\":true,\"metadata\":{}}"
+					name = "model_id"
+					type = "int"
 				}
 				column {
-					name      = "timestamp"
-					position  = 1
-					type_name = "INT"
-					type_text = "int"
-					type_json = "{\"name\":\"id\",\"type\":\"integer\",\"nullable\":true,\"metadata\":{}}"
+					name = "timestamp"
+					type = "int"
 				}
 				column {
-					name      = "prediction"
-					position  = 2
-					type_name = "INT"
-					type_text = "int"
-					type_json = "{\"name\":\"id\",\"type\":\"integer\",\"nullable\":true,\"metadata\":{}}"
+					name = "prediction"
+					type = "int"
 				}
 			}
 
 			resource "databricks_lakehouse_monitor" "testMonitor" {
-				table_name = "{databricks_catalog.sandbox.id}.{databricks_schema.things.name}.{databricks_table.myTestTable.name}"
-				assets_dir = "/Shared/provider-test/databricks_lakehouse_monitoring/{databricks_table.myTestTable.name}"
-				output_schema_name = "{databricks_catalog.sandbox.id}.{databricks_schema.things.name}"
+				table_name = databricks_sql_table.mytable.id
+				assets_dir = "/Shared/provider-test/databricks_lakehouse_monitoring/${databricks_sql_table.mytable.name}"
+				output_schema_name = databricks_schema.things.id
 				inference_log  {
 				  granularities = ["1 day"]
 				  timestamp_col = "timestamp"

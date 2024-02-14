acea1bae3fb68166a1f9fea0d8276149fff28f63
Hector Castejon Diaz <hector.castejon@databricks.com>
2024-02-14 16:10:29 +0100

    Revert "Revert "Added `databricks_file` resource (#2495)" (#2900)"
    This reverts commit d9a610582cd3694e165d0711f00639564710cedc.

---
+++ storage/resource_databricks_file.go
@@ -0,0 +1,127 @@
+package storage
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"crypto/md5"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+
+	"github.com/databricks/databricks-sdk-go/service/files"
+	"github.com/databricks/terraform-provider-databricks/common"
+	"github.com/databricks/terraform-provider-databricks/workspace"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func getContentReader(data *schema.ResourceData) (io.ReadCloser, error) {
+	source := data.Get("source").(string)
+	var reader io.ReadCloser
+	var err error
+	if source != "" {
+		reader, err = os.Open(source)
+		if err != nil {
+			return nil, err
+		}
+	}
+	contentBase64 := data.Get("content_base64").(string)
+	if contentBase64 != "" {
+		decodedString, err := base64.StdEncoding.DecodeString(contentBase64)
+		if err != nil {
+			return nil, err
+		}
+		reader = io.NopCloser(bytes.NewReader(decodedString))
+		if err != nil {
+			return nil, err
+		}
+	}
+	return reader, err
+}
+
+func ResourceFile() *schema.Resource {
+	s := workspace.FileContentSchema(map[string]*schema.Schema{
+		"modification_time": {
+			Type:     schema.TypeInt,
+			Computed: true,
+		},
+		"file_size": {
+			Type:     schema.TypeInt,
+			Computed: true,
+		},
+	})
+	return common.Resource{
+		Schema: s,
+		Create: func(ctx context.Context, data *schema.ResourceData, c *common.DatabricksClient) error {
+			w, err := c.WorkspaceClient()
+			if err != nil {
+				return err
+			}
+			path := data.Get("path").(string)
+			reader, err := getContentReader(data)
+			if err != nil {
+				return err
+			}
+			err = w.Files.Upload(ctx, files.UploadRequest{Contents: reader, FilePath: path})
+			if err != nil {
+				return err
+			}
+
+			data.SetId(path)
+			return nil
+		},
+		Read: func(ctx context.Context, data *schema.ResourceData, c *common.DatabricksClient) error {
+			w, err := c.WorkspaceClient()
+			if err != nil {
+				return err
+			}
+
+			path := data.Id()
+			fileInfo, err := w.Files.GetStatus(ctx, files.GetStatusRequest{Path: path})
+			if err != nil {
+				return err
+			}
+			data.Set("modification_time", fileInfo.ModificationTime)
+			data.Set("file_size", fileInfo.FileSize)
+
+			downloadResponse, err := w.Files.Download(ctx, files.DownloadRequest{FilePath: path})
+			if err != nil {
+				return err
+			}
+
+			dataReadCloser := downloadResponse.Contents
+			defer dataReadCloser.Close()
+			reader := bufio.NewReader(dataReadCloser)
+			dataByte, err := ioutil.ReadAll(reader)
+			if err != nil {
+				return err
+			}
+			data.Set("md5", fmt.Sprintf("%x", md5.Sum(dataByte)))
+			return common.StructToData(fileInfo, s, data)
+		},
+		Update: func(ctx context.Context, data *schema.ResourceData, c *common.DatabricksClient) error {
+			w, err := c.WorkspaceClient()
+			if err != nil {
+				return err
+			}
+			path := data.Id()
+			reader, err := getContentReader(data)
+			if err != nil {
+				return err
+			}
+			err = w.Files.Upload(ctx, files.UploadRequest{Contents: reader, FilePath: path})
+			return err
+		},
+		Delete: func(ctx context.Context, data *schema.ResourceData, c *common.DatabricksClient) error {
+			w, err := c.WorkspaceClient()
+			if err != nil {
+				return err
+			}
+			path := data.Id()
+			err = w.Files.Delete(ctx, files.DeleteFileRequest{FilePath: path})
+			return err
+		},
+	}.ToResource()
+}
---
+++ storage/resource_databricks_file_test.go
@@ -0,0 +1,271 @@
+package storage
+
+import (
+	"bytes"
+	"encoding/base64"
+	"io"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/databricks/databricks-sdk-go/apierr"
+	ws_api "github.com/databricks/databricks-sdk-go/service/files"
+	"github.com/databricks/terraform-provider-databricks/qa"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestResourceFileCreate(t *testing.T) {
+	path := "/Volumes/CatalogName/SchemaName/VolumeName/fileName"
+	decodedString, err := base64.StdEncoding.DecodeString("YWJjCg==")
+	assert.NoError(t, err)
+	reader := io.NopCloser(bytes.NewReader(decodedString))
+	d, err := qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   http.MethodPut,
+				Resource: "/api/2.0/fs/files/Volumes/CatalogName/SchemaName/VolumeName/fileName",
+				Status:   http.StatusOK,
+				Response: nil,
+			},
+			{
+				Method:   http.MethodGet,
+				Resource: "/api/2.0/fs/get-status?path=%2FVolumes%2FCatalogName%2FSchemaName%2FVolumeName%2FfileName",
+				Response: FileInfo{
+					Path: path,
+				},
+			},
+			{
+				Method:   http.MethodGet,
+				Resource: "/api/2.0/fs/files/Volumes/CatalogName/SchemaName/VolumeName/fileName?",
+				Response: ws_api.DownloadResponse{
+					Contents: reader,
+				},
+			},
+		},
+		Resource: ResourceFile(),
+		State: map[string]any{
+			"content_base64": "YWJjCg==",
+			"path":           path,
+		},
+		Create: true,
+	}.Apply(t)
+	assert.NoError(t, err)
+	assert.Equal(t, path, d.Id())
+}
+
+func TestResourceFileCreateSource(t *testing.T) {
+	path := "/Volumes/CatalogName/SchemaName/VolumeName/fileName"
+	source := "testdata/tf-test-python.py"
+	reader, err := os.Open(source)
+	assert.NoError(t, err)
+	d, err := qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   http.MethodPut,
+				Resource: "/api/2.0/fs/files/Volumes/CatalogName/SchemaName/VolumeName/fileName",
+				Status:   http.StatusOK,
+				Response: nil,
+			},
+			{
+				Method:   http.MethodGet,
+				Resource: "/api/2.0/fs/get-status?path=%2FVolumes%2FCatalogName%2FSchemaName%2FVolumeName%2FfileName",
+				Response: FileInfo{
+					Path: path,
+				},
+			},
+			{
+				Method:   http.MethodGet,
+				Resource: "/api/2.0/fs/files/Volumes/CatalogName/SchemaName/VolumeName/fileName?",
+				Response: ws_api.DownloadResponse{
+					Contents: reader,
+				},
+			},
+		},
+		Resource: ResourceFile(),
+		State: map[string]any{
+			"source": source,
+			"path":   path,
+		},
+		Create: true,
+	}.Apply(t)
+	assert.NoError(t, err)
+	assert.Equal(t, path, d.Id())
+}
+
+func TestResourceFileCreate_Error(t *testing.T) {
+	path := "/Volumes/CatalogName/SchemaName/VolumeName/fileName"
+	d, err := qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   http.MethodPut,
+				Resource: "/api/2.0/fs/files/Volumes/CatalogName/SchemaName/VolumeName/fileName",
+				Response: apierr.APIErrorBody{
+					ErrorCode: "INVALID_REQUEST",
+					Message:   "Internal error happened",
+				},
+				Status: 400,
+			},
+		},
+		Resource: ResourceFile(),
+		State: map[string]any{
+			"content_base64": "YWJjCg==",
+			"path":           path,
+		},
+		Create: true,
+	}.Apply(t)
+	qa.AssertErrorStartsWith(t, err, "Internal error happened")
+	assert.Equal(t, "", d.Id(), "Id should be empty for error creates")
+}
+
+func TestResourceFileRead(t *testing.T) {
+	path := "/Volumes/CatalogName/SchemaName/VolumeName/fileName"
+	d, err := qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   http.MethodGet,
+				Resource: "/api/2.0/fs/get-status?path=%2FVolumes%2FCatalogName%2FSchemaName%2FVolumeName%2FfileName",
+				Response: ws_api.FileInfo{
+					Path: path,
+				},
+			},
+			{
+				Method:   http.MethodGet,
+				Resource: "/api/2.0/fs/files/Volumes/CatalogName/SchemaName/VolumeName/fileName?",
+				Response: ws_api.DownloadResponse{},
+			},
+		},
+		Resource: ResourceFile(),
+		Read:     true,
+		New:      true,
+		ID:       path,
+	}.Apply(t)
+	assert.NoError(t, err)
+	assert.Equal(t, path, d.Id())
+	assert.Equal(t, path, d.Get("path"))
+}
+
+func TestResourceFileRead_NotFound(t *testing.T) {
+	path := "/Volumes/CatalogName/SchemaName/VolumeName/fileName"
+	qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{ // read log output for correct url...
+				Method:   "GET",
+				Resource: "/api/2.0/fs/get-status?path=%2FVolumes%2FCatalogName%2FSchemaName%2FVolumeName%2FfileName",
+				Response: apierr.APIErrorBody{
+					ErrorCode: "NOT_FOUND",
+					Message:   "File not found",
+				},
+				Status: 404,
+			},
+		},
+		Resource: ResourceFile(),
+		Read:     true,
+		Removed:  true,
+		ID:       path,
+	}.ApplyNoError(t)
+}
+
+func TestResourceFileRead_Error(t *testing.T) {
+	path := "/Volumes/CatalogName/SchemaName/VolumeName/fileName"
+	d, err := qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "GET",
+				Resource: "/api/2.0/fs/get-status?path=%2FVolumes%2FCatalogName%2FSchemaName%2FVolumeName%2FfileName",
+				Response: apierr.APIErrorBody{
+					ErrorCode: "INVALID_REQUEST",
+					Message:   "Internal error happened",
+				},
+				Status: 400,
+			},
+		},
+		Resource: ResourceFile(),
+		Read:     true,
+		ID:       path,
+	}.Apply(t)
+	qa.AssertErrorStartsWith(t, err, "Internal error happened")
+	assert.Equal(t, path, d.Id(), "Id should not be empty for error reads")
+}
+
+func TestResourceFileDelete(t *testing.T) {
+	path := "/Volumes/CatalogName/SchemaName/VolumeName/fileName"
+	d, err := qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   http.MethodDelete,
+				Resource: "/api/2.0/fs/files/Volumes/CatalogName/SchemaName/VolumeName/fileName?",
+				Response: nil,
+			},
+		},
+		Resource: ResourceFile(),
+		Delete:   true,
+		New:      true,
+		ID:       path,
+	}.Apply(t)
+	assert.NoError(t, err)
+	assert.Equal(t, path, d.Id())
+}
+
+func TestResourceFileDelete_Error(t *testing.T) {
+	path := "/Volumes/CatalogName/SchemaName/VolumeName/fileName"
+	d, err := qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "DELETE",
+				Resource: "/api/2.0/fs/files/Volumes/CatalogName/SchemaName/VolumeName/fileName?",
+				Response: apierr.APIErrorBody{
+					ErrorCode: "INVALID_REQUEST",
+					Message:   "Internal error happened",
+				},
+				Status: 400,
+			},
+		},
+		Resource: ResourceFile(),
+		Delete:   true,
+		ID:       path,
+	}.Apply(t)
+	qa.AssertErrorStartsWith(t, err, "Internal error happened")
+	assert.Equal(t, path, d.Id())
+}
+
+func TestResourceFileUpdate(t *testing.T) {
+	path := "/Volumes/CatalogName/SchemaName/VolumeName/fileName"
+	qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "PUT",
+				Resource: "/api/2.0/fs/files/Volumes/CatalogName/SchemaName/VolumeName/fileName",
+				Status:   http.StatusOK,
+			},
+			{
+				Method:   http.MethodGet,
+				Resource: "/api/2.0/fs/get-status?path=%2FVolumes%2FCatalogName%2FSchemaName%2FVolumeName%2FfileName",
+				Response: FileInfo{},
+			},
+			{
+				Method:   http.MethodGet,
+				Resource: "/api/2.0/fs/files/Volumes/CatalogName/SchemaName/VolumeName/fileName?",
+				Response: ws_api.DownloadResponse{},
+			},
+		},
+		Resource: ResourceFile(),
+		State: map[string]any{
+			"content_base64": "YWJjCg==",
+			"path":           path,
+		},
+		ID:          path,
+		RequiresNew: true,
+		Update:      true,
+	}.ApplyNoError(t)
+}
+
+func TestResourceFileBadPrefix(t *testing.T) {
+	qa.ResourceFixture{
+		Resource: ResourceFile(),
+		Create:   true,
+		HCL: `
+		path = "Volumes/CatalogName/SchemaName/VolumeName/fileName"
+		content_base64 = "YWJjCg=="
+		`,
+	}.ExpectError(t, "invalid config supplied. [path] Path should start with /Volumes")
+}

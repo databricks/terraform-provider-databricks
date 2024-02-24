7aea1f5bffc297b45eb646e7b0e0c0dfd6207a05
Alex Ott <alexott@gmail.com>
2024-02-24 11:44:25 +0100

    Fix & refactor integration test

--- internal/acceptance/lakehouse_monitor_test.go
+++ internal/acceptance/lakehouse_monitor_test.go
@@ -4,47 +4,50 @@ import (
 	"testing"
 )
 
+var commonPartLakehouseMonitoring = `resource "databricks_catalog" "sandbox" {
+	name         = "sandbox${var.RANDOM}"
+	comment      = "this catalog is managed by terraform"
+	properties = {
+		purpose = "testing"
+	}
+	force_destroy = true
+}
+
+resource "databricks_schema" "things" {
+	catalog_name = databricks_catalog.sandbox.id
+	name         = "things${var.RANDOM}"
+	comment      = "this database is managed by terraform"
+	properties = {
+		kind = "various"
+	}
+}
+
+resource "databricks_sql_table" "myInferenceTable" {
+	catalog_name = databricks_catalog.sandbox.id
+	schema_name = databricks_schema.things.name
+	name = "bar${var.RANDOM}_inference"
+	table_type = "MANAGED"
+	data_source_format = "DELTA"
+	
+	column {
+		name = "model_id"
+		type = "int"
+	}
+	column {
+		name = "timestamp"
+		type = "int"
+	}
+	column {
+		name = "prediction"
+		type = "int"
+	}
+}
+
+`
+
 func TestUcAccLakehouseMonitor(t *testing.T) {
 	unityWorkspaceLevel(t, step{
-		Template: `
-			resource "databricks_catalog" "sandbox" {
-				name         = "sandbox${var.RANDOM}"
-				comment      = "this catalog is managed by terraform"
-				properties = {
-					purpose = "testing"
-				}
-				force_destroy = true
-			}
-
-			resource "databricks_schema" "things" {
-				catalog_name = databricks_catalog.sandbox.id
-				name         = "things${var.RANDOM}"
-				comment      = "this database is managed by terraform"
-				properties = {
-					kind = "various"
-				}
-			}
-
-			resource "databricks_sql_table" "myInferenceTable" {
-				catalog_name = databricks_catalog.sandbox.id
-				schema_name = databricks_schema.things.name
-				name = "bar${var.RANDOM}_inference"
-				table_type = "MANAGED"
-				data_source_format = "DELTA"
-				
-				column {
-					name = "model_id"
-					type = "int"
-				}
-				column {
-					name = "timestamp"
-					type = "int"
-				}
-				column {
-					name = "prediction"
-					type = "int"
-				}
-			}
+		Template: commonPartLakehouseMonitoring + `
 
 			resource "databricks_lakehouse_monitor" "testMonitorInference" {
 				table_name = databricks_sql_table.myInferenceTable.id
@@ -108,46 +111,7 @@ func TestUcAccLakehouseMonitor(t *testing.T) {
 
 func TestUcAccUpdateLakehouseMonitor(t *testing.T) {
 	unityWorkspaceLevel(t, step{
-		Template: `
-			resource "databricks_catalog" "sandbox" {
-				name         = "sandbox${var.RANDOM}"
-				comment      = "this catalog is managed by terraform"
-				properties = {
-					purpose = "testing"
-				}
-				force_destroy = true
-			}
-
-			resource "databricks_schema" "things" {
-				catalog_name = databricks_catalog.sandbox.id
-				name         = "things${var.RANDOM}"
-				comment      = "this database is managed by terraform"
-				properties = {
-					kind = "various"
-				}
-			}
-
-			resource "databricks_sql_table" "myInferenceTable" {
-				catalog_name = databricks_catalog.sandbox.id
-				schema_name = databricks_schema.things.name
-				name = "bar${var.RANDOM}_inference"
-				table_type = "MANAGED"
-				data_source_format = "DELTA"
-				
-				column {
-					name = "model_id"
-					type = "int"
-				}
-				column {
-					name = "timestamp"
-					type = "int"
-				}
-				column {
-					name = "prediction"
-					type = "int"
-				}
-			}
-
+		Template: commonPartLakehouseMonitoring + `
 			resource "databricks_lakehouse_monitor" "testMonitorInference" {
 				table_name = databricks_sql_table.myInferenceTable.id
 				assets_dir = "/Shared/provider-test/databricks_lakehouse_monitoring/${databricks_sql_table.myInferenceTable.name}"
@@ -162,7 +126,7 @@ func TestUcAccUpdateLakehouseMonitor(t *testing.T) {
 			}
 		`,
 	}, step{
-		Template: `
+		Template: commonPartLakehouseMonitoring + `
 		resource "databricks_lakehouse_monitor" "testMonitorInference" {
 			table_name = databricks_sql_table.myInferenceTable.id
 			assets_dir = "/Shared/provider-test/databricks_lakehouse_monitoring/${databricks_sql_table.myInferenceTable.name}"

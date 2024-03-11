2be2d3795c9cbb4b128278bb0a391656ffc8db87
Alvaro Queiroz <ext.asilva@riotgames.com>
2024-03-11 17:43:52 -0300

    remove empty new lines to test for state change

--- catalog/resource_sql_table.go
+++ catalog/resource_sql_table.go
@@ -394,7 +394,7 @@ func ResourceSqlTable() common.Resource {
 				return strings.EqualFold(strings.ToLower(old), strings.ToLower(new))
 			}
 			s["storage_location"].DiffSuppressFunc = ucDirectoryPathSlashAndEmptySuppressDiff
-			s["view_definition"].DiffSuppressFunc = common.SuppressDiffWhitespaceChange
+			s["view_definition"].DiffSuppressFunc = common.SuppressDiffWhitespaceAndEmptyLines
 
 			s["cluster_id"].ConflictsWith = []string{"warehouse_id"}
 			s["warehouse_id"].ConflictsWith = []string{"cluster_id"}

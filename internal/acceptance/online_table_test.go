c1bdd715ec0e0a3dcef3cd444af22a3633177579
Alex Ott <alexott@gmail.com>
2024-03-16 14:47:04 +0100

    Fix test one more time

--- internal/acceptance/online_table_test.go
+++ internal/acceptance/online_table_test.go
@@ -6,12 +6,8 @@ import (
 )
 
 var onlineTableHcl = `
-locals {
-	STICKY_RANDOM = replace(var.STICKY_RANDOM, "/[^a-zA-Z0-9_]/", "_")
-}
-
 resource "databricks_catalog" "sandbox" {
-	name    = "sandbox_${var.STICKY_RANDOM}"
+	name    = "sandbox_{var.STICKY_RANDOM}"
 	comment = "this catalog is managed by terraform"
 	properties = {
 	  purpose = "testing"
@@ -21,7 +17,7 @@ resource "databricks_catalog" "sandbox" {
   
   resource "databricks_schema" "things" {
 	catalog_name = databricks_catalog.sandbox.id
-	name         = "things_${local.STICKY_RANDOM}"
+	name         = "things_{var.STICKY_RANDOM}"
 	comment      = "this database is managed by terraform"
 	properties = {
 	  kind = "various"
@@ -30,7 +26,7 @@ resource "databricks_catalog" "sandbox" {
   }
   
   resource "databricks_sql_endpoint" "this" {
-	name             = "tf-${local.STICKY_RANDOM}"
+	name             = "tf-{var.STICKY_RANDOM}"
 	cluster_size     = "2X-Small"
 	max_num_clusters = 1
 	warehouse_type   = "PRO"
@@ -40,7 +36,7 @@ resource "databricks_catalog" "sandbox" {
   resource "databricks_sql_table" "table" {
 	catalog_name       = databricks_catalog.sandbox.id
 	schema_name        = databricks_schema.things.name
-	name               = "ot_src_${local.STICKY_RANDOM}"
+	name               = "ot_src_{var.STICKY_RANDOM}"
 	table_type         = "MANAGED"
 	data_source_format = "DELTA"
 	warehouse_id       = databricks_sql_endpoint.this.id

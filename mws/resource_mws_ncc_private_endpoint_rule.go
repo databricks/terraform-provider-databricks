3fff5b45eb4e68aa4b465c13b83fd4693238d4e8
Vuong <vuong.nguyen@databricks.com>
2024-03-25 15:32:13 +0000

    add `mws_ncc_private_endpoint_rule`

--- mws/resource_mws_ncc_private_endpoint_rule.go
+++ mws/resource_mws_ncc_private_endpoint_rule.go
@@ -7,41 +7,39 @@ import (
 	"github.com/databricks/terraform-provider-databricks/common"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
 
-func ResourceMwsNetworkConnectivityConfig() common.Resource {
-	s := common.StructToSchema(settings.NetworkConnectivityConfiguration{}, func(m map[string]*schema.Schema) map[string]*schema.Schema {
-		common.CustomizeSchemaPath(m, "name").SetValidateFunc(validation.StringLenBetween(3, 30))
-		for _, p := range []string{"name", "region"} {
+func ResourceMwsNccPrivateEndpointRule() common.Resource {
+	s := common.StructToSchema(settings.NccAzurePrivateEndpointRule{}, func(m map[string]*schema.Schema) map[string]*schema.Schema {
+		for _, p := range []string{"network_connectivity_config_id", "group_id", "resource_id"} {
 			common.CustomizeSchemaPath(m, p).SetRequired()
 			common.CustomizeSchemaPath(m, p).SetForceNew()
 		}
-		for _, p := range []string{"account_id", "network_connectivity_config_id", "creation_time", "updated_time"} {
+		for _, p := range []string{"rule_id", "endpoint_name", "connection_state"} {
 			common.CustomizeSchemaPath(m, p).SetComputed()
 		}
 		return m
 	})
-	p := common.NewPairSeparatedID("account_id", "network_connectivity_config_id", "/")
+	p := common.NewPairSeparatedID("network_connectivity_config_id", "rule_id", "/")
 	return common.Resource{
 		Schema: s,
 		Create: func(ctx context.Context, d *schema.ResourceData, c *common.DatabricksClient) error {
-			var create settings.CreateNetworkConnectivityConfigRequest
+			var create settings.CreatePrivateEndpointRuleRequest
 			common.DataToStructPointer(d, s, &create)
 			acc, err := c.AccountClient()
 			if err != nil {
 				return err
 			}
-			ncc, err := acc.NetworkConnectivity.CreateNetworkConnectivityConfiguration(ctx, create)
+			rule, err := acc.NetworkConnectivity.CreatePrivateEndpointRule(ctx, create)
 			if err != nil {
 				return err
 			}
-			common.StructToData(ncc, s, d)
+			common.StructToData(rule, s, d)
 			p.Pack(d)
 			return nil
 		},
 		Read: func(ctx context.Context, d *schema.ResourceData, c *common.DatabricksClient) error {
-			_, nccId, err := p.Unpack(d)
+			nccId, ruleId, err := p.Unpack(d)
 			if err != nil {
 				return err
 			}
@@ -49,14 +47,14 @@ func ResourceMwsNetworkConnectivityConfig() common.Resource {
 			if err != nil {
 				return err
 			}
-			ncc, err := acc.NetworkConnectivity.GetNetworkConnectivityConfigurationByNetworkConnectivityConfigId(ctx, nccId)
+			rule, err := acc.NetworkConnectivity.GetPrivateEndpointRuleByNetworkConnectivityConfigIdAndPrivateEndpointRuleId(ctx, nccId, ruleId)
 			if err != nil {
 				return err
 			}
-			return common.StructToData(ncc, s, d)
+			return common.StructToData(rule, s, d)
 		},
 		Delete: func(ctx context.Context, d *schema.ResourceData, c *common.DatabricksClient) error {
-			_, nccId, err := p.Unpack(d)
+			nccId, ruleId, err := p.Unpack(d)
 			if err != nil {
 				return err
 			}
@@ -64,7 +62,8 @@ func ResourceMwsNetworkConnectivityConfig() common.Resource {
 			if err != nil {
 				return err
 			}
-			return acc.NetworkConnectivity.DeleteNetworkConnectivityConfigurationByNetworkConnectivityConfigId(ctx, nccId)
+			_, err = acc.NetworkConnectivity.DeletePrivateEndpointRuleByNetworkConnectivityConfigIdAndPrivateEndpointRuleId(ctx, nccId, ruleId)
+			return err
 		},
 	}
 }

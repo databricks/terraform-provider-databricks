7c664927d8bfcaf6d762c86810f7e4410624e88e
Vuong <vuong.nguyen@databricks.com>
2024-03-25 16:01:55 +0000

    add provider

--- provider/provider.go
+++ provider/provider.go
@@ -142,6 +142,7 @@ func DatabricksProvider() *schema.Provider {
 			"databricks_mws_credentials":                 mws.ResourceMwsCredentials().ToResource(),
 			"databricks_mws_log_delivery":                mws.ResourceMwsLogDelivery().ToResource(),
 			"databricks_mws_ncc_binding":                 mws.ResourceMwsNccBinding().ToResource(),
+			"databricks_mws_ncc_private_endpoint_rule":   mws.ResourceMwsNccPrivateEndpointRule().ToResource(),
 			"databricks_mws_networks":                    mws.ResourceMwsNetworks().ToResource(),
 			"databricks_mws_network_connectivity_config": mws.ResourceMwsNetworkConnectivityConfig().ToResource(),
 			"databricks_mws_permission_assignment":       mws.ResourceMwsPermissionAssignment().ToResource(),

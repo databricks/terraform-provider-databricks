28842c5fb31f71de3be3a7f223a9bcec2235c065
Alex Ott <alexott@gmail.com>
2024-04-01 11:01:20 +0200

    Add lookup by name or region to `databricks_metastore` data source
    Sometimes it would be easier to fetch metastore information by a workspace region or by a
    metastore name compared to fetching metastore information by ID.  This PR adds ability to
    specify either `name` or `region` to perform a metastore lookup.

--- catalog/data_metastore.go
+++ catalog/data_metastore.go
@@ -2,6 +2,8 @@ package catalog
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	"github.com/databricks/databricks-sdk-go"
 	"github.com/databricks/databricks-sdk-go/service/catalog"
@@ -10,15 +12,54 @@ import (
 
 func DataSourceMetastore() common.Resource {
 	type AccountMetastoreByID struct {
-		Id        string                 `json:"metastore_id"`
+		Id        string                 `json:"metastore_id,omitempty" tf:"computed"`
+		Name      string                 `json:"name,omitempty" tf:"computed"`
+		Region    string                 `json:"region,omitempty" tf:"computed"`
 		Metastore *catalog.MetastoreInfo `json:"metastore_info,omitempty" tf:"computed" `
 	}
 	return common.AccountData(func(ctx context.Context, data *AccountMetastoreByID, acc *databricks.AccountClient) error {
-		metastore, err := acc.Metastores.GetByMetastoreId(ctx, data.Id)
-		if err != nil {
-			return err
+		if data.Id == "" && data.Name == "" && data.Region == "" {
+			return fmt.Errorf("one of metastore_id, name or region must be provided")
 		}
-		data.Metastore = metastore.MetastoreInfo
+		if (data.Id != "" && data.Name != "") || (data.Region != "" && data.Id != "") || (data.Region != "" && data.Name != "") {
+			return fmt.Errorf("only one of metastore_id, name or region must be provided")
+		}
+		if data.Id != "" {
+			minfo, err := acc.Metastores.GetByMetastoreId(ctx, data.Id)
+			if err != nil {
+				return err
+			}
+			data.Metastore = minfo.MetastoreInfo
+		} else {
+			metastores, err := acc.Metastores.ListAll(ctx)
+			if err != nil {
+				return err
+			}
+			minfos := []catalog.MetastoreInfo{}
+			if data.Name != "" {
+				for _, v := range metastores {
+					if v.Name == data.Name {
+						minfos = append(minfos, v)
+					}
+				}
+			} else {
+				for _, v := range metastores {
+					if strings.EqualFold(v.Region, data.Region) {
+						minfos = append(minfos, v)
+					}
+				}
+			}
+			if len(minfos) == 0 {
+				return fmt.Errorf("a metastore with name '%s' or in region '%s' is not found", data.Name, data.Region)
+			}
+			if len(minfos) > 1 {
+				return fmt.Errorf("there are %d metastores with name '%s' in region '%s'", len(minfos), data.Name, data.Region)
+			}
+			data.Metastore = &minfos[0]
+		}
+		data.Id = data.Metastore.MetastoreId
+		data.Name = data.Metastore.Name
+		data.Region = data.Metastore.Region
 		return nil
 	})
 }

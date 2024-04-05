5b886238e8861b492e0918bde09a11e754a8e88c
Vuong <vuong.nguyen@databricks.com>
2024-04-05 19:28:13 +0100

    correct storage account

--- internal/acceptance/mws_network_connectivity_config_test.go
+++ internal/acceptance/mws_network_connectivity_config_test.go
@@ -19,7 +19,7 @@ func TestMwsAccNetworkConnectivityConfig(t *testing.T) {
 
 			resource "databricks_mws_ncc_private_endpoint_rule" "this" {
 				network_connectivity_config_id = databricks_mws_network_connectivity_config.this.id
-				resource_id = "/subscriptions/653bb673-1234-abcd-a90b-d064d5d53ca4/resourcegroups/example-resource-group/providers/Microsoft.Storage/storageAccounts/examplesa"
+				resource_id = "/subscriptions/2a5a4578-9ca9-47e2-ba46-f6ee6cc731f2/resourceGroups/deco-prod-azure-eastus2-rg/providers/Microsoft.Storage/storageAccounts/decotestprodunity"
 				group_id = "blob"
 			}
 			`,
@@ -33,7 +33,7 @@ func TestMwsAccNetworkConnectivityConfig(t *testing.T) {
 
 			resource "databricks_mws_ncc_private_endpoint_rule" "this" {
 				network_connectivity_config_id = databricks_mws_network_connectivity_config.this.id
-				resource_id = "/subscriptions/653bb673-1234-abcd-a90b-d064d5d53ca4/resourcegroups/example-resource-group/providers/Microsoft.Storage/storageAccounts/examplesa"
+				resource_id = "/subscriptions/2a5a4578-9ca9-47e2-ba46-f6ee6cc731f2/resourceGroups/deco-prod-azure-eastus2-rg/providers/Microsoft.Storage/storageAccounts/decotestprodunity"
 				group_id = "blob"
 			}
 			`,

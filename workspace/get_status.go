005f7f42bcfb67a9359af0c015cbd42379204685
Miles Yucht <miles@databricks.com>
2024-05-06 19:05:09 +0200

    debug log

--- workspace/get_status.go
+++ workspace/get_status.go
@@ -2,6 +2,7 @@ package workspace
 
 import (
 	"context"
+	"log"
 	"regexp"
 
 	"github.com/databricks/databricks-sdk-go"
@@ -14,7 +15,11 @@ var timeoutRegex = regexp.MustCompile(`request timed out after .* of inactivity`
 func robustGetStatus(ctx context.Context, w *databricks.WorkspaceClient, path string) (status *workspace.ObjectInfo, err error) {
 	r := retries.New[workspace.ObjectInfo](retries.WithRetryFunc(func(err error) bool {
 		msg := err.Error()
-		return timeoutRegex.MatchString(msg)
+		isTimeout := timeoutRegex.MatchString(msg)
+		if isTimeout {
+			log.Printf("[DEBUG] Retrying due to timeout: %s", msg)
+		}
+		return isTimeout
 	}))
 	return r.Run(ctx, func(ctx context.Context) (*workspace.ObjectInfo, error) {
 		return w.Workspace.GetStatusByPath(ctx, path)

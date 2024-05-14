db539245b76c44968cb4f5184323f2d7e4078ded
Alex Ott <alexey.ott@databricks.com>
2024-05-14 20:59:52 +0200

    Fix acceptance test for `databricks_table` data source (#3578)
    * Fix acceptance test for `databricks_table` data source
    
    * Another fix

--- internal/acceptance/data_table_test.go
+++ internal/acceptance/data_table_test.go
@@ -34,7 +34,7 @@ func TestUcAccDataSourceTable(t *testing.T) {
 			}
 		}
 		
-		resource "databricks_table" "mytable" {
+		resource "databricks_sql_table" "mytable" {
 			catalog_name = databricks_catalog.sandbox.id
 			schema_name = databricks_schema.things.name
 			name = "bar"
@@ -42,18 +42,15 @@ func TestUcAccDataSourceTable(t *testing.T) {
 			data_source_format = "DELTA"
 			
 			column {
-				name      = "id"
-				position  = 0
-				type_name = "INT"
-				type_text = "int"
-				type_json = "{\"name\":\"id\",\"type\":\"integer\",\"nullable\":true,\"metadata\":{}}"
+				name = "id"
+				type = "int"
 			}
 		}		
 
 		data "databricks_table" "this" {
-			name = databricks_table.mytable.full_name
+			name = databricks_sql_table.mytable.id
 			depends_on = [
-				databricks_table.mytable,
+				databricks_sql_table.mytable,
 			]
 		}
 		`,

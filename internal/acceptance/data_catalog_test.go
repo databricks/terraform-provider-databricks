ab5d2c0d2a01285b621f1432543dcab43dc140c6
Alex Ott <alexott@gmail.com>
2024-05-14 13:41:15 +0200

    Add `databricks_catalog` data source
    It could be used to retrieve information about specific catalogs inside UC metastore.

---
+++ internal/acceptance/data_catalog_test.go
@@ -0,0 +1,34 @@
+package acceptance
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/stretchr/testify/require"
+)
+
+func TestUcAccDataSourceTable(t *testing.T) {
+	unityWorkspaceLevel(t, step{
+		Template: `
+		resource "databricks_catalog" "sandbox" {
+			name         = "sandbox{var.RANDOM}"
+			comment      = "this catalog is managed by terraform"
+			properties = {
+				purpose = "testing"
+			}
+		}
+		
+		data "databricks_catalog" "this" {
+			name = databricks_catalog.name
+			depends_on = [
+				databricks_catalog.sandbox,
+			]
+		}
+		`,
+		Check: func(s *terraform.State) error {
+			_, ok := s.Modules[0].Resources["data.databricks_catalog.this"]
+			require.True(t, ok, "data.databricks_catalog.this has to be there")
+			return nil
+		},
+	})
+}

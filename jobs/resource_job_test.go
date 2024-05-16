d4e5a5e6fd56b333b348530324fa3bd7032204dd
Miles Yucht <miles@databricks.com>
2024-05-16 14:46:35 +0200

    work

--- jobs/resource_job_test.go
+++ jobs/resource_job_test.go
@@ -1381,7 +1381,7 @@ func TestResourceJobCreate_Trigger_TableUpdateCreate(t *testing.T) {
 	}.Apply(t)
 }
 
-func TestResourceJobCreate_ControlRunState_ContinuousUpdateRunNow(t *testing.T) {
+func TestResourceJobUpdate_ControlRunState_ContinuousUpdateRunNow(t *testing.T) {
 	qa.ResourceFixture{
 		Update:   true,
 		ID:       "789",
@@ -1422,6 +1422,64 @@ func TestResourceJobCreate_ControlRunState_ContinuousUpdateRunNow(t *testing.T) 
 					JobID: 789,
 				},
 				Response: JobRun{},
+			},
+		},
+		HCL: `
+		continuous {
+			pause_status = "UNPAUSED"
+		}
+		control_run_state = true
+		max_concurrent_runs = 1
+		name = "Test"
+		`,
+	}.Apply(t)
+}
+
+func TestResourceJobUpdate_ControlRunState_ContinuousUpdateRunNowFailsWith409(t *testing.T) {
+	qa.ResourceFixture{
+		Update:   true,
+		ID:       "789",
+		Resource: ResourceJob(),
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "POST",
+				Resource: "/api/2.0/jobs/reset",
+				ExpectedRequest: UpdateJobRequest{
+					JobID: 789,
+					NewSettings: &JobSettings{
+						MaxConcurrentRuns: 1,
+						Name:              "Test",
+						Continuous: &ContinuousConf{
+							PauseStatus: "UNPAUSED",
+						},
+					},
+				},
+			},
+			{
+				Method:   "GET",
+				Resource: "/api/2.0/jobs/get?job_id=789",
+				Response: Job{
+					JobID: 789,
+					Settings: &JobSettings{
+						MaxConcurrentRuns: 1,
+						Name:              "Test",
+						Continuous: &ContinuousConf{
+							PauseStatus: "UNPAUSED",
+						},
+					},
+				},
+			},
+			{
+				Method:   "POST",
+				Resource: "/api/2.0/jobs/run-now",
+				ExpectedRequest: RunParameters{
+					JobID: 789,
+				},
+				Status: 409,
+				Response: apierr.APIErrorBody{
+					ErrorCode: "CONFLICT",
+					Message: "A concurrent request to run the continuous job is already in progress. Please wait for it to complete before issuing a new request.",
+				},
 			},
 		},
 		HCL: `

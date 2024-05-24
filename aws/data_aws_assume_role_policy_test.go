1980f46de4bdd23de1351ab0f171502625f20b71
Johannes Unterstein <johannes@bobsled.co>
2024-05-24 12:51:47 +0200

    Enhance DataAwsAssumeRolePolicy to allow the role to assume itself

--- aws/data_aws_assume_role_policy_test.go
+++ aws/data_aws_assume_role_policy_test.go
@@ -19,3 +19,19 @@ func TestDataAwsAssumeRolePolicy(t *testing.T) {
 	j := d.Get("json")
 	assert.Lenf(t, j, 299, "Strange length for policy: %s", j)
 }
+
+func TestDataAwsAssumeRolePolicySelfAssume(t *testing.T) {
+	d, err := qa.ResourceFixture{
+		Read:        true,
+		Resource:    DataAwsAssumeRolePolicy(),
+		NonWritable: true,
+		ID:          ".",
+		HCL: `
+		external_id = "abc"
+		predicted_role_arn = "arn:aws:iam::aws-account:role/role-name"
+		`,
+	}.Apply(t)
+	assert.NoError(t, err)
+	j := d.Get("json")
+	assert.Lenf(t, j, 628, "Strange length for policy: %s", j)
+}

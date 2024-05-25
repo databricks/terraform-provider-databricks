56d34c75ac4783b2117b1c1c7c38e3c444bdb7e7
Arpit Jasapara <arpit.jasapara@databricks.com>
2024-05-24 22:14:53 -0700

    Add computed and optional

--- serving/resource_model_serving.go
+++ serving/resource_model_serving.go
@@ -41,6 +41,10 @@ func ResourceModelServing() common.Resource {
 			common.MustSchemaPath(m, "config", "served_entities", "scale_to_zero_enabled").Optional = true
 			common.MustSchemaPath(m, "config", "served_entities", "scale_to_zero_enabled").Default = false
 			common.MustSchemaPath(m, "config", "served_entities", "name").Computed = true
+			common.MustSchemaPath(m, "config", "served_entities", "workload_size").Optional = true
+			common.MustSchemaPath(m, "config", "served_entities", "workload_size").Computed = true
+			common.MustSchemaPath(m, "config", "served_entities", "workload_type").Optional = true
+			common.MustSchemaPath(m, "config", "served_entities", "workload_type").Computed = true
 			common.MustSchemaPath(m, "config", "served_entities", "workload_type").DiffSuppressFunc = func(k, old, new string, d *schema.ResourceData) bool {
 				return old == "" && new == "CPU"
 			}

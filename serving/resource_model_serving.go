7de84a5f39d9cdb2267b5516f75e9d7c41dc756e
Arpit Jasapara <arpit.jasapara@databricks.com>
2024-06-21 18:49:52 -0700

    Fix model serving resource

--- serving/resource_model_serving.go
+++ serving/resource_model_serving.go
@@ -2,10 +2,7 @@ package serving
 
 import (
 	"context"
-	"fmt"
 	"log"
-	"slices"
-	"strings"
 	"time"
 
 	"github.com/databricks/databricks-sdk-go/retries"
@@ -24,33 +21,15 @@ func ResourceModelServing() common.Resource {
 			m["name"].ForceNew = true
 			common.MustSchemaPath(m, "config", "served_models").ConflictsWith = []string{"config.served_entities"}
 			common.MustSchemaPath(m, "config", "served_entities").ConflictsWith = []string{"config.served_models"}
-			common.MustSchemaPath(m, "config", "served_models", "scale_to_zero_enabled").Required = false
-			common.MustSchemaPath(m, "config", "served_models", "scale_to_zero_enabled").Optional = true
-			common.MustSchemaPath(m, "config", "served_models", "scale_to_zero_enabled").Default = true
-			common.MustSchemaPath(m, "config", "served_models", "name").Computed = true
-			common.MustSchemaPath(m, "config", "served_models", "workload_type").Default = "CPU"
-			// TODO: `config.served_models.workload_type` should be a `Optional+Computed` field. Also consider this for other similar fields.
-			// In this scenario, if a workspace does not have GPU serving, specifying `workload_type` = 'CPU' will get empty response from API.
-			common.MustSchemaPath(m, "config", "served_models", "workload_type").DiffSuppressFunc = func(k, old, new string, d *schema.ResourceData) bool {
-				return old == "" && new == "CPU"
-			}
 			common.MustSchemaPath(m, "config", "traffic_config").Computed = true
+
+			common.MustSchemaPath(m, "config", "served_models", "name").Computed = true
+			common.MustSchemaPath(m, "config", "served_models", "workload_type").Computed = true
 			common.MustSchemaPath(m, "config", "served_models").Deprecated = "Please use 'config.served_entities' instead of 'config.served_models'."
 
-			common.MustSchemaPath(m, "config", "served_entities", "scale_to_zero_enabled").Required = false
-			common.MustSchemaPath(m, "config", "served_entities", "scale_to_zero_enabled").Optional = true
-			common.MustSchemaPath(m, "config", "served_entities", "scale_to_zero_enabled").Default = false
 			common.MustSchemaPath(m, "config", "served_entities", "name").Computed = true
-			common.MustSchemaPath(m, "config", "served_entities", "workload_size").Optional = true
 			common.MustSchemaPath(m, "config", "served_entities", "workload_size").Computed = true
-			common.MustSchemaPath(m, "config", "served_entities", "workload_type").Optional = true
 			common.MustSchemaPath(m, "config", "served_entities", "workload_type").Computed = true
-			common.MustSchemaPath(m, "config", "served_entities", "workload_type").DiffSuppressFunc = func(k, old, new string, d *schema.ResourceData) bool {
-				return old == "" && new == "CPU"
-			}
-			common.MustSchemaPath(m, "config", "auto_capture_config", "catalog_name").ForceNew = true
-			common.MustSchemaPath(m, "config", "auto_capture_config", "schema_name").ForceNew = true
-			common.MustSchemaPath(m, "config", "auto_capture_config", "table_name_prefix").ForceNew = true
 
 			m["serving_endpoint_id"] = &schema.Schema{
 				Computed: true,
@@ -60,17 +39,6 @@ func ResourceModelServing() common.Resource {
 		})
 
 	return common.Resource{
-		CustomizeDiff: func(ctx context.Context, d *schema.ResourceDiff) error {
-			old, new := d.GetChange("config.0.auto_capture_config.0.enabled")
-			if old != nil && old == false && new == true {
-				d.ForceNew("config.0.auto_capture_config.0.enabled")
-			}
-			err := validateExternalModelConfig(d)
-			if err != nil {
-				return err
-			}
-			return nil
-		},
 		Create: func(ctx context.Context, d *schema.ResourceData, c *common.DatabricksClient) error {
 			w, err := c.WorkspaceClient()
 			if err != nil {
@@ -78,9 +46,6 @@ func ResourceModelServing() common.Resource {
 			}
 			var e serving.CreateServingEndpoint
 			common.DataToStructPointer(d, s, &e)
-			for i := range e.Config.ServedEntities {
-				e.Config.ServedEntities[i].ForceSendFields = append(e.Config.ServedEntities[i].ForceSendFields, "ScaleToZeroEnabled", "MinProvisionedThroughput")
-			}
 			wait, err := w.ServingEndpoints.Create(ctx, e)
 			if err != nil {
 				return err
@@ -133,9 +98,6 @@ func ResourceModelServing() common.Resource {
 			}
 			var e serving.CreateServingEndpoint
 			common.DataToStructPointer(d, s, &e)
-			for i := range e.Config.ServedEntities {
-				e.Config.ServedEntities[i].ForceSendFields = append(e.Config.ServedEntities[i].ForceSendFields, "ScaleToZeroEnabled")
-			}
 			e.Config.Name = e.Name
 			_, err = w.ServingEndpoints.UpdateConfigAndWait(ctx, e.Config, retries.Timeout[serving.ServingEndpointDetailed](d.Timeout(schema.TimeoutUpdate)))
 			return err
@@ -155,35 +117,4 @@ func ResourceModelServing() common.Resource {
 			Update: schema.DefaultTimeout(DefaultProvisionTimeout),
 		},
 	}
-}
-
-func validateExternalModelConfig(d *schema.ResourceDiff) error {
-	_, e := d.GetOk("config.0.served_entities.0.external_model")
-	provider, p := d.GetOk("config.0.served_entities.0.external_model.0.provider")
-
-	if !e || !p {
-		return nil
-	}
-
-	name := strings.ReplaceAll(provider.(string), "-", "_")
-	config := d.Get(fmt.Sprintf("config.0.served_entities.0.external_model.0.%s_config", name)).([]interface{})
-
-	if len(config) == 0 {
-		return fmt.Errorf("external_model provider is set to \"%s\" but \"%s_config\" block is missing", name, name)
-	}
-
-	if configBlock, ok := d.Get("config.0.served_entities.0.external_model.0").(map[string]interface{}); ok {
-		var found []string
-		for key, value := range configBlock {
-			if strings.HasSuffix(key, "_config") && len(value.([]interface{})) > 0 {
-				found = append(found, key)
-			}
-		}
-		slices.Sort(found)
-		if len(found) > 1 {
-			msg := strings.Join(found, ", ")
-			return fmt.Errorf("only one external_model config block is allowed. Found: %s", msg)
-		}
-	}
-	return nil
 }

2b57c130ea1e20e06849060115d0a21223e323df
Karol <lus.karol@gmail.com>
2024-07-02 12:07:11 +0200

    acceptance test

--- internal/acceptance/data_volume_test.go
+++ internal/acceptance/data_volume_test.go
@@ -4,15 +4,13 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func checkDataSourceVolume(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
-		r, ok := s.Modules[0].Resources["data.databricks_volume.this"]
+		_, ok := s.Modules[0].Resources["data.databricks_volume.this"]
 		require.True(t, ok, "data.databricks_volume.this has to be there")
-		assert.Equal(t, s.Modules[0].Resources["databricks_volume.this"].Primary.ID, r.Primary.Attributes["full_name"])
 		return nil
 	}
 }

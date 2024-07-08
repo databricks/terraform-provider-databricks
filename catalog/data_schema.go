d24adbd945bf14be245f6069e3e3b64a789bf218
Alex Ott <alexey.ott@databricks.com>
2024-07-08 23:00:43 +0200

    Add `databricks_schema` data source (#3732)
    Adding new data source for completeness of our data sources for UC objects.

---
+++ catalog/data_schema.go
@@ -0,0 +1,25 @@
+package catalog
+
+import (
+	"context"
+
+	"github.com/databricks/databricks-sdk-go"
+	"github.com/databricks/databricks-sdk-go/service/catalog"
+	"github.com/databricks/terraform-provider-databricks/common"
+)
+
+func DataSourceSchema() common.Resource {
+	return common.WorkspaceData(func(ctx context.Context, data *struct {
+		Id     string              `json:"id,omitempty" tf:"computed"`
+		Name   string              `json:"name"`
+		Schema *catalog.SchemaInfo `json:"schema_info,omitempty" tf:"computed"`
+	}, w *databricks.WorkspaceClient) error {
+		schema, err := w.Schemas.GetByFullName(ctx, data.Name)
+		if err != nil {
+			return err
+		}
+		data.Schema = schema
+		data.Id = schema.FullName
+		return nil
+	})
+}

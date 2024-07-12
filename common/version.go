26e0384317a768e97a24b60f5e1940ee743ac62d
Tanmay Rustagi <tanmay.rustagi@databricks.com>
2024-07-12 17:03:11 +0200

    -

--- common/version.go
+++ common/version.go
@@ -3,7 +3,7 @@ package common
 import "context"
 
 var (
-	version = "1.48.3"
+	version = "1.49.0"
 	// ResourceName is resource name without databricks_ prefix
 	ResourceName contextKey = 1
 	// Provider is the current instance of provider

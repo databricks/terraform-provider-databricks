a8bdb4d9bbc2adb545cb2eba609da93e7449aea4
Cedric <23346008+840@users.noreply.github.com>
2024-07-22 17:46:07 +0200

    Add support for name or regional lookup (still broken because of invalid OpenAPI spec)

--- mws/data_mws_network_connectivity_config.go
+++ mws/data_mws_network_connectivity_config.go
@@ -2,37 +2,33 @@ package mws
 
 import (
 	"context"
-
 	"github.com/databricks/databricks-sdk-go"
 	"github.com/databricks/databricks-sdk-go/service/settings"
 	"github.com/databricks/terraform-provider-databricks/common"
 )
 
 func DataSourceMwsNetworkConnectivityConfig() common.Resource {
-	return common.AccountData(func(ctx context.Context, data *struct {
-		Id                          string                   `json:"id" tf:"computed,optional"`
-		AccountId                   string                   `json:"account_id,omitempty" tf:"computed"`
-		CreationTime                int64                    `json:"creation_time,omitempty" tf:"computed"`
-		EgressConfig                settings.NccEgressConfig `json:"egress_config,omitempty" tf:"computed"`
-		Name                        string                   `json:"name,omitempty" tf:"computed"`
-		NetworkConnectivityConfigId string                   `json:"network_connectivity_config_id"`
-		Region                      string                   `json:"region,omitempty" tf:"computed"`
-		UpdatedTime                 int64                    `json:"updated_time,omitempty" tf:"computed"`
-	}, a *databricks.AccountClient) error {
-		ncc, err := a.NetworkConnectivity.GetNetworkConnectivityConfigurationByNetworkConnectivityConfigId(ctx, data.NetworkConnectivityConfigId)
-		if err != nil {
-			return err
+	type MwsNetworkConnectivity struct {
+		settings.NetworkConnectivityConfiguration
+	}
+
+	type MwsNetworkConnectivityParams struct {
+		Name   string `json:"name" tf:"computed,optional"`
+		Region string `json:"region" tf:"computed,optional"`
+	}
+
+	return common.AccountDataWithParams(func(ctx context.Context, data MwsNetworkConnectivityParams, a *databricks.AccountClient) (*MwsNetworkConnectivity, error) {
+		nccList := a.NetworkConnectivity.ListNetworkConnectivityConfigurations(ctx, settings.ListNetworkConnectivityConfigurationsRequest{})
+		for nccList.HasNext(ctx) {
+			ncc, err := nccList.Next(ctx)
+
+			if err != nil {
+				return nil, err
+			}
+			if data.Region == ncc.Region || data.Name == ncc.Name {
+				return &MwsNetworkConnectivity{NetworkConnectivityConfiguration: ncc}, nil
+			}
 		}
-		data.Id = ncc.NetworkConnectivityConfigId
-		data.AccountId = ncc.AccountId
-		data.CreationTime = ncc.CreationTime
-		data.EgressConfig = settings.NccEgressConfig{
-			DefaultRules: ncc.EgressConfig.DefaultRules,
-			TargetRules:  ncc.EgressConfig.TargetRules,
-		}
-		data.Name = ncc.Name
-		data.Region = ncc.Region
-		data.UpdatedTime = ncc.UpdatedTime
-		return nil
+		return nil, nil
 	})
 }

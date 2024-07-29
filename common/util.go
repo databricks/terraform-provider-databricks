46b5b37190988f507e1e6264938cb205133d2e2f
Vuong <vuong.nguyen@databricks.com>
2024-07-29 13:36:01 +0800

    comment

--- common/util.go
+++ common/util.go
@@ -49,8 +49,7 @@ func MustInt64(s string) int64 {
 	return n
 }
 
-// GetInt64 returns the data for the given key and cast it to int64, or nil if the key doesn't exist
-// in the schema.
+// GetInt64 returns the data for the given key and cast it to int64
 func GetInt64(d *schema.ResourceData, key string) int64 {
 	return int64(d.Get(key).(int))
 }

086a64f05ebd2c8cff78dd1ea85a836740bb255f
Tanmay Rustagi <tanmay.rustagi@databricks.com>
2024-08-08 12:20:29 +0200

    -

--- pluginframework/resource_quality_monitor_test.go
+++ pluginframework/resource_quality_monitor_test.go
@@ -3,11 +3,21 @@ package pluginframework
 import (
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
+func getProtoV6ProviderFactory() map[string]func() (tfprotov6.ProviderServer, error) {
+	return nil
+}
+
 func TestQualityMonitorCreate(t *testing.T) {
 	resource.UnitTest(t, resource.TestCase{
-		Steps: []resource.TestStep{},
+		ProtoV6ProviderFactories: getProtoV6ProviderFactory(),
+		Steps: []resource.TestStep{
+			{
+				ResourceName: "databricks_quality_monitor_pluginframework",
+			},
+		},
 	})
 }

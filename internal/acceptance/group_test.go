4ad0fbb99ef72636b5d52d82ff13550dc5cde4ef
Edward Feng <67326663+edwardfeng-db@users.noreply.github.com>
2024-08-09 13:51:56 +0200

    Update init test and migrate imports for plugin framework (#3867)
    ## Changes
    <!-- Summary of your changes that are easy to understand -->
    - Part of
    https://github.com/databricks/terraform-provider-databricks/pull/3841
    - Migrated imports following the documentation
    [here](https://developer.hashicorp.com/terraform/plugin/testing/migrating)
      - 
    <img width="688" alt="image"
    src="https://github.com/user-attachments/assets/f2f14175-f3f8-47b7-a19f-1f0fca13eb47">
    
    - Made `run` in `init_test` able to take both plugin framework resources
    as well as sdkv2 resources
    
    ## Tests
    <!-- 
    How is this tested? Please see the checklist below and also describe any
    other relevant tests
    -->
    
    - [x] `make test` run locally
    - [x] relevant change in `docs/` folder
    - [x] covered with integration tests in `internal/acceptance`
    - [x] relevant acceptance tests are passing
    - [x] using Go SDK

--- internal/acceptance/group_test.go
+++ internal/acceptance/group_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/databricks/terraform-provider-databricks/qa"
 	"github.com/databricks/terraform-provider-databricks/scim"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 
 	"testing"
 )
--- mws/resource_mws_workspaces.go
+++ mws/resource_mws_workspaces.go
@@ -19,8 +19,8 @@ import (
 
 	"github.com/hashicorp/go-cty/cty"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
 // DefaultProvisionTimeout is the amount of minutes terraform will wait

209271e7a198270a5f9a751fd88d9db4216ea05b
edwardfeng-db <jinghao.feng@databricks.com>
2024-08-09 13:46:42 +0200

    update

--- internal/acceptance/permissions_test.go
+++ internal/acceptance/permissions_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/databricks/terraform-provider-databricks/common"
 	"github.com/databricks/terraform-provider-databricks/permissions"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/stretchr/testify/assert"

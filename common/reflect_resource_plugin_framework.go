3f8b2959a89e843e5057c3ffaa59a1ddddb9f301
edwardfeng-db <jinghao.feng@databricks.com>
2024-08-09 17:00:05 +0200

    update

--- common/reflect_resource_plugin_framework.go
+++ common/reflect_resource_plugin_framework.go
@@ -7,7 +7,9 @@ import (
 	"reflect"
 	"strings"
 
+	dataschema "github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
@@ -483,6 +485,106 @@ func pluginFrameworkResourceTypeToSchema(v reflect.Value) map[string]schema.Attr
 	return scm
 }
 
+func pluginFrameworkDataSourceTypeToSchema(v reflect.Value) map[string]dataschema.Attribute {
+	scm := map[string]dataschema.Attribute{}
+	rk := v.Kind()
+	if rk == reflect.Ptr {
+		v = v.Elem()
+		rk = v.Kind()
+	}
+	if rk != reflect.Struct {
+		panic(fmt.Errorf("Schema value of Struct is expected, but got %s: %#v", reflectKind(rk), v))
+	}
+	fields := listAllFields(v)
+	for _, field := range fields {
+		typeField := field.sf
+		fieldName := typeField.Tag.Get("tfsdk")
+		if fieldName == "-" {
+			continue
+		}
+		isOptional := fieldIsOptional(typeField)
+		// For now everything is marked as optional. TODO: add tf annotations for computed, optional, etc.
+		kind := typeField.Type.Kind()
+		if kind == reflect.Ptr {
+			elem := typeField.Type.Elem()
+			sv := reflect.New(elem).Elem()
+			nestedScm := pluginFrameworkDataSourceTypeToSchema(sv)
+			scm[fieldName] = dataschema.SingleNestedAttribute{Attributes: nestedScm, Optional: isOptional, Required: !isOptional}
+		} else if kind == reflect.Slice {
+			elem := typeField.Type.Elem()
+			if elem.Kind() == reflect.Ptr {
+				elem = elem.Elem()
+			}
+			if elem.Kind() != reflect.Struct {
+				panic(fmt.Errorf("unsupported slice value for %s: %s", fieldName, reflectKind(elem.Kind())))
+			}
+			switch elem {
+			case reflect.TypeOf(types.Bool{}):
+				scm[fieldName] = dataschema.ListAttribute{ElementType: types.BoolType, Optional: isOptional, Required: !isOptional}
+			case reflect.TypeOf(types.Int64{}):
+				scm[fieldName] = dataschema.ListAttribute{ElementType: types.Int64Type, Optional: isOptional, Required: !isOptional}
+			case reflect.TypeOf(types.Float64{}):
+				scm[fieldName] = dataschema.ListAttribute{ElementType: types.Float64Type, Optional: isOptional, Required: !isOptional}
+			case reflect.TypeOf(types.String{}):
+				scm[fieldName] = dataschema.ListAttribute{ElementType: types.StringType, Optional: isOptional, Required: !isOptional}
+			default:
+				// Nested struct
+				nestedScm := pluginFrameworkDataSourceTypeToSchema(reflect.New(elem).Elem())
+				scm[fieldName] = dataschema.ListNestedAttribute{NestedObject: dataschema.NestedAttributeObject{Attributes: nestedScm}, Optional: isOptional, Required: !isOptional}
+			}
+		} else if kind == reflect.Map {
+			elem := typeField.Type.Elem()
+			if elem.Kind() == reflect.Ptr {
+				elem = elem.Elem()
+			}
+			if elem.Kind() != reflect.Struct {
+				panic(fmt.Errorf("unsupported map value for %s: %s", fieldName, reflectKind(elem.Kind())))
+			}
+			switch elem {
+			case reflect.TypeOf(types.Bool{}):
+				scm[fieldName] = dataschema.MapAttribute{ElementType: types.BoolType, Optional: isOptional, Required: !isOptional}
+			case reflect.TypeOf(types.Int64{}):
+				scm[fieldName] = dataschema.MapAttribute{ElementType: types.Int64Type, Optional: isOptional, Required: !isOptional}
+			case reflect.TypeOf(types.Float64{}):
+				scm[fieldName] = dataschema.MapAttribute{ElementType: types.Float64Type, Optional: isOptional, Required: !isOptional}
+			case reflect.TypeOf(types.String{}):
+				scm[fieldName] = dataschema.MapAttribute{ElementType: types.StringType, Optional: isOptional, Required: !isOptional}
+			default:
+				// Nested struct
+				nestedScm := pluginFrameworkDataSourceTypeToSchema(reflect.New(elem).Elem())
+				scm[fieldName] = dataschema.MapNestedAttribute{NestedObject: dataschema.NestedAttributeObject{Attributes: nestedScm}, Optional: isOptional, Required: !isOptional}
+			}
+		} else if kind == reflect.Struct {
+			switch field.v.Interface().(type) {
+			case types.Bool:
+				scm[fieldName] = dataschema.BoolAttribute{Optional: isOptional, Required: !isOptional}
+			case types.Int64:
+				scm[fieldName] = dataschema.Int64Attribute{Optional: isOptional, Required: !isOptional}
+			case types.Float64:
+				scm[fieldName] = dataschema.Float64Attribute{Optional: isOptional, Required: !isOptional}
+			case types.String:
+				scm[fieldName] = dataschema.StringAttribute{Optional: isOptional, Required: !isOptional}
+			case types.List:
+				panic("types.List should never be used in tfsdk structs")
+			case types.Map:
+				panic("types.Map should never be used in tfsdk structs")
+			default:
+				// If it is a real stuct instead of a tfsdk type, recursively resolve it.
+				elem := typeField.Type
+				sv := reflect.New(elem)
+				nestedScm := pluginFrameworkDataSourceTypeToSchema(sv)
+				scm[fieldName] = dataschema.SingleNestedAttribute{Attributes: nestedScm, Optional: isOptional, Required: !isOptional}
+			}
+		} else if kind == reflect.String {
+			// This case is for Enum types.
+			scm[fieldName] = dataschema.StringAttribute{Optional: isOptional, Required: !isOptional}
+		} else {
+			panic(fmt.Errorf("unknown type for field: %s", typeField.Name))
+		}
+	}
+	return scm
+}
+
 func fieldIsOptional(field reflect.StructField) bool {
 	tagValue := field.Tag.Get("tf")
 	return strings.Contains(tagValue, "optional")
@@ -493,11 +595,27 @@ func PluginFrameworkResourceStructToSchema(v any, customizeSchema func(Customiza
 	return schema.Schema{Attributes: attributes}
 }
 
+func PluginFrameworkDataSourceStructToSchema(v any, customizeSchema func(CustomizableSchemaPluginFrameworkDataSource) CustomizableSchemaPluginFrameworkDataSource) dataschema.Schema {
+	attributes := PluginFrameworkDataSourceStructToSchemaMap(v, customizeSchema)
+	return dataschema.Schema{Attributes: attributes}
+}
+
 func PluginFrameworkResourceStructToSchemaMap(v any, customizeSchema func(CustomizableSchemaPluginFramework) CustomizableSchemaPluginFramework) map[string]schema.Attribute {
 	attributes := pluginFrameworkResourceTypeToSchema(reflect.ValueOf(v))
 
 	if customizeSchema != nil {
 		cs := customizeSchema(*ConstructCustomizableSchema(attributes))
+		return cs.ToAttributeMap()
+	} else {
+		return attributes
+	}
+}
+
+func PluginFrameworkDataSourceStructToSchemaMap(v any, customizeSchema func(CustomizableSchemaPluginFrameworkDataSource) CustomizableSchemaPluginFrameworkDataSource) map[string]dataschema.Attribute {
+	attributes := pluginFrameworkDataSourceTypeToSchema(reflect.ValueOf(v))
+
+	if customizeSchema != nil {
+		cs := customizeSchema(*ConstructCustomizableSchemaDataSource(attributes))
 		return cs.ToAttributeMap()
 	} else {
 		return attributes

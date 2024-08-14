c0b13d196c77ae52296f863815d52dfbb635b19d
edwardfeng-db <jinghao.feng@databricks.com>
2024-08-14 17:32:09 +0200

    update

--- common/customizable_schema_plugin_framework.go
+++ common/customizable_schema_plugin_framework.go
@@ -2,33 +2,30 @@ package common
 
 import (
 	"fmt"
-	"reflect"
-
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 )
 
 type CustomizableSchemaPluginFramework struct {
-	attr schema.Attribute
+	attr Attribute
 }
 
-func ConstructCustomizableSchema(attributes map[string]schema.Attribute) *CustomizableSchemaPluginFramework {
-	attr := schema.Attribute(schema.SingleNestedAttribute{Attributes: attributes})
+func ConstructCustomizableSchema(attributes map[string]Attribute) *CustomizableSchemaPluginFramework {
+	attr := Attribute(SingleNestedAttribute{Attributes: attributes})
 	return &CustomizableSchemaPluginFramework{attr: attr}
 }
 
 // Converts CustomizableSchema into a map from string to Attribute.
-func (s *CustomizableSchemaPluginFramework) ToAttributeMap() map[string]schema.Attribute {
+func (s *CustomizableSchemaPluginFramework) ToAttributeMap() map[string]Attribute {
 	return attributeToMap(&s.attr)
 }
 
-func attributeToMap(attr *schema.Attribute) map[string]schema.Attribute {
-	var m map[string]schema.Attribute
+func attributeToMap(attr *Attribute) map[string]Attribute {
+	var m map[string]Attribute
 	switch attr := (*attr).(type) {
-	case schema.SingleNestedAttribute:
+	case SingleNestedAttribute:
 		m = attr.Attributes
-	case schema.ListNestedAttribute:
+	case ListNestedAttribute:
 		m = attr.NestedObject.Attributes
-	case schema.MapNestedAttribute:
+	case MapNestedAttribute:
 		m = attr.NestedObject.Attributes
 	default:
 		panic(fmt.Errorf("cannot convert to map, attribute is not nested"))
@@ -38,37 +35,8 @@ func attributeToMap(attr *schema.Attribute) map[string]schema.Attribute {
 }
 
 func (s *CustomizableSchemaPluginFramework) AddValidator(v any, path ...string) *CustomizableSchemaPluginFramework {
-	cb := func(attr schema.Attribute) schema.Attribute {
-		val := reflect.ValueOf(attr)
-
-		// Make a copy of the existing attr.
-		newAttr := reflect.New(val.Type()).Elem()
-		newAttr.Set(val)
-		val = newAttr
-
-		field := val.FieldByName("Validators")
-		if !field.IsValid() {
-			panic(fmt.Sprintf("Validators field not found in %T", attr))
-		}
-		if field.Kind() != reflect.Slice {
-			panic(fmt.Sprintf("Validators field is not a slice in %T", attr))
-		}
-		if !field.CanSet() {
-			panic(fmt.Sprintf("Validators field cannot be set in %T", attr))
-		}
-
-		elemType := field.Type().Elem()
-		value := reflect.ValueOf(v)
-
-		if !value.Type().AssignableTo(elemType) {
-			panic(fmt.Sprintf("Value of type %T is not assignable to slice of %s", v, elemType))
-		}
-
-		// Append the value
-		newSlice := reflect.Append(field, value)
-		field.Set(newSlice)
-
-		return val.Interface().(schema.Attribute)
+	cb := func(attr Attribute) Attribute {
+		return attr.AddValidators(v)
 	}
 
 	navigateSchemaWithCallback(&s.attr, cb, path...)
@@ -77,38 +45,8 @@ func (s *CustomizableSchemaPluginFramework) AddValidator(v any, path ...string) 
 }
 
 func (s *CustomizableSchemaPluginFramework) SetOptional(path ...string) *CustomizableSchemaPluginFramework {
-	cb := func(attr schema.Attribute) schema.Attribute {
-		// Get the concrete value stored in the interface
-		v := reflect.ValueOf(attr)
-
-		// Make a new addressable value and copy the original value into it
-		newAttr := reflect.New(v.Type()).Elem()
-		newAttr.Set(v)
-		v = newAttr
-
-		field := v.FieldByName("Required")
-		if field.IsValid() && field.CanSet() {
-			if field.Kind() == reflect.Bool {
-				field.SetBool(false)
-			} else {
-				panic(fmt.Sprintf("Required is not a bool field in %T", attr))
-			}
-		} else {
-			panic(fmt.Sprintf("Required field not found or cannot be set in %T", attr))
-		}
-
-		field = v.FieldByName("Optional")
-		if field.IsValid() && field.CanSet() {
-			if field.Kind() == reflect.Bool {
-				field.SetBool(true)
-			} else {
-				panic(fmt.Sprintf("Optional is not a bool field in %T", attr))
-			}
-		} else {
-			panic(fmt.Sprintf("Optional field not found or cannot be set in %T", attr))
-		}
-
-		return v.Interface().(schema.Attribute)
+	cb := func(attr Attribute) Attribute {
+		return attr.SetOptional()
 	}
 
 	navigateSchemaWithCallback(&s.attr, cb, path...)
@@ -117,38 +55,8 @@ func (s *CustomizableSchemaPluginFramework) SetOptional(path ...string) *Customi
 }
 
 func (s *CustomizableSchemaPluginFramework) SetRequired(path ...string) *CustomizableSchemaPluginFramework {
-	cb := func(attr schema.Attribute) schema.Attribute {
-		// Get the concrete value stored in the interface
-		v := reflect.ValueOf(attr)
-
-		// Make a new addressable value and copy the original value into it
-		newAttr := reflect.New(v.Type()).Elem()
-		newAttr.Set(v)
-		v = newAttr
-
-		field := v.FieldByName("Required")
-		if field.IsValid() && field.CanSet() {
-			if field.Kind() == reflect.Bool {
-				field.SetBool(true)
-			} else {
-				panic(fmt.Sprintf("Required is not a bool field in %T", attr))
-			}
-		} else {
-			panic(fmt.Sprintf("Required field not found or cannot be set in %T", attr))
-		}
-
-		field = v.FieldByName("Optional")
-		if field.IsValid() && field.CanSet() {
-			if field.Kind() == reflect.Bool {
-				field.SetBool(false)
-			} else {
-				panic(fmt.Sprintf("Optional is not a bool field in %T", attr))
-			}
-		} else {
-			panic(fmt.Sprintf("Optional field not found or cannot be set in %T", attr))
-		}
-
-		return v.Interface().(schema.Attribute)
+	cb := func(attr Attribute) Attribute {
+		return attr.SetRequired()
 	}
 
 	navigateSchemaWithCallback(&s.attr, cb, path...)
@@ -157,27 +65,8 @@ func (s *CustomizableSchemaPluginFramework) SetRequired(path ...string) *Customi
 }
 
 func (s *CustomizableSchemaPluginFramework) SetSensitive(path ...string) *CustomizableSchemaPluginFramework {
-	cb := func(attr schema.Attribute) schema.Attribute {
-		// Get the concrete value stored in the interface
-		v := reflect.ValueOf(attr)
-
-		// Make a new addressable value and copy the original value into it
-		newAttr := reflect.New(v.Type()).Elem()
-		newAttr.Set(v)
-		v = newAttr
-
-		field := v.FieldByName("Sensitive")
-		if field.IsValid() && field.CanSet() {
-			if field.Kind() == reflect.Bool {
-				field.SetBool(true)
-			} else {
-				panic(fmt.Sprintf("Sensitive is not a bool field in %T", attr))
-			}
-		} else {
-			panic(fmt.Sprintf("Sensitive field not found or cannot be set in %T", attr))
-		}
-
-		return v.Interface().(schema.Attribute)
+	cb := func(attr Attribute) Attribute {
+		return attr.SetSensitive()
 	}
 
 	navigateSchemaWithCallback(&s.attr, cb, path...)
@@ -185,27 +74,8 @@ func (s *CustomizableSchemaPluginFramework) SetSensitive(path ...string) *Custom
 }
 
 func (s *CustomizableSchemaPluginFramework) SetDeprecated(msg string, path ...string) *CustomizableSchemaPluginFramework {
-	cb := func(attr schema.Attribute) schema.Attribute {
-		// Get the concrete value stored in the interface
-		v := reflect.ValueOf(attr)
-
-		// Make a new addressable value and copy the original value into it
-		newAttr := reflect.New(v.Type()).Elem()
-		newAttr.Set(v)
-		v = newAttr
-
-		field := v.FieldByName("DeprecationMessage")
-		if field.IsValid() && field.CanSet() {
-			if field.Kind() == reflect.String {
-				field.SetString(msg)
-			} else {
-				panic(fmt.Sprintf("DeprecationMessage is not a string field in %T", attr))
-			}
-		} else {
-			panic(fmt.Sprintf("DeprecationMessage field not found or cannot be set in %T", attr))
-		}
-
-		return v.Interface().(schema.Attribute)
+	cb := func(attr Attribute) Attribute {
+		return attr.SetDeprecated(msg)
 	}
 
 	navigateSchemaWithCallback(&s.attr, cb, path...)
@@ -214,27 +84,8 @@ func (s *CustomizableSchemaPluginFramework) SetDeprecated(msg string, path ...st
 }
 
 func (s *CustomizableSchemaPluginFramework) SetComputed(path ...string) *CustomizableSchemaPluginFramework {
-	cb := func(attr schema.Attribute) schema.Attribute {
-		// Get the concrete value stored in the interface
-		v := reflect.ValueOf(attr)
-
-		// Make a new addressable value and copy the original value into it
-		newAttr := reflect.New(v.Type()).Elem()
-		newAttr.Set(v)
-		v = newAttr
-
-		field := v.FieldByName("Computed")
-		if field.IsValid() && field.CanSet() {
-			if field.Kind() == reflect.Bool {
-				field.SetBool(true)
-			} else {
-				panic(fmt.Sprintf("Computed is not a bool field in %T", attr))
-			}
-		} else {
-			panic(fmt.Sprintf("Computed field not found or cannot be set in %T", attr))
-		}
-
-		return v.Interface().(schema.Attribute)
+	cb := func(attr Attribute) Attribute {
+		return attr.SetComputed()
 	}
 
 	navigateSchemaWithCallback(&s.attr, cb, path...)
@@ -245,49 +96,8 @@ func (s *CustomizableSchemaPluginFramework) SetComputed(path ...string) *Customi
 // This should be used for fields that are not user-configurable but are returned
 // by the platform.
 func (s *CustomizableSchemaPluginFramework) SetReadOnly(path ...string) *CustomizableSchemaPluginFramework {
-	cb := func(attr schema.Attribute) schema.Attribute {
-		// Get the concrete value stored in the interface
-		v := reflect.ValueOf(attr)
-
-		// Make a new addressable value and copy the original value into it
-		newAttr := reflect.New(v.Type()).Elem()
-		newAttr.Set(v)
-		v = newAttr
-
-		field := v.FieldByName("Computed")
-		if field.IsValid() && field.CanSet() {
-			if field.Kind() == reflect.Bool {
-				field.SetBool(true)
-			} else {
-				panic(fmt.Sprintf("Computed is not a bool field in %T", attr))
-			}
-		} else {
-			panic(fmt.Sprintf("Computed field not found or cannot be set in %T", attr))
-		}
-
-		field = v.FieldByName("Optional")
-		if field.IsValid() && field.CanSet() {
-			if field.Kind() == reflect.Bool {
-				field.SetBool(false)
-			} else {
-				panic(fmt.Sprintf("Optional is not a bool field in %T", attr))
-			}
-		} else {
-			panic(fmt.Sprintf("Optional field not found or cannot be set in %T", attr))
-		}
-
-		field = v.FieldByName("Required")
-		if field.IsValid() && field.CanSet() {
-			if field.Kind() == reflect.Bool {
-				field.SetBool(false)
-			} else {
-				panic(fmt.Sprintf("Required is not a bool field in %T", attr))
-			}
-		} else {
-			panic(fmt.Sprintf("Required field not found or cannot be set in %T", attr))
-		}
-
-		return v.Interface().(schema.Attribute)
+	cb := func(attr Attribute) Attribute {
+		return attr.SetReadOnly()
 	}
 
 	navigateSchemaWithCallback(&s.attr, cb, path...)
@@ -295,39 +105,8 @@ func (s *CustomizableSchemaPluginFramework) SetReadOnly(path ...string) *Customi
 	return s
 }
 
-// Given a attribute map, navigate through the given path, panics if the path is not valid.
-func MustSchemaAttributePath(attrs map[string]schema.Attribute, path ...string) schema.Attribute {
-	attr := ConstructCustomizableSchema(attrs).attr
-
-	res, err := navigateSchema(&attr, path...)
-	if err != nil {
-		panic(err)
-	}
-
-	return res
-}
-
 // Helper function for navigating through schema attributes, panics if path does not exist or invalid.
-func navigateSchema(s *schema.Attribute, path ...string) (schema.Attribute, error) {
-	current_scm := s
-	for i, p := range path {
-		m := attributeToMap(current_scm)
-
-		v, ok := m[p]
-		if !ok {
-			return nil, fmt.Errorf("missing key %s", p)
-		}
-
-		if i == len(path)-1 {
-			return v, nil
-		}
-		current_scm = &v
-	}
-	return nil, fmt.Errorf("path %v is incomplete", path)
-}
-
-// Helper function for navigating through schema attributes, panics if path does not exist or invalid.
-func navigateSchemaWithCallback(s *schema.Attribute, cb func(schema.Attribute) schema.Attribute, path ...string) (schema.Attribute, error) {
+func navigateSchemaWithCallback(s *Attribute, cb func(Attribute) Attribute, path ...string) (Attribute, error) {
 	current_scm := s
 	for i, p := range path {
 		m := attributeToMap(current_scm)
--- common/reflect_resource_plugin_framework.go
+++ common/reflect_resource_plugin_framework.go
@@ -127,7 +127,7 @@ func tfSdkToGoSdkSingleField(srcField reflect.Value, destField reflect.Value, sr
 		case types.Map:
 			panic("types.Map should never be used, use go native maps instead")
 		default:
-			if srcField.IsNil() {
+			if srcField.IsZero() {
 				// Skip nils
 				return nil
 			}
@@ -312,7 +312,7 @@ func goSdkToTfSdkSingleField(srcField reflect.Value, destField reflect.Value, sr
 			destField.Set(reflect.ValueOf(types.StringNull()))
 		}
 	case reflect.Struct:
-		if srcField.IsNil() {
+		if srcField.IsZero() {
 			// Skip nils
 			return nil
 		}
@@ -385,8 +385,8 @@ func checkTheStringInForceSendFields(fieldName string, forceSendFields []string)
 	return false
 }
 
-func pluginFrameworkResourceTypeToSchema(v reflect.Value) map[string]schema.Attribute {
-	scm := map[string]schema.Attribute{}
+func pluginFrameworkTypeToSchema(v reflect.Value) map[string]Attribute {
+	scm := map[string]Attribute{}
 	rk := v.Kind()
 	if rk == reflect.Ptr {
 		v = v.Elem()
@@ -408,8 +408,8 @@ func pluginFrameworkResourceTypeToSchema(v reflect.Value) map[string]schema.Attr
 		if kind == reflect.Ptr {
 			elem := typeField.Type.Elem()
 			sv := reflect.New(elem).Elem()
-			nestedScm := pluginFrameworkResourceTypeToSchema(sv)
-			scm[fieldName] = schema.SingleNestedAttribute{Attributes: nestedScm, Optional: isOptional, Required: !isOptional}
+			nestedScm := pluginFrameworkTypeToSchema(sv)
+			scm[fieldName] = SingleNestedAttribute{Attributes: nestedScm, Optional: isOptional, Required: !isOptional}
 		} else if kind == reflect.Slice {
 			elem := typeField.Type.Elem()
 			if elem.Kind() == reflect.Ptr {
@@ -420,17 +420,17 @@ func pluginFrameworkResourceTypeToSchema(v reflect.Value) map[string]schema.Attr
 			}
 			switch elem {
 			case reflect.TypeOf(types.Bool{}):
-				scm[fieldName] = schema.ListAttribute{ElementType: types.BoolType, Optional: isOptional, Required: !isOptional}
+				scm[fieldName] = ListAttribute{ElementType: types.BoolType, Optional: isOptional, Required: !isOptional}
 			case reflect.TypeOf(types.Int64{}):
-				scm[fieldName] = schema.ListAttribute{ElementType: types.Int64Type, Optional: isOptional, Required: !isOptional}
+				scm[fieldName] = ListAttribute{ElementType: types.Int64Type, Optional: isOptional, Required: !isOptional}
 			case reflect.TypeOf(types.Float64{}):
-				scm[fieldName] = schema.ListAttribute{ElementType: types.Float64Type, Optional: isOptional, Required: !isOptional}
+				scm[fieldName] = ListAttribute{ElementType: types.Float64Type, Optional: isOptional, Required: !isOptional}
 			case reflect.TypeOf(types.String{}):
-				scm[fieldName] = schema.ListAttribute{ElementType: types.StringType, Optional: isOptional, Required: !isOptional}
+				scm[fieldName] = ListAttribute{ElementType: types.StringType, Optional: isOptional, Required: !isOptional}
 			default:
 				// Nested struct
-				nestedScm := pluginFrameworkResourceTypeToSchema(reflect.New(elem).Elem())
-				scm[fieldName] = schema.ListNestedAttribute{NestedObject: schema.NestedAttributeObject{Attributes: nestedScm}, Optional: isOptional, Required: !isOptional}
+				nestedScm := pluginFrameworkTypeToSchema(reflect.New(elem).Elem())
+				scm[fieldName] = ListNestedAttribute{NestedObject: NestedAttributeObject{Attributes: nestedScm}, Optional: isOptional, Required: !isOptional}
 			}
 		} else if kind == reflect.Map {
 			elem := typeField.Type.Elem()
@@ -442,28 +442,28 @@ func pluginFrameworkResourceTypeToSchema(v reflect.Value) map[string]schema.Attr
 			}
 			switch elem {
 			case reflect.TypeOf(types.Bool{}):
-				scm[fieldName] = schema.MapAttribute{ElementType: types.BoolType, Optional: isOptional, Required: !isOptional}
+				scm[fieldName] = MapAttribute{ElementType: types.BoolType, Optional: isOptional, Required: !isOptional}
 			case reflect.TypeOf(types.Int64{}):
-				scm[fieldName] = schema.MapAttribute{ElementType: types.Int64Type, Optional: isOptional, Required: !isOptional}
+				scm[fieldName] = MapAttribute{ElementType: types.Int64Type, Optional: isOptional, Required: !isOptional}
 			case reflect.TypeOf(types.Float64{}):
-				scm[fieldName] = schema.MapAttribute{ElementType: types.Float64Type, Optional: isOptional, Required: !isOptional}
+				scm[fieldName] = MapAttribute{ElementType: types.Float64Type, Optional: isOptional, Required: !isOptional}
 			case reflect.TypeOf(types.String{}):
-				scm[fieldName] = schema.MapAttribute{ElementType: types.StringType, Optional: isOptional, Required: !isOptional}
+				scm[fieldName] = MapAttribute{ElementType: types.StringType, Optional: isOptional, Required: !isOptional}
 			default:
 				// Nested struct
-				nestedScm := pluginFrameworkResourceTypeToSchema(reflect.New(elem).Elem())
-				scm[fieldName] = schema.MapNestedAttribute{NestedObject: schema.NestedAttributeObject{Attributes: nestedScm}, Optional: isOptional, Required: !isOptional}
+				nestedScm := pluginFrameworkTypeToSchema(reflect.New(elem).Elem())
+				scm[fieldName] = MapNestedAttribute{NestedObject: NestedAttributeObject{Attributes: nestedScm}, Optional: isOptional, Required: !isOptional}
 			}
 		} else if kind == reflect.Struct {
 			switch field.v.Interface().(type) {
 			case types.Bool:
-				scm[fieldName] = schema.BoolAttribute{Optional: isOptional, Required: !isOptional}
+				scm[fieldName] = BoolAttribute{Optional: isOptional, Required: !isOptional}
 			case types.Int64:
-				scm[fieldName] = schema.Int64Attribute{Optional: isOptional, Required: !isOptional}
+				scm[fieldName] = Int64Attribute{Optional: isOptional, Required: !isOptional}
 			case types.Float64:
-				scm[fieldName] = schema.Float64Attribute{Optional: isOptional, Required: !isOptional}
+				scm[fieldName] = Float64Attribute{Optional: isOptional, Required: !isOptional}
 			case types.String:
-				scm[fieldName] = schema.StringAttribute{Optional: isOptional, Required: !isOptional}
+				scm[fieldName] = StringAttribute{Optional: isOptional, Required: !isOptional}
 			case types.List:
 				panic("types.List should never be used in tfsdk structs")
 			case types.Map:
@@ -472,112 +472,12 @@ func pluginFrameworkResourceTypeToSchema(v reflect.Value) map[string]schema.Attr
 				// If it is a real stuct instead of a tfsdk type, recursively resolve it.
 				elem := typeField.Type
 				sv := reflect.New(elem)
-				nestedScm := pluginFrameworkResourceTypeToSchema(sv)
-				scm[fieldName] = schema.SingleNestedAttribute{Attributes: nestedScm, Optional: isOptional, Required: !isOptional}
+				nestedScm := pluginFrameworkTypeToSchema(sv)
+				scm[fieldName] = SingleNestedAttribute{Attributes: nestedScm, Optional: isOptional, Required: !isOptional}
 			}
 		} else if kind == reflect.String {
 			// This case is for Enum types.
-			scm[fieldName] = schema.StringAttribute{Optional: isOptional, Required: !isOptional}
-		} else {
-			panic(fmt.Errorf("unknown type for field: %s", typeField.Name))
-		}
-	}
-	return scm
-}
-
-func pluginFrameworkDataSourceTypeToSchema(v reflect.Value) map[string]dataschema.Attribute {
-	scm := map[string]dataschema.Attribute{}
-	rk := v.Kind()
-	if rk == reflect.Ptr {
-		v = v.Elem()
-		rk = v.Kind()
-	}
-	if rk != reflect.Struct {
-		panic(fmt.Errorf("Schema value of Struct is expected, but got %s: %#v", reflectKind(rk), v))
-	}
-	fields := listAllFields(v)
-	for _, field := range fields {
-		typeField := field.sf
-		fieldName := typeField.Tag.Get("tfsdk")
-		if fieldName == "-" {
-			continue
-		}
-		isOptional := fieldIsOptional(typeField)
-		// For now everything is marked as optional. TODO: add tf annotations for computed, optional, etc.
-		kind := typeField.Type.Kind()
-		if kind == reflect.Ptr {
-			elem := typeField.Type.Elem()
-			sv := reflect.New(elem).Elem()
-			nestedScm := pluginFrameworkDataSourceTypeToSchema(sv)
-			scm[fieldName] = dataschema.SingleNestedAttribute{Attributes: nestedScm, Optional: isOptional, Required: !isOptional}
-		} else if kind == reflect.Slice {
-			elem := typeField.Type.Elem()
-			if elem.Kind() == reflect.Ptr {
-				elem = elem.Elem()
-			}
-			if elem.Kind() != reflect.Struct {
-				panic(fmt.Errorf("unsupported slice value for %s: %s", fieldName, reflectKind(elem.Kind())))
-			}
-			switch elem {
-			case reflect.TypeOf(types.Bool{}):
-				scm[fieldName] = dataschema.ListAttribute{ElementType: types.BoolType, Optional: isOptional, Required: !isOptional}
-			case reflect.TypeOf(types.Int64{}):
-				scm[fieldName] = dataschema.ListAttribute{ElementType: types.Int64Type, Optional: isOptional, Required: !isOptional}
-			case reflect.TypeOf(types.Float64{}):
-				scm[fieldName] = dataschema.ListAttribute{ElementType: types.Float64Type, Optional: isOptional, Required: !isOptional}
-			case reflect.TypeOf(types.String{}):
-				scm[fieldName] = dataschema.ListAttribute{ElementType: types.StringType, Optional: isOptional, Required: !isOptional}
-			default:
-				// Nested struct
-				nestedScm := pluginFrameworkDataSourceTypeToSchema(reflect.New(elem).Elem())
-				scm[fieldName] = dataschema.ListNestedAttribute{NestedObject: dataschema.NestedAttributeObject{Attributes: nestedScm}, Optional: isOptional, Required: !isOptional}
-			}
-		} else if kind == reflect.Map {
-			elem := typeField.Type.Elem()
-			if elem.Kind() == reflect.Ptr {
-				elem = elem.Elem()
-			}
-			if elem.Kind() != reflect.Struct {
-				panic(fmt.Errorf("unsupported map value for %s: %s", fieldName, reflectKind(elem.Kind())))
-			}
-			switch elem {
-			case reflect.TypeOf(types.Bool{}):
-				scm[fieldName] = dataschema.MapAttribute{ElementType: types.BoolType, Optional: isOptional, Required: !isOptional}
-			case reflect.TypeOf(types.Int64{}):
-				scm[fieldName] = dataschema.MapAttribute{ElementType: types.Int64Type, Optional: isOptional, Required: !isOptional}
-			case reflect.TypeOf(types.Float64{}):
-				scm[fieldName] = dataschema.MapAttribute{ElementType: types.Float64Type, Optional: isOptional, Required: !isOptional}
-			case reflect.TypeOf(types.String{}):
-				scm[fieldName] = dataschema.MapAttribute{ElementType: types.StringType, Optional: isOptional, Required: !isOptional}
-			default:
-				// Nested struct
-				nestedScm := pluginFrameworkDataSourceTypeToSchema(reflect.New(elem).Elem())
-				scm[fieldName] = dataschema.MapNestedAttribute{NestedObject: dataschema.NestedAttributeObject{Attributes: nestedScm}, Optional: isOptional, Required: !isOptional}
-			}
-		} else if kind == reflect.Struct {
-			switch field.v.Interface().(type) {
-			case types.Bool:
-				scm[fieldName] = dataschema.BoolAttribute{Optional: isOptional, Required: !isOptional}
-			case types.Int64:
-				scm[fieldName] = dataschema.Int64Attribute{Optional: isOptional, Required: !isOptional}
-			case types.Float64:
-				scm[fieldName] = dataschema.Float64Attribute{Optional: isOptional, Required: !isOptional}
-			case types.String:
-				scm[fieldName] = dataschema.StringAttribute{Optional: isOptional, Required: !isOptional}
-			case types.List:
-				panic("types.List should never be used in tfsdk structs")
-			case types.Map:
-				panic("types.Map should never be used in tfsdk structs")
-			default:
-				// If it is a real stuct instead of a tfsdk type, recursively resolve it.
-				elem := typeField.Type
-				sv := reflect.New(elem)
-				nestedScm := pluginFrameworkDataSourceTypeToSchema(sv)
-				scm[fieldName] = dataschema.SingleNestedAttribute{Attributes: nestedScm, Optional: isOptional, Required: !isOptional}
-			}
-		} else if kind == reflect.String {
-			// This case is for Enum types.
-			scm[fieldName] = dataschema.StringAttribute{Optional: isOptional, Required: !isOptional}
+			scm[fieldName] = StringAttribute{Optional: isOptional, Required: !isOptional}
 		} else {
 			panic(fmt.Errorf("unknown type for field: %s", typeField.Name))
 		}
@@ -595,29 +495,29 @@ func PluginFrameworkResourceStructToSchema(v any, customizeSchema func(Customiza
 	return schema.Schema{Attributes: attributes}
 }
 
-func PluginFrameworkDataSourceStructToSchema(v any, customizeSchema func(CustomizableSchemaPluginFrameworkDataSource) CustomizableSchemaPluginFrameworkDataSource) dataschema.Schema {
+func PluginFrameworkDataSourceStructToSchema(v any, customizeSchema func(CustomizableSchemaPluginFramework) CustomizableSchemaPluginFramework) dataschema.Schema {
 	attributes := PluginFrameworkDataSourceStructToSchemaMap(v, customizeSchema)
 	return dataschema.Schema{Attributes: attributes}
 }
 
 func PluginFrameworkResourceStructToSchemaMap(v any, customizeSchema func(CustomizableSchemaPluginFramework) CustomizableSchemaPluginFramework) map[string]schema.Attribute {
-	attributes := pluginFrameworkResourceTypeToSchema(reflect.ValueOf(v))
+	attributes := pluginFrameworkTypeToSchema(reflect.ValueOf(v))
 
 	if customizeSchema != nil {
 		cs := customizeSchema(*ConstructCustomizableSchema(attributes))
-		return cs.ToAttributeMap()
+		return ToResourceAttributeMap(cs.ToAttributeMap())
 	} else {
-		return attributes
+		return ToResourceAttributeMap(attributes)
 	}
 }
 
-func PluginFrameworkDataSourceStructToSchemaMap(v any, customizeSchema func(CustomizableSchemaPluginFrameworkDataSource) CustomizableSchemaPluginFrameworkDataSource) map[string]dataschema.Attribute {
-	attributes := pluginFrameworkDataSourceTypeToSchema(reflect.ValueOf(v))
+func PluginFrameworkDataSourceStructToSchemaMap(v any, customizeSchema func(CustomizableSchemaPluginFramework) CustomizableSchemaPluginFramework) map[string]dataschema.Attribute {
+	attributes := pluginFrameworkTypeToSchema(reflect.ValueOf(v))
 
 	if customizeSchema != nil {
-		cs := customizeSchema(*ConstructCustomizableSchemaDataSource(attributes))
-		return cs.ToAttributeMap()
+		cs := customizeSchema(*ConstructCustomizableSchema(attributes))
+		return ToDataSourceAttributeMap(cs.ToAttributeMap())
 	} else {
-		return attributes
+		return ToDataSourceAttributeMap(attributes)
 	}
 }

74f9d15d661c38224cbd738281fe220130706d76
Miles Yucht <miles@databricks.com>
2024-08-14 10:05:35 +0200

    work

--- pipelines/resource_pipeline.go
+++ pipelines/resource_pipeline.go
@@ -176,6 +176,7 @@ type Pipeline struct {
 
 func (Pipeline) Aliases() map[string]map[string]string {
 	return map[string]map[string]string{
+		"pipelines.Pipeline":     aliasMap,
 		"pipelines.PipelineSpec": aliasMap,
 	}
 

2855ef5f4730a370c17320617236499139df24f8
Tanmay Rustagi <88379306+tanmay-db@users.noreply.github.com>
2024-08-16 16:26:38 +0200

    [Internal] Added support to use protocol version 6 provider server for SDK plugin (#3862)
    ## Changes
    <!-- Summary of your changes that are easy to understand -->
    Merging
    https://github.com/databricks/terraform-provider-databricks/pull/3719 in
    main branch before release.
    
    ## Tests
    <!-- 
    How is this tested? Please see the checklist below and also describe any
    other relevant tests
    -->
    Nightly tests are running...
    - [ ] `make test` run locally
    - [ ] relevant change in `docs/` folder
    - [ ] covered with integration tests in `internal/acceptance`
    - [ ] relevant acceptance tests are passing
    - [ ] using Go SDK

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
@@ -8,8 +9,19 @@ import (
 	"github.com/databricks/terraform-provider-databricks/common"
 	"github.com/databricks/terraform-provider-databricks/exporter"
 	"github.com/databricks/terraform-provider-databricks/provider"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6/tf6server"
+	"github.com/hashicorp/terraform-plugin-mux/tf5to6server"
+	"github.com/hashicorp/terraform-plugin-mux/tf6muxserver"
 )
+
+const startMessageFormat = `Databricks Terraform Provider
+	
+Version %s
+	
+https://registry.terraform.io/providers/databricks/databricks/latest/docs
+	
+`
 
 func main() {
 	log.SetFlags(0)
@@ -24,20 +36,40 @@ func main() {
 		}
 		return
 	}
-	var debug bool
-	if len(os.Args) > 1 && os.Args[1] == "debug" {
-		debug = true
+
+	log.Printf(startMessageFormat, common.Version())
+
+	sdkPluginProvider := provider.DatabricksProvider()
+
+	upgradedSdkPluginProvider, err := tf5to6server.UpgradeServer(
+		context.Background(),
+		sdkPluginProvider.GRPCProvider,
+	)
+	if err != nil {
+		log.Fatal(err)
 	}
-	log.Printf(`Databricks Terraform Provider
 
-Version %s
-
-https://registry.terraform.io/providers/databricks/databricks/latest/docs
-
-`, common.Version())
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: provider.DatabricksProvider,
-		ProviderAddr: "registry.terraform.io/databricks/databricks",
-		Debug:        debug,
+	ctx := context.Background()
+	muxServer, err := tf6muxserver.NewMuxServer(ctx, func() tfprotov6.ProviderServer {
+		return upgradedSdkPluginProvider
 	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var serveOpts []tf6server.ServeOpt
+	if len(os.Args) > 1 && os.Args[1] == "debug" { // debug mode
+		serveOpts = append(serveOpts, tf6server.WithManagedDebug())
+	}
+
+	err = tf6server.Serve(
+		"registry.terraform.io/databricks/databricks",
+		muxServer.ProviderServer,
+		serveOpts...,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }

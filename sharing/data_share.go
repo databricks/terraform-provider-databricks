f5e5895bb077aa48fe068f9410928027bfa040f4
Omer Lachish <rauchy@users.noreply.github.com>
2024-08-19 13:41:31 +0200

    [Internal] migrate Share data source to Go SDK

--- sharing/data_share.go
+++ sharing/data_share.go
@@ -3,20 +3,28 @@ package sharing
 import (
 	"context"
 
+	"github.com/databricks/databricks-sdk-go/service/sharing"
 	"github.com/databricks/terraform-provider-databricks/common"
 )
 
 func DataSourceShare() common.Resource {
 	type ShareDetail struct {
-		Name      string             `json:"name,omitempty" tf:"computed"`
-		Objects   []SharedDataObject `json:"objects,omitempty" tf:"computed,slice_set,alias:object"`
-		CreatedAt int64              `json:"created_at,omitempty" tf:"computed"`
-		CreatedBy string             `json:"created_by,omitempty" tf:"computed"`
+		Name      string                     `json:"name,omitempty" tf:"computed"`
+		Objects   []sharing.SharedDataObject `json:"objects,omitempty" tf:"computed,slice_set,alias:object"`
+		CreatedAt int64                      `json:"created_at,omitempty" tf:"computed"`
+		CreatedBy string                     `json:"created_by,omitempty" tf:"computed"`
 	}
 	return common.DataResource(ShareDetail{}, func(ctx context.Context, e any, c *common.DatabricksClient) error {
 		data := e.(*ShareDetail)
-		sharesAPI := NewSharesAPI(ctx, c)
-		share, err := sharesAPI.get(data.Name)
+		client, err := c.WorkspaceClient()
+		if err != nil {
+			return err
+		}
+
+		share, err := client.Shares.Get(ctx, sharing.GetShareRequest{
+			Name:              data.Name,
+			IncludeSharedData: true,
+		})
 		if err != nil {
 			return err
 		}

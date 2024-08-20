b72057b9f5655999d33ac32acc1f8ac17b54563d
Tanmay Rustagi <tanmay.rustagi@databricks.com>
2024-08-20 17:42:00 +0200

    comments

--- provider/provider_factory.go
+++ provider/provider_factory.go
@@ -10,6 +10,14 @@ import (
 	"github.com/hashicorp/terraform-plugin-mux/tf6muxserver"
 )
 
+// GetProviderServer initializes and returns a Terraform Protocol v6 ProviderServer.
+// The function begins by initializing the Databricks provider using the SDK plugin
+// and then upgrades this provider to be compatible with Terraform's Protocol v6 using
+// the v5-to-v6 upgrade mechanism. Additionally, it retrieves the Databricks provider
+// that is implemented using the Terraform Plugin Framework. These different provider
+// implementations are then combined using a multiplexing server, which allows multiple
+// Protocol v6 providers to be served together. The function returns the multiplexed
+// ProviderServer, or an error if any part of the process fails.
 func GetProviderServer(ctx context.Context) (tfprotov6.ProviderServer, error) {
 	sdkPluginProvider := DatabricksProvider()
 

da0c32a306bac2387e76a5b3862982bd91aa75f2
edwardfeng-db <jinghao.feng@databricks.com>
2024-08-22 15:31:28 +0200

    update

--- internal/pluginframework/tfschema/struct_to_schema_test.go
+++ internal/pluginframework/tfschema/struct_to_schema_test.go
@@ -3,8 +3,10 @@ package tfschema
 import (
 	"context"
 	"fmt"
+	"strings"
 	"testing"
 
+	"github.com/databricks/terraform-provider-databricks/common"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/stretchr/testify/assert"
@@ -168,4 +170,81 @@ func TestStructToSchemaOptionalVsRequiredField(t *testing.T) {
 	data_scm := DataSourceStructToSchema(TestBoolTfSdk{}, nil)
 	assert.True(t, !data_scm.Attributes["enabled"].IsOptional())
 	assert.True(t, data_scm.Attributes["enabled"].IsRequired())
+}
+
+func testStructToSchemaPanics(t *testing.T, testStruct any, expectedError string) {
+	defer func() {
+		err := recover()
+		if err == nil {
+			t.Fatalf(fmt.Sprintf("did not fail with error %q", expectedError))
+		}
+		var errMsg string
+		switch e := err.(type) {
+		case error:
+			errMsg = e.Error()
+		case string:
+			errMsg = e
+		default:
+			t.Fatalf("recovered panic is of unknown type: %T, value: %v", e, e)
+		}
+
+		if !strings.Contains(errMsg, expectedError) {
+			t.Fatalf("error %s did not include expected error message %q", errMsg, expectedError)
+		}
+	}()
+	ResourceStructToSchema(testStruct, nil)
+}
+
+type TestTfSdkList struct {
+	Description types.List `tfsdk:"description" tf:"optional"`
+}
+
+type TestTfSdkMap struct {
+	Description types.Map `tfsdk:"description" tf:"optional"`
+}
+
+type TestSliceOfSlice struct {
+	NestedList [][]string `tfsdk:"nested_list" tf:"optional"`
+}
+
+type TestMapOfMap struct {
+	NestedMap map[string]map[string]string `tfsdk:"nested_map" tf:"optional"`
+}
+
+var error_tests = []struct {
+	name          string
+	testStruct    any
+	expectedError string
+}{
+	{
+		"tf list conversion",
+		TestTfSdkList{},
+		fmt.Sprintf("types.List should never be used in tfsdk structs. %s", common.TerraformBugErrorMessage),
+	},
+	{
+		"tf map conversion",
+		TestTfSdkMap{},
+		fmt.Sprintf("types.Map should never be used in tfsdk structs. %s", common.TerraformBugErrorMessage),
+	},
+	{
+		"non-struct conversion",
+		"Abc",
+		fmt.Sprintf("schema value of Struct is expected, but got string: \"Abc\". %s", common.TerraformBugErrorMessage),
+	},
+	{
+		"slice of slice conversion",
+		TestSliceOfSlice{},
+		fmt.Sprintf("unsupported slice value for nested_list: slice. %s", common.TerraformBugErrorMessage),
+	},
+	{
+		"map of map conversion",
+		TestMapOfMap{},
+		fmt.Sprintf("unsupported map value for nested_map: map. %s", common.TerraformBugErrorMessage),
+	},
+}
+
+func TestStructToSchemaExpectedError(t *testing.T) {
+	for _, test := range error_tests {
+		t.Run(test.name, func(t *testing.T) { testStructToSchemaPanics(t, test.testStruct, test.expectedError) })
+	}
 }

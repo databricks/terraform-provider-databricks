f4687d60ed95f41ce28f94a9c724d552d3eb2a55
Tanmay Rustagi <88379306+tanmay-db@users.noreply.github.com>
2024-08-30 15:10:47 +0200

    [Internal] Added `databricks_quality_monitor` resource and `databricks_volumes` data source to plugin framework (#3958)
    ## Changes
    <!-- Summary of your changes that are easy to understand -->
    Add support for Quality Monitor resource and Volumes data source in
    plugin framework. Also add utility method to get workspace and account
    client with diagnostics.
    
    Subpart of:
    https://github.com/databricks/terraform-provider-databricks/pull/3751 +
    Addressed comments from:
    https://github.com/databricks/terraform-provider-databricks/pull/3893/files#diff-8edec0f6289f63bfb61c7c6fb76fc1a61eec69c33fc2ba3a4ee6e77a8103f85b
    
    Note: The changes for flipping
    `databricks_quality_monitor_pluginframework` ->
    `databricks_quality_monitor` in pluginframework along with making this
    legacy for sdkv2 i.e. `databricks_quality_monitor` ->
    `databricks_quality_monitor_legacy` will be done in separate PR. Same
    for `databricks_volumes`
    
    ## Tests
    <!-- 
    How is this tested? Please see the checklist below and also describe any
    other relevant tests
    -->
    Existing unit tests.
    
    Integration and Unit tests for this resource will be merged after this
    PR is merged.
    
    - [ ] `make test` run locally
    - [ ] relevant change in `docs/` folder
    - [ ] covered with integration tests in `internal/acceptance`
    - [ ] relevant acceptance tests are passing
    - [ ] using Go SDK
    
    ---------
    
    Co-authored-by: Miles Yucht <miles@databricks.com>

---
+++ internal/providers/pluginfw/common/common.go
@@ -0,0 +1,39 @@
+package common
+
+import (
+	"fmt"
+
+	"github.com/databricks/terraform-provider-databricks/common"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+)
+
+// ConfigureDataSource is a helper function for configuring a general data source.
+// It returns the DatabricksClient if it can be successfully fetched from the ProviderData in the request;
+// otherwise, the error is appended to the diagnostics of the response.
+func ConfigureDataSource(req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) *common.DatabricksClient {
+	client, ok := req.ProviderData.(*common.DatabricksClient)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("Expected *common.DatabricksClient, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+		return nil
+	}
+	return client
+}
+
+// ConfigureDataSource is a helper function for configuring a general resource.
+// It returns the DatabricksClient if it can be successfully fetched from the ProviderData in the request;
+// otherwise, the error is appended to the diagnostics of the response.
+func ConfigureResource(req resource.ConfigureRequest, resp *resource.ConfigureResponse) *common.DatabricksClient {
+	client, ok := req.ProviderData.(*common.DatabricksClient)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected *common.DatabricksClient, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+		return nil
+	}
+	return client
+}
---
+++ internal/providers/pluginfw/resources/volume/data_volumes.go
@@ -0,0 +1,78 @@
+package volume
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/databricks/databricks-sdk-go/apierr"
+	"github.com/databricks/databricks-sdk-go/service/catalog"
+	"github.com/databricks/terraform-provider-databricks/common"
+	pluginfwcommon "github.com/databricks/terraform-provider-databricks/internal/providers/pluginfw/common"
+	"github.com/databricks/terraform-provider-databricks/internal/providers/pluginfw/converters"
+	"github.com/databricks/terraform-provider-databricks/internal/providers/pluginfw/tfschema"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+func DataSourceVolumes() datasource.DataSource {
+	return &VolumesDataSource{}
+}
+
+var _ datasource.DataSourceWithConfigure = &VolumesDataSource{}
+
+type VolumesDataSource struct {
+	Client *common.DatabricksClient
+}
+
+type VolumesList struct {
+	CatalogName types.String   `tfsdk:"catalog_name"`
+	SchemaName  types.String   `tfsdk:"schema_name"`
+	Ids         []types.String `tfsdk:"ids" tf:"computed,optional"`
+}
+
+func (d *VolumesDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = "databricks_volumes_pluginframework"
+}
+
+func (d *VolumesDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: tfschema.DataSourceStructToSchemaMap(VolumesList{}, nil),
+	}
+}
+
+func (d *VolumesDataSource) Configure(_ context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	if d.Client == nil {
+		d.Client = pluginfwcommon.ConfigureDataSource(req, resp)
+	}
+}
+
+func (d *VolumesDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	w, diags := d.Client.GetWorkspaceClient()
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var volumesList VolumesList
+	diags = req.Config.Get(ctx, &volumesList)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	var listVolumesRequest catalog.ListVolumesRequest
+	converters.TfSdkToGoSdkStruct(ctx, volumesList, &listVolumesRequest)
+	volumes, err := w.Volumes.ListAll(ctx, listVolumesRequest)
+	if err != nil {
+		if apierr.IsMissing(err) {
+			resp.State.RemoveResource(ctx)
+			return
+		}
+		resp.Diagnostics.AddError(fmt.Sprintf("Failed to get volumes for the catalog:%s and schema%s", listVolumesRequest.CatalogName, listVolumesRequest.SchemaName), err.Error())
+		return
+	}
+	for _, v := range volumes {
+		volumesList.Ids = append(volumesList.Ids, types.StringValue(v.FullName))
+	}
+	resp.State.Set(ctx, volumesList)
+}

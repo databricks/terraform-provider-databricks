1c2c94648ca6998eb51887ac612bf705cc1d7a05
Cedric <23346008+840@users.noreply.github.com>
2024-09-02 16:30:37 +0200

    Use SDK struct

--- mws/resource_mws_budgets.go
+++ mws/resource_mws_budgets.go
@@ -5,66 +5,34 @@ import (
 	"github.com/databricks/databricks-sdk-go/service/billing"
 	"github.com/databricks/terraform-provider-databricks/common"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
 
-type Tag struct {
-	Key   string                                   `json:"key"`
-	Value *billing.BudgetConfigurationFilterClause `json:"value"`
-}
-
-type WorkspaceFilter struct {
-	Operator string  `json:"operator,omitempty"`
-	Values   []int64 `json:"values,omitempty"`
-}
-
-type Filter struct {
-	WorkspaceId WorkspaceFilter `json:"workspace_id,omitempty"`
-	Tags        []Tag           `json:"tags,omitempty"`
-}
-
-type ActionConfiguration struct {
-	ActionType billing.ActionConfigurationType `json:"action_type,omitempty"`
-	Target     string                          `json:"target,omitempty"`
-}
-
-type AlertConfiguration struct {
-	ActionConfigurations []ActionConfiguration                  `json:"action_configurations,omitempty"`
-	QuantityThreshold    string                                 `json:"quantity_threshold,omitempty"`
-	QuantityType         billing.AlertConfigurationQuantityType `json:"quantity_type,omitempty"`
-	TimePeriod           billing.AlertConfigurationTimePeriod   `json:"time_period,omitempty"`
-	TriggerType          billing.AlertConfigurationTriggerType  `json:"trigger_type,omitempty"`
-}
-
-type Budget struct {
-	AccountId             string               `json:"account_id,omitempty" tf:"computed"`
-	BudgetConfigurationId string               `json:"budget_configuration_id,omitempty" tf:"computed"`
-	DisplayName           string               `json:"display_name,omitempty"`
-	AlertConfigurations   []AlertConfiguration `json:"alert_configurations,omitempty"`
-	Filter                *Filter              `json:"filter,omitempty"`
-}
-
 func ResourceMwsBudget() common.Resource {
-	s := common.StructToSchema(
-		Budget{},
-		common.NoCustomize,
-	)
+	s := common.StructToSchema(billing.BudgetConfiguration{}, func(m map[string]*schema.Schema) map[string]*schema.Schema {
+		common.CustomizeSchemaPath(m, "display_name").SetValidateFunc(validation.StringLenBetween(1, 128))
+		for _, p := range []string{"account_id", "budget_configuration_id", "create_time", "update_time"} {
+			common.CustomizeSchemaPath(m, p).SetComputed()
+		}
+		return m
+	})
 	p := common.NewPairSeparatedID("account_id", "budget_configuration_id", "/")
 	return common.Resource{
 		Create: func(ctx context.Context, d *schema.ResourceData, c *common.DatabricksClient) error {
-			var budget billing.CreateBudgetConfigurationBudget
-			common.DataToStructPointer(d, s, &budget)
+			var create billing.CreateBudgetConfigurationBudget
+			common.DataToStructPointer(d, s, &create)
 			acc, err := c.AccountClientWithAccountIdFromConfig(d)
 			if err != nil {
 				return err
 			}
-			req, err := acc.Budgets.Create(ctx, billing.CreateBudgetConfigurationRequest{
-				Budget: budget,
-			})
+			budget, err := acc.Budgets.Create(ctx, billing.CreateBudgetConfigurationRequest{Budget: create})
 			if err != nil {
 				return err
 			}
-			d.Set("budget_configuration_id", req.Budget.BudgetConfigurationId)
+			d.SetId(budget.Budget.BudgetConfigurationId)
+			d.Set("budget_configuration_id", budget.Budget.BudgetConfigurationId)
 			d.Set("account_id", c.Config.AccountID)
+			common.StructToData(budget, s, d)
 			p.Pack(d)
 			return nil
 		},
@@ -84,8 +52,7 @@ func ResourceMwsBudget() common.Resource {
 			if err != nil {
 				return err
 			}
-			err = acc.Budgets.DeleteByBudgetId(ctx, d.Get("budget_configuration_id").(string))
-			return err
+			return acc.Budgets.DeleteByBudgetId(ctx, d.Get("budget_configuration_id").(string))
 		},
 		Schema: s,
 	}

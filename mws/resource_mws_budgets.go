e70e844d8200c700b8dbcc47772eb4d213748a38
Cedric <23346008+840@users.noreply.github.com>
2024-09-02 13:58:31 +0200

    WIP on tests

--- mws/resource_mws_budgets.go
+++ mws/resource_mws_budgets.go
@@ -7,52 +7,53 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
+type Tag struct {
+	Key   string                                   `json:"key"`
+	Value *billing.BudgetConfigurationFilterClause `json:"value"`
+}
+
+type WorkspaceFilter struct {
+	Operator string  `json:"operator,omitempty"`
+	Values   []int64 `json:"values,omitempty"`
+}
+
+type Filter struct {
+	WorkspaceId WorkspaceFilter `json:"workspace_id,omitempty"`
+	Tags        []Tag           `json:"tags,omitempty"`
+}
+
+type ActionConfiguration struct {
+	ActionType billing.ActionConfigurationType `json:"action_type,omitempty"`
+	Target     string                          `json:"target,omitempty"`
+}
+
+type AlertConfiguration struct {
+	ActionConfigurations []ActionConfiguration                  `json:"action_configurations,omitempty"`
+	QuantityThreshold    string                                 `json:"quantity_threshold,omitempty"`
+	QuantityType         billing.AlertConfigurationQuantityType `json:"quantity_type,omitempty"`
+	TimePeriod           billing.AlertConfigurationTimePeriod   `json:"time_period,omitempty"`
+	TriggerType          billing.AlertConfigurationTriggerType  `json:"trigger_type,omitempty"`
+}
+
+type Budget struct {
+	AccountId             string               `json:"account_id,omitempty" tf:"computed"`
+	BudgetConfigurationId string               `json:"budget_configuration_id,omitempty" tf:"computed"`
+	DisplayName           string               `json:"display_name,omitempty"`
+	AlertConfigurations   []AlertConfiguration `json:"alert_configurations,omitempty"`
+	Filter                *Filter              `json:"filter,omitempty"`
+}
+
 func ResourceMwsBudget() common.Resource {
-	type Tag struct {
-		Key   string                                   `json:"key"`
-		Value *billing.BudgetConfigurationFilterClause `json:"value"`
-	}
-
-	type WorkspaceFilter struct {
-		Operator string  `json:"operator,omitempty"`
-		Values   []int64 `json:"values,omitempty"`
-	}
-
-	type Filter struct {
-		WorkspaceId WorkspaceFilter `json:"workspace_id,omitempty"`
-		Tags        []Tag           `json:"tags,omitempty"`
-	}
-
-	type ActionConfiguration struct {
-		ActionType billing.ActionConfigurationType `json:"action_type,omitempty"`
-		Target     string                          `json:"target,omitempty"`
-	}
-
-	type AlertConfiguration struct {
-		ActionConfigurations []ActionConfiguration                  `json:"action_configurations,omitempty"`
-		QuantityThreshold    string                                 `json:"quantity_threshold,omitempty"`
-		QuantityType         billing.AlertConfigurationQuantityType `json:"quantity_type,omitempty"`
-		TimePeriod           billing.AlertConfigurationTimePeriod   `json:"time_period,omitempty"`
-		TriggerType          billing.AlertConfigurationTriggerType  `json:"trigger_type,omitempty"`
-	}
-
-	type Budget struct {
-		BudgetConfigurationId string               `json:"budget_configuration_id,omitempty" tf:"computed"`
-		DisplayName           string               `json:"display_name,omitempty" tf:"computed"`
-		AlertConfigurations   []AlertConfiguration `json:"alert_configurations,omitempty"`
-		Filter                *Filter              `json:"filter,omitempty"`
-	}
-
 	s := common.StructToSchema(
 		Budget{},
 		common.NoCustomize,
 	)
-
+	p := common.NewPairSeparatedID("account_id", "budget_configuration_id", "/")
 	return common.Resource{
 		Create: func(ctx context.Context, d *schema.ResourceData, c *common.DatabricksClient) error {
 			var budget billing.CreateBudgetConfigurationBudget
 			common.DataToStructPointer(d, s, &budget)
-			acc, err := c.AccountClient()
+			acc, err := c.AccountClientWithAccountIdFromConfig(d)
 			if err != nil {
 				return err
 			}
@@ -62,7 +63,9 @@ func ResourceMwsBudget() common.Resource {
 			if err != nil {
 				return err
 			}
-			d.SetId(req.Budget.BudgetConfigurationId)
+			d.Set("budget_configuration_id", req.Budget.BudgetConfigurationId)
+			d.Set("account_id", c.Config.AccountID)
+			p.Pack(d)
 			return nil
 		},
 		Read: func(ctx context.Context, d *schema.ResourceData, c *common.DatabricksClient) error {
@@ -70,18 +73,18 @@ func ResourceMwsBudget() common.Resource {
 			if err != nil {
 				return err
 			}
-			res, err := acc.Budgets.GetByBudgetId(ctx, d.Id())
+			res, err := acc.Budgets.GetByBudgetId(ctx, d.Get("budget_configuration_id").(string))
 			if err != nil {
 				return err
 			}
-			return common.StructToData(*res.Budget, s, d)
+			return common.StructToData(res.Budget, s, d)
 		},
 		Delete: func(ctx context.Context, d *schema.ResourceData, c *common.DatabricksClient) error {
 			acc, err := c.AccountClient()
 			if err != nil {
 				return err
 			}
-			err = acc.Budgets.DeleteByBudgetId(ctx, d.Id())
+			err = acc.Budgets.DeleteByBudgetId(ctx, d.Get("budget_configuration_id").(string))
 			return err
 		},
 		Schema: s,
---
+++ mws/resource_mws_budgets_test.go
@@ -0,0 +1,100 @@
+package mws
+
+import (
+	"github.com/databricks/databricks-sdk-go/experimental/mocks"
+	"github.com/databricks/databricks-sdk-go/service/billing"
+	"github.com/stretchr/testify/mock"
+	"testing"
+
+	"github.com/databricks/terraform-provider-databricks/qa"
+)
+
+func getTestBudget() *billing.BudgetConfiguration {
+	return &billing.BudgetConfiguration{
+		AccountId: "account_id",
+		AlertConfigurations: []billing.AlertConfiguration{
+			{
+				ActionConfigurations: []billing.ActionConfiguration{
+					{
+						ActionType: billing.ActionConfigurationTypeEmailNotification,
+						Target:     "me@databricks.com",
+					},
+				},
+				AlertConfigurationId: "alert_configuration_id",
+				QuantityThreshold:    "840",
+				QuantityType:         billing.AlertConfigurationQuantityTypeListPriceDollarsUsd,
+				TimePeriod:           billing.AlertConfigurationTimePeriodMonth,
+				TriggerType:          billing.AlertConfigurationTriggerTypeCumulativeSpendingExceeded,
+			},
+		},
+		Filter: &billing.BudgetConfigurationFilter{
+			Tags: nil,
+			WorkspaceId: &billing.BudgetConfigurationFilterWorkspaceIdClause{
+				Operator: billing.BudgetConfigurationFilterOperatorIn,
+				Values: []int64{
+					1234567890098765,
+				},
+			},
+		},
+		BudgetConfigurationId: "budget_configuration_id",
+		DisplayName:           "budget_name",
+	}
+}
+
+func TestResourceBudgetCreate(t *testing.T) {
+	qa.ResourceFixture{
+		MockAccountClientFunc: func(a *mocks.MockAccountClient) {
+			api := a.GetMockbudgetsAPI().EXPECT()
+			api.Create(mock.Anything, billing.CreateBudgetConfigurationRequest{
+				Budget: billing.CreateBudgetConfigurationBudget{
+					AlertConfigurations: []billing.CreateBudgetConfigurationBudgetAlertConfigurations{
+						{
+							ActionConfigurations: []billing.CreateBudgetConfigurationBudgetActionConfigurations{
+								{
+									ActionType: billing.ActionConfigurationTypeEmailNotification,
+									Target:     "me@databricks.com",
+								},
+							},
+							QuantityThreshold: "840",
+							QuantityType:      billing.AlertConfigurationQuantityTypeListPriceDollarsUsd,
+							TimePeriod:        billing.AlertConfigurationTimePeriodMonth,
+							TriggerType:       billing.AlertConfigurationTriggerTypeCumulativeSpendingExceeded,
+						},
+					},
+					DisplayName: getTestBudget().DisplayName,
+					Filter:      getTestBudget().Filter,
+				},
+			}).Return(&billing.CreateBudgetConfigurationResponse{Budget: getTestBudget()}, nil)
+			api.GetByBudgetId(mock.Anything, "budget_configuration_id").Return(
+				&billing.GetBudgetConfigurationResponse{Budget: getTestBudget()}, nil,
+			)
+		},
+		Create:    true,
+		AccountID: "account_id",
+		HCL: `
+		display_name = "budget_name"
+	
+		alert_configurations {
+			time_period         = "MONTH"
+			trigger_type        = "CUMULATIVE_SPENDING_EXCEEDED" 
+			quantity_type       = "LIST_PRICE_DOLLARS_USD"
+			quantity_threshold  = "840"
+	
+			action_configurations {
+				action_type = "EMAIL_NOTIFICATION"
+				target      = "me@databricks.com"
+			}
+		}
+	
+		filter {
+			workspace_id {
+				operator = "IN"
+				values   = [
+					1234567890098765
+				]                              
+			}
+		}
+		`,
+		Resource: ResourceMwsBudget(),
+	}.ApplyAndExpectData(t, nil) // ???
+}

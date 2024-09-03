fe44b95b644c9086f89e8ae139656233f7360434
Cedric <23346008+840@users.noreply.github.com>
2024-09-03 11:37:51 +0200

    Rename files to singular, add acceptance test

---
+++ internal/acceptance/mws_budget_test.go
@@ -0,0 +1,40 @@
+package acceptance
+
+import "testing"
+
+func TestMwsAccBudget(t *testing.T) {
+	accountLevel(t, step{
+		Template: `resource "databricks_mws_budget" "this" {
+			display_name = "tf-{var.RANDOM}"
+		
+			alert_configurations {
+				time_period         = "MONTH"
+				trigger_type        = "CUMULATIVE_SPENDING_EXCEEDED" 
+				quantity_type       = "LIST_PRICE_DOLLARS_USD"
+				quantity_threshold  = "840"
+		
+				action_configurations {
+					action_type = "EMAIL_NOTIFICATION"
+					target      = "me@databricks.com"
+				}
+			}
+	
+			filter {
+				tags {
+					key   = "Environment"
+					value {
+						operator = "IN"
+						values = ["Testing"]
+					}
+				}
+	
+				workspace_id {
+					operator = "IN"
+					values   = [
+						{env.DUMMY_WORKSPACE_ID}
+					]                              
+				}
+			}
+		}`,
+	})
+}

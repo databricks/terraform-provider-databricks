c7f32db7886ff8091c52ad26b9d0416eb2ced347
Cedric <23346008+840@users.noreply.github.com>
2024-09-06 12:45:32 +0200

    Use `SetCustomSuppressDiff` to detect drift for `quantity_threshold` rather than converting BigDecimals

--- mws/resource_mws_budget.go
+++ mws/resource_mws_budget.go
@@ -2,12 +2,10 @@ package mws
 
 import (
 	"context"
-	"fmt"
 	"github.com/databricks/databricks-sdk-go/service/billing"
 	"github.com/databricks/terraform-provider-databricks/common"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	"strconv"
 	"strings"
 )
 
@@ -19,6 +17,16 @@ func ResourceMwsBudget() common.Resource {
 		}
 		common.CustomizeSchemaPath(m, "alert_configurations", "alert_configuration_id").SetComputed()
 		common.CustomizeSchemaPath(m, "alert_configurations", "action_configurations", "action_configuration_id").SetComputed()
+		common.CustomizeSchemaPath(m, "alert_configurations", "quantity_threshold").SetCustomSuppressDiff(func(k, old, new string, d *schema.ResourceData) bool {
+			normalize := func(v string) string {
+				if strings.Contains(v, ".") {
+					v = strings.TrimRight(v, "0")
+					v = strings.TrimSuffix(v, ".")
+				}
+				return v
+			}
+			return normalize(old) == normalize(new)
+		})
 		return m
 	})
 	p := common.NewPairID("account_id", "budget_configuration_id")
@@ -26,13 +34,6 @@ func ResourceMwsBudget() common.Resource {
 		Create: func(ctx context.Context, d *schema.ResourceData, c *common.DatabricksClient) error {
 			var create billing.CreateBudgetConfigurationBudget
 			common.DataToStructPointer(d, s, &create)
-			for i := range create.AlertConfigurations {
-				var err error
-				create.AlertConfigurations[i].QuantityThreshold, err = StringToBigDecimal(create.AlertConfigurations[i].QuantityThreshold)
-				if err != nil {
-					return err
-				}
-			}
 			acc, err := c.AccountClient()
 			if err != nil {
 				return err
@@ -60,12 +61,6 @@ func ResourceMwsBudget() common.Resource {
 			if err != nil {
 				return err
 			}
-			for i := range budget.Budget.AlertConfigurations {
-				budget.Budget.AlertConfigurations[i].QuantityThreshold, err = BigDecimalToString(budget.Budget.AlertConfigurations[i].QuantityThreshold)
-				if err != nil {
-					return err
-				}
-			}
 			return common.StructToData(budget.Budget, s, d)
 		},
 		Update: func(ctx context.Context, d *schema.ResourceData, c *common.DatabricksClient) error {
@@ -75,12 +70,6 @@ func ResourceMwsBudget() common.Resource {
 				return err
 			}
 			common.DataToStructPointer(d, s, &update)
-			for i := range update.AlertConfigurations {
-				update.AlertConfigurations[i].QuantityThreshold, err = StringToBigDecimal(update.AlertConfigurations[i].QuantityThreshold)
-				if err != nil {
-					return err
-				}
-			}
 			acc, err := c.AccountClient()
 			if err != nil {
 				return err
@@ -107,43 +96,4 @@ func ResourceMwsBudget() common.Resource {
 		},
 		Schema: s,
 	}
-}
-
-// API always return a BigDecimal for QuantityThreshold in string representation.
-// To avoid drift, we convert the provided number to BigDecimal in string representation
-// when making requests to the API.
-func BigDecimalToString(input string) (string, error) {
-	if input == "" {
-		return "", fmt.Errorf("input cannot be empty")
-	}
-
-	if strings.Contains(input, ".") {
-		input = strings.TrimRight(input, "0")
-		input = strings.TrimSuffix(input, ".")
-	}
-	return input, nil
-}
-
-func StringToBigDecimal(input string) (string, error) {
-	if input == "" {
-		return "", fmt.Errorf("input cannot be empty")
-	}
-
-	if _, err := strconv.ParseFloat(input, 64); err != nil {
-		return "", fmt.Errorf("invalid input string")
-	}
-
-	if !strings.Contains(input, ".") {
-		return input + ".000000000000000000", nil
-	}
-
-	parts := strings.SplitN(input, ".", 2)
-	decimalPart := parts[1]
-
-	if len(decimalPart) < 18 {
-		decimalPart += strings.Repeat("0", 18-len(decimalPart))
-	} else {
-		decimalPart = decimalPart[:18]
-	}
-	return parts[0] + "." + decimalPart, nil
 }

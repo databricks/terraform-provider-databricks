b13a6d49770a38db392896321b55b14114acd41f
Alex Ott <alexott@gmail.com>
2024-09-16 18:09:16 +0200

    [Exporter] Expand list of non-interactive clusters
    So we can avoid generation of not related cluster resources

--- exporter/importables.go
+++ exporter/importables.go
@@ -327,7 +327,7 @@ var resourcesMap map[string]importable = map[string]importable{
 				return err
 			}
 			lastActiveMs := ic.getLastActiveMs()
-			nonInteractiveClusters := []string{"JOB", "PIPELINE_MAINTENANCE", "PIPELINE", "SQL"}
+			nonInteractiveClusters := []string{"JOB", "MODELS", "PIPELINE_MAINTENANCE", "PIPELINE", "SQL"}
 			for offset, c := range clusters {
 				if slices.Contains(nonInteractiveClusters, string(c.ClusterSource)) {
 					// TODO: Should we check cluster name as well?

300991035b3d62c5c8f36db8661f07c4eb7a0611
dgomez04 <dagomezmoreno@outlook.com>
2024-09-20 10:07:31 -0600

    renamed resource func to DataSourceUsers, removed various acceptance tests, and added the resource definition into the provider

--- internal/acceptance/data_users_test.go
+++ internal/acceptance/data_users_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 )
 
-func TestAccDataSourceDataUsers_DisplayNameContains(t *testing.T) {
+func TestAccDataSourceDataUsers(t *testing.T) {
 	accountLevel(t, step{
 		Template: `
 		data "databricks_data_users" "this" {
@@ -21,45 +21,6 @@ func TestAccDataSourceDataUsers_DisplayNameContains(t *testing.T) {
 			ids := r.Primary.Attributes["users.#"]
 			if ids == "" {
 				return fmt.Errorf("users is empty: %v", r.Primary.Attributes)
-			}
-			return nil
-		},
-	})
-}
-
-func TestAccDataSourceDataUsers_UserNameContains(t *testing.T) {
-	accountLevel(t, step{
-		Template: `
-		data "databricks_data_users" "this" {
-			user_name_contains = "example.com"
-		}`,
-		Check: func(s *terraform.State) error {
-			r, ok := s.RootModule().Resources["data.databricks_data_users.this"]
-			if !ok {
-				return fmt.Errorf("data not found in state")
-			}
-			usersCount := r.Primary.Attributes["users.#"]
-			if usersCount == "" || usersCount == "0" {
-				return fmt.Errorf("users list is empty: %v", r.Primary.Attributes)
-			}
-			return nil
-		},
-	})
-}
-
-func TestAccDataSourceDataUsers_NoFilters(t *testing.T) {
-	accountLevel(t, step{
-		Template: `
-		data "databricks_data_users" "this" {
-		}`,
-		Check: func(s *terraform.State) error {
-			r, ok := s.RootModule().Resources["data.databricks_data_users.this"]
-			if !ok {
-				return fmt.Errorf("data not found in state")
-			}
-			usersCount := r.Primary.Attributes["users.#"]
-			if usersCount == "" || usersCount == "0" {
-				return fmt.Errorf("users list is empty: %v", r.Primary.Attributes)
 			}
 			return nil
 		},

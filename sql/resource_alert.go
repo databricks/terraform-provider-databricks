91a4e459abc9735c5b3781a2cd6b2bf5ecd5937f
Alex Ott <alexott@gmail.com>
2024-09-27 13:16:38 +0200

    [Feature] Add `databricks_alert` resource to replace `databricks_sql_alert`
    New resource use the [new Alerts API](https://docs.databricks.com/api/workspace/alerts/create)
    instead of legacy one that will be deprecated. New resource has slightly different set of
    parameters, so it was decided to create new resource and deprecate old one.
    
    This resource uses old TF SDK to be compatible with TF exporter (until #4050 is implemented).

--- sql/resource_alert.go
+++ sql/resource_alert.go
@@ -19,7 +19,6 @@ func ResourceAlert() common.Resource {
 		// TODO: can we automatically generate it from SDK? Or should we avoid validation at all?
 		common.CustomizeSchemaPath(m, "condition", "op").SetRequired().SetValidateFunc(validation.StringInSlice([]string{
 			"GREATER_THAN", "GREATER_THAN_OR_EQUAL", "LESS_THAN", "LESS_THAN_OR_EQUAL", "EQUAL", "NOT_EQUAL", "IS_NULL"}, true))
-		common.CustomizeSchemaPath(m, "condition", "op").SetRequired()
 		common.CustomizeSchemaPath(m, "parent_path").SetCustomSuppressDiff(common.WorkspaceOrEmptyPathPrefixDiffSuppress).SetForceNew()
 		common.CustomizeSchemaPath(m, "condition", "operand").SetRequired()
 		common.CustomizeSchemaPath(m, "condition", "operand", "column").SetRequired()

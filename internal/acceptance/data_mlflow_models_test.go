d8823073168246c52b60972e03d7fe311f2efa87
Alex Ott <alexott@gmail.com>
2024-10-02 08:38:41 -0400

    Remove not necessary step in the acceptance test

--- internal/acceptance/data_mlflow_models_test.go
+++ internal/acceptance/data_mlflow_models_test.go
@@ -24,23 +24,6 @@ func TestAccDataMlflowModels(t *testing.T) {
 				key   = "key2"
 				value = "value2"
 			  }
-			}`,
-		},
-		step{
-			Template: `
-			resource "databricks_mlflow_model" "this" {
-			  name = "model-{var.RANDOM}"
-			
-			  description = "My MLflow model description"
-			
-			  tags {
-				key   = "key1"
-				value = "value1"
-			  }
-			  tags {
-				key   = "key2"
-				value = "value2"
-			  }
 			}			
 
 			data "databricks_mlflow_models" "this" {

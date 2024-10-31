f94dfd885f9206bb021d3097b91786a8d2b29900
dgomez04 <dagomezmoreno@outlook.com>
2024-10-31 16:43:25 -0600

    started working on CRUD operations for functions resource.

--- internal/providers/pluginfw/resources/catalog/resource_function.go
+++ internal/providers/pluginfw/resources/catalog/resource_function.go
@@ -5,8 +5,10 @@ import (
 
 	"github.com/databricks/terraform-provider-databricks/common"
 	pluginfwcommon "github.com/databricks/terraform-provider-databricks/internal/providers/pluginfw/common"
+	pluginfwcontext "github.com/databricks/terraform-provider-databricks/internal/providers/pluginfw/context"
 	"github.com/databricks/terraform-provider-databricks/internal/providers/pluginfw/tfschema"
 	"github.com/databricks/terraform-provider-databricks/internal/service/catalog_tf"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 )
@@ -32,7 +34,7 @@ func (r *FunctionResource) Schema(ctx context.Context, req resource.SchemaReques
 	attrs, blocks := tfschema.ResourceStructToSchemaMap(catalog_tf.FunctionInfo{}, nil)
 
 	resp.Schema = schema.Schema{
-		Description: "Terraform schema for Databricks Functions",
+		Description: "Terraform schema for Databricks Function",
 		Attributes:  attrs,
 		Blocks:      blocks,
 	}
@@ -42,4 +44,30 @@ func (r *FunctionResource) Configure(ctx context.Context, req resource.Configure
 	if r.Client == nil && req.ProviderData != nil {
 		r.Client = pluginfwcommon.ConfigureResource(req, resp)
 	}
+}
+
+/* Is there a way I can make this general, accept any type of Response? There seems to be no base class that relates them... */
+func AppendDiagAndCheckErrors(resp *resource.CreateResponse, diags diag.Diagnostics) bool {
+	resp.Diagnostics.Append(diags...)
+	return resp.Diagnostics.HasError()
+}
+
+func (r *FunctionResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	ctx = pluginfwcontext.SetUserAgentInResourceContext(ctx, resourceName)
+	w, diags := r.Client.GetWorkspaceClient()
+	if AppendDiagAndCheckErrors(resp, diags) {
+		return
+	}
+}
+
+func (r *FunctionResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	/* TODO */
+}
+
+func (r *FunctionResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	/* TODO */
+}
+
+func (r *FunctionResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	/* TODO */
 }

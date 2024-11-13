d9bb13bd338747427615313e6f795ef3526787bc
xinyijiang-db <158105497+xinyijiang-db@users.noreply.github.com>
2024-11-13 15:08:16 -0800

    [ML-47387] fix UpdateMonitor failure

--- catalog/resource_quality_monitor.go
+++ catalog/resource_quality_monitor.go
@@ -53,7 +53,6 @@ func ResourceQualityMonitor() common.Resource {
 			common.CustomizeSchemaPath(m, "profile_metrics_table_name").SetReadOnly()
 			common.CustomizeSchemaPath(m, "status").SetReadOnly()
 			common.CustomizeSchemaPath(m, "dashboard_id").SetReadOnly()
-			
 			return m
 		},
 	)

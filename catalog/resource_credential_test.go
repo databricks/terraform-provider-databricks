ae215ef4423142e2e1c637cfda17d4dc3b6183ad
Vuong <vuong.nguyen@databricks.com>
2024-11-14 13:42:07 +0700

    fix test again

--- catalog/resource_credential_test.go
+++ catalog/resource_credential_test.go
@@ -23,11 +23,13 @@ func TestCreateCredential(t *testing.T) {
 					RoleArn: "def",
 				},
 				Comment: "c",
+				Purpose: "SERVICE",
 			}).Return(&catalog.CredentialInfo{
 				Name: "a",
 				AwsIamRole: &catalog.AwsIamRole{
 					RoleArn: "def",
 				},
+				Purpose: "SERVICE",
 				Comment: "c",
 			}, nil)
 			e.GetCredentialByNameArg(mock.Anything, "a").Return(&catalog.CredentialInfo{
@@ -36,6 +38,7 @@ func TestCreateCredential(t *testing.T) {
 					RoleArn:    "def",
 					ExternalId: "123",
 				},
+				Purpose:       "SERVICE",
 				MetastoreId:   "d",
 				Id:            "1234-5678",
 				Owner:         "f",
@@ -49,12 +52,14 @@ func TestCreateCredential(t *testing.T) {
 		aws_iam_role {
 			role_arn = "def"
 		}
+		purpose = "SERVICE"
 		comment = "c"
 		`,
 	}.ApplyAndExpectData(t, map[string]any{
 		"aws_iam_role.0.external_id": "123",
 		"aws_iam_role.0.role_arn":    "def",
 		"name":                       "a",
+		"purpose":                    "SERVICE",
 	})
 }
 
@@ -68,12 +73,14 @@ func TestCreateIsolatedCredential(t *testing.T) {
 					RoleArn: "def",
 				},
 				Comment: "c",
+				Purpose: "SERVICE",
 			}).Return(&catalog.CredentialInfo{
 				Name: "a",
 				AwsIamRole: &catalog.AwsIamRole{
 					RoleArn:    "def",
 					ExternalId: "123",
 				},
+				Purpose:     "SERVICE",
 				MetastoreId: "d",
 				Id:          "1234-5678",
 				Owner:       "f",
@@ -91,6 +98,7 @@ func TestCreateIsolatedCredential(t *testing.T) {
 					RoleArn:    "def",
 					ExternalId: "123",
 				},
+				Purpose:       "SERVICE",
 				MetastoreId:   "d",
 				Id:            "1234-5678",
 				Owner:         "f",
@@ -123,6 +131,7 @@ func TestCreateIsolatedCredential(t *testing.T) {
 					RoleArn:    "def",
 					ExternalId: "123",
 				},
+				Purpose:       "SERVICE",
 				MetastoreId:   "d",
 				Id:            "1234-5678",
 				Owner:         "f",
@@ -137,6 +146,7 @@ func TestCreateIsolatedCredential(t *testing.T) {
 			role_arn = "def"
 		}
 		comment = "c"
+		purpose = "SERVICE"
 		isolation_mode = "ISOLATION_MODE_ISOLATED"
 		`,
 	}.ApplyAndExpectData(t, map[string]any{

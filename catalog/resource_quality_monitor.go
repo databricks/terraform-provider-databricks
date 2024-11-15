d76766523374936de7491a12f44dabbcdd16044e
xinyijiang-db <158105497+xinyijiang-db@users.noreply.github.com>
2024-11-15 11:01:38 -0800

    Update resource_quality_monitor.go

--- catalog/resource_quality_monitor.go
+++ catalog/resource_quality_monitor.go
@@ -100,6 +100,7 @@ func ResourceQualityMonitor() common.Resource {
 			var update catalog.UpdateMonitor
 			common.DataToStructPointer(d, monitorSchema, &update)
 			update.TableName = d.Get("table_name").(string)
+			updateMonitorGoSDK.PauseStatus = ""
 			_, err = w.QualityMonitors.Update(ctx, update)
 			if err != nil {
 				return err

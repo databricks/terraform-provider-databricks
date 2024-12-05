5b52d7700802098ef4ce6a28606aa08fc19ecfdb
Miles Yucht <miles@databricks.com>
2024-12-05 21:07:23 +0100

    fix

--- internal/providers/pluginfw/tfschema/nested_block_object.go
+++ internal/providers/pluginfw/tfschema/nested_block_object.go
@@ -28,19 +28,6 @@ func (a NestedBlockObject) ToNestedAttributeObject() NestedAttributeObject {
 	}
 }
 
-func (a NestedBlockObject) ToNestedAttributeObject() NestedAttributeObject {
-	attributes := make(map[string]AttributeBuilder)
-	for k, v := range a.Attributes {
-		attributes[k] = v
-	}
-	for k, v := range a.Blocks {
-		attributes[k] = v.ToAttribute()
-	}
-	return NestedAttributeObject{
-		Attributes: attributes,
-	}
-}
-
 func (a NestedBlockObject) BuildDataSourceAttribute() dataschema.NestedBlockObject {
 	dataSourceAttributes := BuildDataSourceAttributeMap(a.Attributes)
 	dataSourceBlocks := BuildDataSourceBlockMap(a.Blocks)

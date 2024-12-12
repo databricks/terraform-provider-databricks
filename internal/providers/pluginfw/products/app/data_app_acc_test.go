52e975cff8b7946a21dad3d175bff155eb26ded1
Miles Yucht <miles@databricks.com>
2024-12-12 14:51:18 +0100

    fast app

--- internal/providers/pluginfw/products/app/data_app_acc_test.go
+++ internal/providers/pluginfw/products/app/data_app_acc_test.go
@@ -6,13 +6,38 @@ import (
 	"github.com/databricks/terraform-provider-databricks/internal/acceptance"
 )
 
+const fastApp = `
+	resource "databricks_secret_scope" "this" {
+		name = "tf-{var.STICKY_RANDOM}"
+	}
+
+	resource "databricks_secret" "this" {
+	    scope = databricks_secret_scope.this.name
+		key = "tf-{var.STICKY_RANDOM}"
+		string_value = "secret"
+	}
+
+	resource "databricks_app" "this" {
+		name = "{var.STICKY_RANDOM}"
+		description = "%s"
+		resources = [{
+			name = "secret"
+			description = "secret for app"
+			secret = {
+				scope = databricks_secret_scope.this.name
+				key = databricks_secret.this.key
+				permission = "MANAGE"
+			}
+		}]
+	}`
+
 func TestAccAppDataSource(t *testing.T) {
 	acceptance.LoadWorkspaceEnv(t)
 	if acceptance.IsGcp(t) {
 		acceptance.Skipf(t)("not available on GCP")
 	}
 	acceptance.WorkspaceLevel(t, acceptance.Step{
-		Template: makeTemplate("My app") + `
+		Template: fastApp + `
 		data "databricks_app" "this" {
 			name = databricks_app.this.name
 		}

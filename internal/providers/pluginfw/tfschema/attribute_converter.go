402f40a07a5086d94da5002ad815f04c6095e455
Miles Yucht <miles@databricks.com>
2024-12-12 10:51:22 +0100

    docs

--- internal/providers/pluginfw/tfschema/attribute_converter.go
+++ internal/providers/pluginfw/tfschema/attribute_converter.go
@@ -1,9 +1,13 @@
 package tfschema
 
+// Blockable is an interface that can be implemented by an AttributeBuilder to convert it to a BlockBuilder.
 type Blockable interface {
+	// ToBlock converts the AttributeBuilder to a BlockBuilder.
 	ToBlock() BlockBuilder
 }
 
+// convertAttributesToBlocks converts all attributes implementing the Blockable interface to blocks, returning
+// a new NestedBlockObject with the converted attributes and the original blocks.
 func convertAttributesToBlocks(attributes map[string]AttributeBuilder, blocks map[string]BlockBuilder) NestedBlockObject {
 	newAttributes := make(map[string]AttributeBuilder)
 	newBlocks := make(map[string]BlockBuilder)

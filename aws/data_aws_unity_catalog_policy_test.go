044f9b2f7b639144d2f2b34f45d16d58794ba6ae
Alex Ott <alexott@gmail.com>
2025-01-28 08:55:34 +0100

    Fix all tests

--- aws/data_aws_unity_catalog_policy_test.go
+++ aws/data_aws_unity_catalog_policy_test.go
@@ -149,7 +149,10 @@ func TestDataAwsUnityCatalogPolicyFullKms(t *testing.T) {
                 "s3:PutObject",
                 "s3:DeleteObject",
                 "s3:ListBucket",
-                "s3:GetBucketLocation"
+                "s3:GetBucketLocation",
+                "s3:ListBucketMultipartUploads",
+					      "s3:ListMultipartUploadParts",
+					      "s3:AbortMultipartUpload"
               ],
               "Resource": [
                 "arn:aws:s3:::databricks-bucket/*",
@@ -261,7 +264,10 @@ func TestDataAwsUnityCatalogPolicyWithoutKMS(t *testing.T) {
                 "s3:PutObject",
                 "s3:DeleteObject",
                 "s3:ListBucket",
-                "s3:GetBucketLocation"
+                "s3:GetBucketLocation",
+                "s3:ListBucketMultipartUploads",
+					      "s3:ListMultipartUploadParts",
+					      "s3:AbortMultipartUpload"
               ],
               "Resource": [
                 "arn:aws:s3:::databricks-bucket/*",
@@ -364,7 +370,10 @@ func TestDataAwsUnityCatalogPolicyPartionGov(t *testing.T) {
                 "s3:PutObject",
                 "s3:DeleteObject",
                 "s3:ListBucket",
-                "s3:GetBucketLocation"
+                "s3:GetBucketLocation",
+                "s3:ListBucketMultipartUploads",
+					      "s3:ListMultipartUploadParts",
+					      "s3:AbortMultipartUpload"
               ],
               "Resource": [
                 "arn:aws-us-gov:s3:::databricks-bucket/*",

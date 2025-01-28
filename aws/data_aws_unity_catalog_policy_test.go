3170de8144dd5013f8049cf9dc2b2393ae2626bb
Alex Ott <alexott@gmail.com>
2025-01-28 08:44:01 +0100

    [Feature] Add multipart permissions to `databricks_aws_unity_catalog_policy` data source
    To support uploading of big files to UC volumes, etc. we need to give permissions to
    perform operations related to AWS multipart uploads APIs.

--- aws/data_aws_unity_catalog_policy_test.go
+++ aws/data_aws_unity_catalog_policy_test.go
@@ -33,7 +33,10 @@ func TestDataAwsUnityCatalogPolicy(t *testing.T) {
                 "s3:PutObject",
                 "s3:DeleteObject",
                 "s3:ListBucket",
-                "s3:GetBucketLocation"
+                "s3:GetBucketLocation",
+                "s3:ListBucketMultipartUploads",
+					      "s3:ListMultipartUploadParts",
+					      "s3:AbortMultipartUpload"
               ],
               "Resource": [
                 "arn:aws:s3:::databricks-bucket/*",

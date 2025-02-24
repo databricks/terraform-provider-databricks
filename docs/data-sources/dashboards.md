1699b58967a17e90e0cf22445bd1844ef6908b2b
Vuong <vuong.nguyen@databricks.com>
2025-02-24 18:02:22 +0800

    add `databricks_dashboards` data source

---
+++ docs/data-sources/dashboards.md
@@ -0,0 +1,43 @@
+---
+subcategory: "Workspace"
+---
+# databricks_dashboards Data Source
+
+This data source allows you to retrieve information about Databricks [Dashboards](https://docs.databricks.com/en/dashboards/index.html).
+
+## Example Usage
+
+```hcl
+data "databricks_dashboards" "all" {
+}
+
+resource "databricks_permissions" "dashboards_permissions" {
+  depends = [ data.databricks_dashboards.all ]
+  for_each = data.databricks_dashboards.this.dashboards[*].dashboard_id
+
+  dashboard_id = each.value
+
+  access_control {
+    group_name       = "Example Group"
+    permission_level = "CAN_MANAGE"
+  }
+}
+
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `display_name_contains` - (Optional) A **case-insensitive** substring to filter Dashboards by their display name.
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `dashboards` - A list of dashboards matching the specified criteria. Each element contains the following attributes:
+  * `dashboard_id` - The unique ID of the dashboard.
+  * `display_name` - The display name of the dashboard.
+  * `create_time` - The timestamp of when the dashboard was created.
+
+If no matches are found, an empty list will be returned.
---
+++ internal/providers/pluginfw/products/dashboards/data_dashboards_acc_test.go
@@ -0,0 +1,43 @@
+package dashboards_test
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/databricks/terraform-provider-databricks/internal/acceptance"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/stretchr/testify/require"
+)
+
+func CheckDataSourceDashboardsPopulated(t *testing.T) func(s *terraform.State) error {
+	return func(s *terraform.State) error {
+		ds, ok := s.Modules[0].Resources["data.databricks_dashboards.this"]
+		require.True(t, ok, "data.databricks_dashboards.this has to be there")
+
+		dashboardsCount := ds.Primary.Attributes["databricks_dashboards.#"]
+		dashboardsCountInt, err := strconv.Atoi(dashboardsCount)
+		require.NoError(t, err, "dashboards count is not a number")
+		require.NotEqual(t, 0, dashboardsCountInt, "dashboard list is empty")
+
+		return nil
+	}
+}
+
+func TestAccDashboardsCreation(t *testing.T) {
+	acceptance.WorkspaceLevel(t, acceptance.Step{
+		Template: `
+			resource "databricks_dashboard" "dashboard" {
+				display_name         = "New Dashboard"
+				warehouse_id         = data.databricks_sql_warehouse.starter.id
+				serialized_dashboard = "{\"pages\":[{\"name\":\"new_name\",\"displayName\":\"New Page\"}]}"
+				embed_credentials    = false // Optional
+				parent_path          = "/Shared/provider-test"
+			}
+
+			data "databricks_dashboards" "this" {
+				depends_on = [databricks_dashboard.dashboard]
+			}
+		`,
+		Check: CheckDataSourceDashboardsPopulated(t),
+	})
+}

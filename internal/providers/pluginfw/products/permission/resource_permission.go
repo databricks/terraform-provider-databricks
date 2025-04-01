020a48b4d7e9a6ff0176d8e28447fb845244da3d
Tanchwa <dres2dogs@gmail.com>
2025-04-01 10:50:12 -0400

    started outline for create method, set option to configure workspace or account clients in Configure method

--- internal/providers/pluginfw/products/permission/resource_permission.go
+++ internal/providers/pluginfw/products/permission/resource_permission.go
@@ -10,13 +10,15 @@ import (
 	"github.com/databricks/databricks-sdk-go/apierr"
 	"github.com/databricks/databricks-sdk-go/service/iam"
 	"github.com/databricks/terraform-provider-databricks/common"
-  pluginfwcommon "github.com/databricks/terraform-provider-databricks/internal/providers/pluginfw/common"
+	pluginfwcommon "github.com/databricks/terraform-provider-databricks/internal/providers/pluginfw/common"
+	pluginfwcontext "github.com/databricks/terraform-provider-databricks/internal/providers/pluginfw/context"
 	"github.com/databricks/terraform-provider-databricks/internal/providers/pluginfw/tfschema"
 	"github.com/databricks/terraform-provider-databricks/permissions/entity"
 
+	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
-  "github.com/hashicorp/terraform-plugin-framework/resource/schema"
-  "github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 const resourceName = "permission"
@@ -40,34 +42,49 @@ func (r *permissionResource) Configure(ctx context.Context, req resource.Configu
                 return
         }
 
-        client, ok := req.ProviderData.(*common.DatabricksClient)
+        var provider 
+        //TODO CHECK IF ACCOUNTID ATTRIBUTE IS SET IN PROVIDER CONFIG STRUCT,
+        // SHOULD BE ABLE TO PULL IF IT'S SET OR NOT FROM THE GOLANG STRUCT ITSELF
+        if != "" {
+                accountClient, err := req.ProviderData.(*common.DatabricksClient).AccountClient()
+                if err != nil {
+                        resp.Diagnostics.AddError(
+                                //TODO ADD ERROR MESSAGE IN LINE WITH PROVIDER
+                                "Unable to configure the Databricks client",
+                                fmt.Sprintf("Expected *common.DatabricksClient, got %T", req.ProviderData),
+                        )
+                r.client = accountClient
+                }
 
-        if !ok {
-                resp.Diagnostics.AddError(
-                        //TODO ADD ERROR MESSAGE IN LINE WITH PROVIDER
-                        "Unable to configure the Databricks client",
-                        fmt.Sprintf("Expected *common.DatabricksClient, got %T", req.ProviderData),
-                )
+        } else {
+                workspaceClient, err := req.ProviderData.(*common.DatabricksClient).WorkspaceClient()
+                if err != nil {
+                        resp.Diagnostics.AddError(
+                                //TODO ADD ERROR MESSAGE IN LINE WITH Provider
+                                "Unable to configure the Databricks client",
+                                fmt.Sprintf("Expected *common.DatabricksClient, got %T", req.ProviderData),
+                        )
 
-                return
+                r.client = workspaceClient
+                }
         }
-
-        r.client = client
 }
 
 func (r *permissionResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
-        resp.TypeName = pluginfwcommon.GetDatabrikcsProductionName(resourceName)
+        resp.TypeName = pluginfwcommon.GetDatabricksProductionName(resourceName)
 }
 
 
 type permissionResourceModel struct {
         ObjectID types.String `tfsdk:"object_id"`
         ObjectType types.String `tfsdk:"object_type"`
-        AccessControlList []accessControlListModel `tfsdk:"access_control_list"`
+        AccessControlList []permissionAccessControlListModel `tfsdk:"access_control_list"`
         LastUpdated types.String `tfsdk:"last_updated"`
 }
 
-type accessControlListModel struct {
+//accessControlListModel is the same as iam.AccessControlRequest
+// was originally just called this way in entity.go
+type permissionAccessControlListModel struct {
         ServicePrincipalName types.String `tfsdk:"service_principal_name"`
         GroupName types.String `tfsdk:"group_name"`
         UserName types.String `tfsdk:"user_name"`
@@ -76,6 +93,7 @@ type accessControlListModel struct {
 
 
 //TODO set some attributes as optional, required
+// see /databricks/permissions/resource_permissions.go line 160
 func (r *permissionResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
         attrs, blocks := tfschema.ResourceStructToSchemaMap(ctx, permissionResourceModel{}, nil)
         resp.Schema = schema.Schema{
@@ -86,21 +104,41 @@ func (r *permissionResource) Schema(ctx context.Context, req resource.SchemaRequ
 
 
 func (r *permissionResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-        ctx := pluginfwcontext.SetUserAgentInResourceContext(ctx, resourceName)
-        w, diags := r.client.WorkspaceClient()
+        ctx = pluginfwcontext.SetUserAgentInResourceContext(ctx, resourceName)
+
+        var plan permissionResourceModel
+        diags := req.Plan.Get(ctx, &plan)
         resp.Diagnostics.Append(diags...)
+        if resp.Diagnostics.HasError() {
+                return
+        }
+
+        // generate API request from plan
+        var acls []iam.AccessControlRequest
+        for _, acl := range plan.AccessControlList {
+        }
+
+        // create the permission
+        //TODO: FIGURE OUT PROPER API CALL METHOD FOR THIS ACTION
+        acl, err := r.client.AccessControl.Update(<OBJECT ID?>, iam.UpdateRuleSetRequest{
+                Name: plan.ObjectID,
+                ObjectType: plan.ObjectType,
+                AccessControlList: acls,
+        })
+
+
 }
 
 func (r *permissionResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
-        ctx := pluginfwcontext.SetUserAgentInResourceContext(ctx, resourceName)
+        ctx = pluginfwcontext.SetUserAgentInResourceContext(ctx, resourceName)
 }
 
 func (r *permissionResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-        ctx := pluginfwcontext.SetUserAgentInResourceContext(ctx, resourceName)
+        ctx = pluginfwcontext.SetUserAgentInResourceContext(ctx, resourceName)
 }
 
 func (r *permissionResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
-        ctx := pluginfwcontext.SetUserAgentInResourceContext(ctx, resourceName)
+        ctx = pluginfwcontext.SetUserAgentInResourceContext(ctx, resourceName)
 }
 
 

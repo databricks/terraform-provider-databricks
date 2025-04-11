3e15c353faa7b21709ae6863e47b7afe72be05fc
Alex Ott <alexott@gmail.com>
2025-04-11 12:09:59 +0200

    [Feature] Support for `lifetime` attribute in `databricks_service_principal_secret` resource
    Also refactored it to use Go SDK to expose more attributes.

--- NEXT_CHANGELOG.md
+++ NEXT_CHANGELOG.md
@@ -4,6 +4,8 @@
 
 ### New Features and Improvements
 
+* Support for `lifetime` attribute in `databricks_service_principal_secret` resource ([#4633](https://github.com/databricks/terraform-provider-databricks/pull/4633)).
+
 ### Bug Fixes
 
 ### Documentation
--- tokens/resource_service_principal_secret_test.go
+++ tokens/resource_service_principal_secret_test.go
@@ -2,59 +2,33 @@ package tokens
 
 import (
 	"testing"
+	"time"
 
 	"github.com/databricks/terraform-provider-databricks/qa"
+
+	"github.com/databricks/databricks-sdk-go/service/oauth2"
 )
 
 func TestServicePrincipalSecretCreate(t *testing.T) {
+	expireTime := time.Now().Add(20 * time.Second).Format(time.RFC3339)
+	currentTime := time.Now().Format(time.RFC3339)
 	qa.ResourceFixture{
 		Fixtures: []qa.HTTPFixture{
 			{
-				Method:   "GET",
-				Resource: "/api/2.0/accounts/xyz/servicePrincipals/abc/credentials/secrets",
-				Response: ListServicePrincipalSecrets{
-					Secrets: []ServicePrincipalSecret{
-						{
-							ID: "001",
-						},
-					},
-				},
-			},
-			{
 				Method:   "POST",
-				Resource: "/api/2.0/accounts/xyz/servicePrincipals/abc/credentials/secrets",
-				Response: ServicePrincipalSecret{
-					Secret: "qwe",
+				Resource: "/api/2.0/accounts/xyz/servicePrincipals/123/credentials/secrets",
+				ExpectedRequest: oauth2.CreateServicePrincipalSecretRequest{
+					ServicePrincipalId: 123,
+					Lifetime:           "20s",
 				},
-			},
-			{
-				Method:   "GET",
-				Resource: "/api/2.0/accounts/xyz/servicePrincipals/abc/credentials/secrets",
-				Response: ListServicePrincipalSecrets{
-					Secrets: []ServicePrincipalSecret{
-						{
-							ID: "001",
-						},
-						{
-							ID: "002",
-						},
-					},
-				},
-			},
-			{
-				Method:   "GET",
-				Resource: "/api/2.0/accounts/xyz/servicePrincipals/abc/credentials/secrets",
-				Response: ListServicePrincipalSecrets{
-					Secrets: []ServicePrincipalSecret{
-						{
-							ID:     "001",
-							Status: "foo",
-						},
-						{
-							ID:     "002",
-							Status: "bar",
-						},
-					},
+				Response: oauth2.CreateServicePrincipalSecretResponse{
+					Secret:     "qwe",
+					Id:         "003",
+					Status:     "ACTIVE",
+					ExpireTime: expireTime,
+					CreateTime: currentTime,
+					UpdateTime: currentTime,
+					SecretHash: "abc",
 				},
 			},
 		},
@@ -62,12 +36,20 @@ func TestServicePrincipalSecretCreate(t *testing.T) {
 		Create:    true,
 		AccountID: "xyz",
 		HCL: `
-		service_principal_id = "abc"
+		service_principal_id = "123"
+		lifetime  	  		 = "20s"
 		`,
 		New: true,
 	}.ApplyAndExpectData(t, map[string]any{
-		"secret": "qwe",
-		"status": "bar",
+		"secret":               "qwe",
+		"status":               "ACTIVE",
+		"service_principal_id": "123",
+		"id":                   "003",
+		"expire_time":          expireTime,
+		"create_time":          currentTime,
+		"update_time":          currentTime,
+		"secret_hash":          "abc",
+		"lifetime":             "20s",
 	})
 }
 
@@ -76,7 +58,7 @@ func TestServicePrincipalSecretDelete(t *testing.T) {
 		Fixtures: []qa.HTTPFixture{
 			{
 				Method:   "DELETE",
-				Resource: "/api/2.0/accounts/xyz/servicePrincipals/abc/credentials/secrets/003",
+				Resource: "/api/2.0/accounts/xyz/servicePrincipals/123/credentials/secrets/003?",
 			},
 		},
 		Resource:  ResourceServicePrincipalSecret(),
@@ -84,12 +66,65 @@ func TestServicePrincipalSecretDelete(t *testing.T) {
 		Delete:    true,
 		AccountID: "xyz",
 		HCL: `
-		service_principal_id = "abc"
+		service_principal_id = "123"
 		`,
 	}.ApplyNoError(t)
 }
 
-func TestServicePrincipalSecretFuzz(t *testing.T) {
-	qa.ResourceCornerCases(t, ResourceServicePrincipalSecret(),
-		qa.CornerCaseExpectError("must have `account_id` on provider"))
+func TestServicePrincipalSecretRead(t *testing.T) {
+	qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "GET",
+				Resource: "/api/2.0/accounts/xyz/servicePrincipals/123/credentials/secrets?",
+				Response: oauth2.ListServicePrincipalSecretsResponse{
+					Secrets: []oauth2.SecretInfo{
+						{
+							Id:         "003",
+							SecretHash: "abc",
+							Status:     "ACTIVE",
+						},
+					},
+				},
+			},
+		},
+		Resource:  ResourceServicePrincipalSecret(),
+		ID:        "003",
+		Read:      true,
+		AccountID: "xyz",
+		HCL: `
+		service_principal_id = "123"
+		`,
+	}.ApplyAndExpectData(t, map[string]any{
+		"secret_hash": "abc",
+		"status":      "ACTIVE",
+	})
+}
+
+func TestServicePrincipalSecretReadRemoved(t *testing.T) {
+	qa.ResourceFixture{
+		Fixtures: []qa.HTTPFixture{
+			{
+				Method:   "GET",
+				Resource: "/api/2.0/accounts/xyz/servicePrincipals/123/credentials/secrets?",
+				Response: oauth2.ListServicePrincipalSecretsResponse{
+					Secrets: []oauth2.SecretInfo{
+						{
+							Id:         "004",
+							SecretHash: "abc",
+							Status:     "ACTIVE",
+						},
+					},
+				},
+			},
+		},
+		Resource:  ResourceServicePrincipalSecret(),
+		ID:        "003",
+		Read:      true,
+		Removed:   true,
+		AccountID: "xyz",
+		HCL: `
+		service_principal_id = "123"
+		`,
+	}.ApplyNoError(t)
 }

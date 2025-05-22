749aedbe1080739427a7b5d6e9d45c334b6bbee2
Tanmay Rustagi <tanmay.rustagi@databricks.com>
2025-05-22 15:35:13 +0200

    -

--- common/client_test.go
+++ common/client_test.go
@@ -319,7 +319,7 @@ func TestGetJWTProperty_Authenticate_Fail(t *testing.T) {
 	_, err := client.GetAzureJwtProperty("tid")
 	require.Error(t, err)
 	assert.True(t, strings.HasPrefix(err.Error(),
-		"default auth: azure-cli: cannot get account info"))
+		"default auth: cannot configure default credentials"))
 }
 
 type mockInternalUserService struct {

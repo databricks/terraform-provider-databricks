5bfd69d2806cc1205e3425453e67d9db579039e9
Vuong <vuong.nguyen@databricks.com>
2025-05-23 15:55:09 +0800

    don't change instance pool

--- pools/resource_instance_pool.go
+++ pools/resource_instance_pool.go
@@ -2,6 +2,7 @@ package pools
 
 import (
 	"context"
+	"strings"
 
 	"github.com/databricks/terraform-provider-databricks/clusters"
 	"github.com/databricks/terraform-provider-databricks/common"
@@ -185,7 +186,9 @@ func ResourceInstancePool() common.Resource {
 		if v, err := common.SchemaPath(s, "aws_attributes", "spot_bid_price_percent"); err == nil {
 			v.Default = 100
 		}
-		common.MustSchemaPath(s, "aws_attributes", "zone_id").DiffSuppressFunc = common.EqualFoldDiffSuppress
+		common.MustSchemaPath(s, "aws_attributes", "zone_id").DiffSuppressFunc = func(k, oldValue, newValue string, d *schema.ResourceData) bool {
+			return oldValue != "" && strings.ToLower(newValue) == "auto"
+		}
 
 		if v, err := common.SchemaPath(s, "azure_attributes", "availability"); err == nil {
 			v.Default = clusters.AzureAvailabilityOnDemand

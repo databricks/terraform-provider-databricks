ecc4374399b4380e225a2e149b04a3a1171bc26c
Alex Ott <alexott@gmail.com>
2025-10-06 10:32:01 +0200

    [Internal] Rewrite `databricks_job` data source to use Go SDK
    With this refactoring we'll fix a problem when a job has more than 100 tasks - current
    implementation won't handle it correctly. Plus, it won't work in the workspaces with a lot
    of jobs...
    
    Also, refactored job tests to use Mock API instead of `HTTPFixture`.

--- jobs/data_job.go
+++ jobs/data_job.go
@@ -3,22 +3,23 @@ package jobs
 import (
 	"context"
 	"fmt"
+	"strconv"
 
+	"github.com/databricks/databricks-sdk-go"
+	sdk_jobs "github.com/databricks/databricks-sdk-go/service/jobs"
 	"github.com/databricks/terraform-provider-databricks/common"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func DataSourceJob() common.Resource {
 	type queryableJobData struct {
-		Id      string `json:"id,omitempty" tf:"computed"`
-		JobId   string `json:"job_id,omitempty" tf:"computed"`
-		Name    string `json:"name,omitempty" tf:"computed"`
-		JobName string `json:"job_name,omitempty" tf:"computed"`
-		Job     *Job   `json:"job_settings,omitempty" tf:"computed"`
+		Id      string        `json:"id,omitempty" tf:"computed"`
+		JobId   string        `json:"job_id,omitempty" tf:"computed"`
+		Name    string        `json:"name,omitempty" tf:"computed"`
+		JobName string        `json:"job_name,omitempty" tf:"computed"`
+		Job     *sdk_jobs.Job `json:"job_settings,omitempty" tf:"computed"`
 	}
-	return common.DataResource(queryableJobData{}, func(ctx context.Context, e any, c *common.DatabricksClient) error {
-		data := e.(*queryableJobData)
-		jobsAPI := NewJobsAPI(ctx, c)
-		var list []Job
+	return common.WorkspaceDataWithCustomizeFunc(func(ctx context.Context, data *queryableJobData, w *databricks.WorkspaceClient) error {
 		var err error
 		if data.Id == "" {
 			data.Id = data.JobId
@@ -28,35 +29,52 @@ func DataSourceJob() common.Resource {
 		}
 		if data.Name != "" {
 			// if name is provided, need to list all jobs ny name
-			list, err = jobsAPI.ListByName(data.Name, true)
-		} else {
-			// otherwise, just read the job
-			var job Job
-			job, err = jobsAPI.Read(data.Id)
+			// find only job ID, then use `GetByJobId` to get the job with all fields
+			list, err := w.Jobs.ListAll(ctx, sdk_jobs.ListJobsRequest{ExpandTasks: false, Name: data.Name, Limit: 100})
 			if err != nil {
 				return err
 			}
-			data.Job = &job
-			data.Name = job.Settings.Name
+			found := false
+			for _, job := range list {
+				currentJob := job // De-referencing the temp variable used by the loop
+				currentJobName := currentJob.Settings.Name
+				jobIdString := strconv.FormatInt(currentJob.JobId, 10)
+				if currentJobName == data.Name || jobIdString == data.Id {
+					data.Name = currentJobName
+					data.Id = jobIdString
+					found = true
+					break // break the loop after we found the job
+				}
+			}
+			if !found {
+				return fmt.Errorf("no job found with specified name")
+			}
 		}
+		// read the job by ID
+		jobId, err := strconv.ParseInt(data.Id, 10, 64)
 		if err != nil {
 			return err
 		}
-		for _, job := range list {
-			currentJob := job // De-referencing the temp variable used by the loop
-			currentJobId := currentJob.ID()
-			currentJobName := currentJob.Settings.Name
-			if currentJobName == data.Name || currentJobId == data.Id {
-				data.Job = &currentJob
-				data.Name = currentJobName
-				data.Id = currentJobId
-				data.JobId = currentJobId
-				return nil // break the loop after we found the job
-			}
+		job, err := w.Jobs.Get(ctx, sdk_jobs.GetJobRequest{JobId: jobId})
+		if err != nil {
+			return err
 		}
+		data.Job = job
+		data.Name = job.Settings.Name
+		data.JobName = job.Settings.Name
+		data.JobId = data.Id
 		if data.Job == nil {
 			return fmt.Errorf("no job found with specified name")
 		}
+
 		return nil
+	}, func(s map[string]*schema.Schema) map[string]*schema.Schema {
+		// Customize the git_source schema to fix ConflictsWith paths
+		if gitSource, err := common.SchemaPath(s, "job_settings", "settings", "git_source"); err == nil {
+			if gitSourceResource, ok := gitSource.Elem.(*schema.Resource); ok {
+				gitSourceSchema(gitSourceResource.Schema, "job_settings.0.settings.0.")
+			}
+		}
+		return s
 	})
 }

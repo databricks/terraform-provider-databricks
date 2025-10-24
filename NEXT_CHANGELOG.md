49956d216a716567a52c116a28a0a15b42dc0881
Tanmay Rustagi <88379306+tanmay-db@users.noreply.github.com>
2025-10-24 15:35:31 +0530

    [Feature] Add unified provider support for manual plugin framework resources and data sources (#5127)
    ## Changes
    <!-- Summary of your changes that are easy to understand -->
    Support for unified provider for manual plugin framework resources.
    
    To be merge after:
    https://github.com/databricks/terraform-provider-databricks/pull/5115
    
    ## Tests
    <!--
    How is this tested? Please see the checklist below and also describe any
    other relevant tests
    -->
    Integration tests
    Unit tests

--- NEXT_CHANGELOG.md
+++ NEXT_CHANGELOG.md
@@ -6,6 +6,8 @@
 
 ### New Features and Improvements
 
+* Add `provider_config` support for manual plugin framework resources and data sources([#5127](https://github.com/databricks/terraform-provider-databricks/pull/5127))
+
 ### Bug Fixes
 
 ### Documentation
--- internal/providers/pluginfw/products/dashboards/data_dashboards.go
+++ internal/providers/pluginfw/products/dashboards/data_dashboards.go
@@ -34,18 +34,20 @@ type DashboardsDataSource struct {
 type DashboardsInfo struct {
 	DashboardNameContains types.String `tfsdk:"dashboard_name_contains"`
 	Dashboards            types.List   `tfsdk:"dashboards"`
+	tfschema.Namespace
 }
 
 func (DashboardsInfo) ApplySchemaCustomizations(attrs map[string]tfschema.AttributeBuilder) map[string]tfschema.AttributeBuilder {
 	attrs["dashboard_name_contains"] = attrs["dashboard_name_contains"].SetOptional()
 	attrs["dashboards"] = attrs["dashboards"].SetComputed()
-
+	attrs["provider_config"] = attrs["provider_config"].SetOptional()
 	return attrs
 }
 
 func (DashboardsInfo) GetComplexFieldTypes(context.Context) map[string]reflect.Type {
 	return map[string]reflect.Type{
-		"dashboards": reflect.TypeOf(dashboards_tf.Dashboard{}),
+		"dashboards":      reflect.TypeOf(dashboards_tf.Dashboard{}),
+		"provider_config": reflect.TypeOf(tfschema.ProviderConfigData{}),
 	}
 }
 
@@ -74,13 +76,21 @@ func AppendDiagAndCheckErrors(resp *datasource.ReadResponse, diags diag.Diagnost
 
 func (d *DashboardsDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
 	ctx = pluginfwcontext.SetUserAgentInDataSourceContext(ctx, dataSourceName)
-	w, diags := d.Client.GetWorkspaceClient()
-	if AppendDiagAndCheckErrors(resp, diags) {
-		return
-	}
 
 	var dashboardInfo DashboardsInfo
 	if AppendDiagAndCheckErrors(resp, req.Config.Get(ctx, &dashboardInfo)) {
+		return
+	}
+
+	workspaceID, diags := tfschema.GetWorkspaceIDDataSource(ctx, dashboardInfo.ProviderConfig)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	w, diags := d.Client.GetWorkspaceClientForUnifiedProviderWithDiagnostics(ctx, workspaceID)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
 		return
 	}
 

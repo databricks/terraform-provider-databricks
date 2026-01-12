bf91b5c95d924ae4e5478b4afbc2f5fc487723cd
Raymond Guo <raymond.guo@databricks.com>
2026-01-12 14:19:27 -0800

    Add fallback to sql_warehouse if data source API fails

--- sql/data_sql_warehouse.go
+++ sql/data_sql_warehouse.go
@@ -22,38 +22,68 @@ func DataSourceWarehouse() common.Resource {
 		if data.Id == "" && data.Name == "" {
 			return nil, fmt.Errorf("either 'id' or 'name' should be provided")
 		}
-		selected := []sql.DataSource{}
+
+		var warehouseId string
+		var dataSourceId string
+
 		dataSources, err := w.DataSources.List(ctx)
-		if err != nil {
-			return nil, err
-		}
-		for _, source := range dataSources {
-			if data.Name != "" && source.Name == data.Name {
-				selected = append(selected, source)
-			} else if data.Id != "" && source.WarehouseId == data.Id {
-				selected = append(selected, source)
-				break
+		if err == nil {
+			selected := []sql.DataSource{}
+			for _, source := range dataSources {
+				if data.Name != "" && source.Name == data.Name {
+					selected = append(selected, source)
+				} else if data.Id != "" && source.WarehouseId == data.Id {
+					selected = append(selected, source)
+					break
+				}
 			}
-		}
-		if len(selected) == 0 {
-			if data.Name != "" {
+			if len(selected) == 0 {
+				if data.Name != "" {
+					return nil, fmt.Errorf("can't find SQL warehouse with the name '%s'", data.Name)
+				} else {
+					return nil, fmt.Errorf("can't find SQL warehouse with the ID '%s'", data.Id)
+				}
+			}
+			if len(selected) > 1 {
+				if data.Name != "" {
+					return nil, fmt.Errorf("there are multiple SQL warehouses with the name '%s'", data.Name)
+				} else {
+					return nil, fmt.Errorf("there are multiple SQL warehouses with the ID '%s'", data.Id)
+				}
+			}
+			warehouseId = selected[0].WarehouseId
+			dataSourceId = selected[0].Id
+	} else {
+		// DataSources.List failed; fall back to finding warehouse without DataSourceId
+		if data.Name != "" {
+			// Find warehouse by name using Warehouses.ListAll
+			list, err := w.Warehouses.ListAll(ctx, sql.ListWarehousesRequest{})
+			if err != nil {
+				return nil, err
+			}
+			var matched []sql.EndpointInfo
+			for _, wh := range list {
+				if wh.Name == data.Name {
+					matched = append(matched, wh)
+				}
+			}
+			if len(matched) == 0 {
 				return nil, fmt.Errorf("can't find SQL warehouse with the name '%s'", data.Name)
-			} else {
-				return nil, fmt.Errorf("can't find SQL warehouse with the ID '%s'", data.Id)
 			}
-		}
-		if len(selected) > 1 {
-			if data.Name != "" {
+			if len(matched) > 1 {
 				return nil, fmt.Errorf("there are multiple SQL warehouses with the name '%s'", data.Name)
-			} else {
-				return nil, fmt.Errorf("there are multiple SQL warehouses with the ID '%s'", data.Id)
 			}
+			warehouseId = matched[0].Id
+		} else {
+			warehouseId = data.Id
 		}
-		warehouse, err := getSqlWarehouse(ctx, w, selected[0].WarehouseId)
+	}
+
+		warehouse, err := getSqlWarehouse(ctx, w, warehouseId)
 		if err != nil {
 			return nil, err
 		}
-		warehouse.DataSourceId = selected[0].Id
+		warehouse.DataSourceId = dataSourceId
 		return warehouse, nil
 	})
 }

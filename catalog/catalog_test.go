3bafda4156754072eccb0ce75850f92c88fa99a6
Tanmay Rustagi <tanmay.rustagi@databricks.com>
2026-02-11 00:03:42 +0100

    -

--- catalog/catalog_test.go
+++ catalog/catalog_test.go
@@ -284,7 +284,6 @@ func TestAccCatalog_ProviderConfig_Mismatched(t *testing.T) {
 			}
 		`),
 		ExpectError: regexp.MustCompile(`workspace_id mismatch.*please check the workspace_id provided in provider_config`),
-		PlanOnly:    true,
 	})
 }
 

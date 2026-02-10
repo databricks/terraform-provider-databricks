79a84a1bb93861d9c83e97075f2e8452ad2e20f5
Tanmay Rustagi <tanmay.rustagi@databricks.com>
2026-02-10 15:28:25 +0100

    [Internal] Onboard settings resources to unified provider

--- settings/default_namespace_test.go
+++ settings/default_namespace_test.go
@@ -3,16 +3,156 @@ package settings_test
 import (
 	"context"
 	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
 	"testing"
 
+	"github.com/databricks/databricks-sdk-go"
 	"github.com/databricks/databricks-sdk-go/apierr"
 	"github.com/databricks/databricks-sdk-go/service/settings"
 	"github.com/databricks/terraform-provider-databricks/common"
 	"github.com/databricks/terraform-provider-databricks/internal/acceptance"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func defaultNamespaceSettingTemplate(providerConfig string) string {
+	return fmt.Sprintf(`
+	resource "databricks_default_namespace_setting" "this" {
+		namespace {
+			value = "namespace_value"
+		}
+		%s
+	}
+	`, providerConfig)
+}
+
+func TestAccDefaultNamespaceSetting_ProviderConfig_Invalid(t *testing.T) {
+	acceptance.WorkspaceLevel(t, acceptance.Step{
+		Template: defaultNamespaceSettingTemplate(`
+			provider_config {
+				workspace_id = "invalid"
+			}
+		`),
+		ExpectError: regexp.MustCompile(`workspace_id must be a positive integer without leading zeros`),
+		PlanOnly:    true,
+	})
+}
+
+func TestAccDefaultNamespaceSetting_ProviderConfig_Mismatched(t *testing.T) {
+	acceptance.WorkspaceLevel(t, acceptance.Step{
+		Template: defaultNamespaceSettingTemplate(`
+			provider_config {
+				workspace_id = "123"
+			}
+		`),
+		ExpectError: regexp.MustCompile(`workspace_id mismatch.*please check the workspace_id provided in provider_config`),
+	})
+}
+
+func TestAccDefaultNamespaceSetting_ProviderConfig_Required(t *testing.T) {
+	acceptance.WorkspaceLevel(t, acceptance.Step{
+		Template: defaultNamespaceSettingTemplate(`
+			provider_config {
+			}
+		`),
+		ExpectError: regexp.MustCompile(`The argument "workspace_id" is required, but no definition was found.`),
+	})
+}
+
+func TestAccDefaultNamespaceSetting_ProviderConfig_EmptyID(t *testing.T) {
+	acceptance.WorkspaceLevel(t, acceptance.Step{
+		Template: defaultNamespaceSettingTemplate(`
+			provider_config {
+				workspace_id = ""
+			}
+		`),
+		ExpectError: regexp.MustCompile(`expected "provider_config.0.workspace_id" to not be an empty string`),
+	})
+}
+
+func TestAccDefaultNamespaceSetting_ProviderConfig_Match(t *testing.T) {
+	acceptance.LoadWorkspaceEnv(t)
+	ctx := context.Background()
+	w := databricks.Must(databricks.NewWorkspaceClient())
+	workspaceID, err := w.CurrentWorkspaceID(ctx)
+	require.NoError(t, err)
+	workspaceIDStr := strconv.FormatInt(workspaceID, 10)
+	acceptance.WorkspaceLevel(t, acceptance.Step{
+		Template: defaultNamespaceSettingTemplate(""),
+	}, acceptance.Step{
+		Template: defaultNamespaceSettingTemplate(fmt.Sprintf(`
+			provider_config {
+				workspace_id = "%s"
+			}
+		`, workspaceIDStr)),
+		ConfigPlanChecks: resource.ConfigPlanChecks{
+			PreApply: []plancheck.PlanCheck{
+				plancheck.ExpectResourceAction("databricks_default_namespace_setting.this", plancheck.ResourceActionUpdate),
+			},
+		},
+	})
+}
+
+func TestAccDefaultNamespaceSetting_ProviderConfig_Recreate(t *testing.T) {
+	acceptance.LoadWorkspaceEnv(t)
+	ctx := context.Background()
+	w := databricks.Must(databricks.NewWorkspaceClient())
+	workspaceID, err := w.CurrentWorkspaceID(ctx)
+	require.NoError(t, err)
+	workspaceIDStr := strconv.FormatInt(workspaceID, 10)
+	acceptance.WorkspaceLevel(t, acceptance.Step{
+		Template: defaultNamespaceSettingTemplate(""),
+	}, acceptance.Step{
+		Template: defaultNamespaceSettingTemplate(fmt.Sprintf(`
+			provider_config {
+				workspace_id = "%s"
+			}
+		`, workspaceIDStr)),
+	}, acceptance.Step{
+		Template: defaultNamespaceSettingTemplate(`
+			provider_config {
+				workspace_id = "123"
+			}
+		`),
+		ConfigPlanChecks: resource.ConfigPlanChecks{
+			PostApplyPreRefresh: []plancheck.PlanCheck{
+				plancheck.ExpectResourceAction("databricks_default_namespace_setting.this", plancheck.ResourceActionDestroyBeforeCreate),
+			},
+		},
+		PlanOnly:           true,
+		ExpectNonEmptyPlan: true,
+	})
+}
+
+func TestAccDefaultNamespaceSetting_ProviderConfig_Remove(t *testing.T) {
+	acceptance.LoadWorkspaceEnv(t)
+	ctx := context.Background()
+	w := databricks.Must(databricks.NewWorkspaceClient())
+	workspaceID, err := w.CurrentWorkspaceID(ctx)
+	require.NoError(t, err)
+	workspaceIDStr := strconv.FormatInt(workspaceID, 10)
+	acceptance.WorkspaceLevel(t, acceptance.Step{
+		Template: defaultNamespaceSettingTemplate(""),
+	}, acceptance.Step{
+		Template: defaultNamespaceSettingTemplate(fmt.Sprintf(`
+			provider_config {
+				workspace_id = "%s"
+			}
+		`, workspaceIDStr)),
+	}, acceptance.Step{
+		Template: defaultNamespaceSettingTemplate(""),
+		ConfigPlanChecks: resource.ConfigPlanChecks{
+			PreApply: []plancheck.PlanCheck{
+				plancheck.ExpectResourceAction("databricks_default_namespace_setting.this", plancheck.ResourceActionUpdate),
+			},
+		},
+	})
+}
 
 func TestAccDefaultNamespaceSetting(t *testing.T) {
 	template := `

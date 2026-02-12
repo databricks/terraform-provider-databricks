2c703a3bd7df1d1292157e799d9ae44dd526fedb
jlieow <52845474+jlieow@users.noreply.github.com>
2026-02-12 14:20:03 +0000

    [Fix] Fix permanent permissions drift when user_name casing differs from API response (#5183)

--- permissions/permission_definitions.go
+++ permissions/permission_definitions.go
@@ -228,7 +228,7 @@ func (p resourcePermissions) prepareResponse(objectID string, objectACL *iam.Obj
 		// If the user doesn't include an access_control block for themselves, do not include it in the state.
 		// On create/update, the provider will automatically include the current user in the access_control block
 		// for appropriate resources. Otherwise, it must be included in state to prevent configuration drift.
-		if me == accessControl.UserName || me == accessControl.ServicePrincipalName {
+		if strings.EqualFold(me, accessControl.UserName) || strings.EqualFold(me, accessControl.ServicePrincipalName) {
 			if !existing.ContainsUserOrServicePrincipal(me) {
 				continue
 			}

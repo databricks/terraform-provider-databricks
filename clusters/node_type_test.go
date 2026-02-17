1c71c4bab936e4a2ca1952389ca6c97a224dad9b
Tanmay Rustagi <tanmay.rustagi@databricks.com>
2026-02-18 00:55:57 +0100

    [Internal] Node type validation

---
+++ clusters/node_type_test.go
@@ -0,0 +1,116 @@
+package clusters_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/databricks/databricks-sdk-go"
+	"github.com/databricks/terraform-provider-databricks/internal/acceptance"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAccCluster_ListNodeTypesAWS(t *testing.T) {
+	acceptance.LoadWorkspaceEnv(t)
+	w := databricks.Must(databricks.NewWorkspaceClient())
+	nodeTypes, err := w.Clusters.ListNodeTypes(context.Background())
+	require.NoError(t, err)
+	require.NotEmpty(t, nodeTypes.NodeTypes)
+
+	azureAndGcpNodeTypes := []string{
+		"Standard_D4pds_v6",
+		"Standard_D4ds_v5",
+		"n1-standard-4",
+	}
+	for _, nt := range nodeTypes.NodeTypes {
+		for _, excluded := range azureAndGcpNodeTypes {
+			assert.NotEqual(t, excluded, nt.NodeTypeId,
+				"node type %s should not be present in the response", excluded)
+		}
+	}
+
+	awsNodeTypes := []string{
+		"rgd-fleet.xlarge",
+		"m6g.xlarge",
+		"md-fleet.xlarge",
+		"i3.xlarge",
+	}
+	nodeTypeIds := make(map[string]bool)
+	for _, nt := range nodeTypes.NodeTypes {
+		nodeTypeIds[nt.NodeTypeId] = true
+	}
+	for _, expected := range awsNodeTypes {
+		assert.True(t, nodeTypeIds[expected],
+			"expected AWS node type %s to be present in the response", expected)
+	}
+}
+
+func TestAccClusterAPI_ListNodeTypesAzure(t *testing.T) {
+	acceptance.LoadWorkspaceEnv(t)
+	w := databricks.Must(databricks.NewWorkspaceClient())
+	nodeTypes, err := w.Clusters.ListNodeTypes(context.Background())
+	require.NoError(t, err)
+	require.NotEmpty(t, nodeTypes.NodeTypes)
+
+	awsAndGcpNodeTypes := []string{
+		"rgd-fleet.xlarge",
+		"m6g.xlarge",
+		"md-fleet.xlarge",
+		"i3.xlarge",
+		"n1-standard-4",
+	}
+	for _, nt := range nodeTypes.NodeTypes {
+		for _, excluded := range awsAndGcpNodeTypes {
+			assert.NotEqual(t, excluded, nt.NodeTypeId,
+				"node type %s should not be present in the response", excluded)
+		}
+	}
+
+	azureNodeTypes := []string{
+		"Standard_D4pds_v6",
+		"Standard_D4ds_v5",
+	}
+	nodeTypeIds := make(map[string]bool)
+	for _, nt := range nodeTypes.NodeTypes {
+		nodeTypeIds[nt.NodeTypeId] = true
+	}
+	for _, expected := range azureNodeTypes {
+		assert.True(t, nodeTypeIds[expected],
+			"expected Azure node type %s to be present in the response", expected)
+	}
+}
+
+func TestAccClusterAPI_ListNodeTypesGcp(t *testing.T) {
+	acceptance.LoadWorkspaceEnv(t)
+	w := databricks.Must(databricks.NewWorkspaceClient())
+	nodeTypes, err := w.Clusters.ListNodeTypes(context.Background())
+	require.NoError(t, err)
+	require.NotEmpty(t, nodeTypes.NodeTypes)
+
+	awsAndAzureNodeTypes := []string{
+		"rgd-fleet.xlarge",
+		"m6g.xlarge",
+		"md-fleet.xlarge",
+		"i3.xlarge",
+		"Standard_D4pds_v6",
+		"Standard_D4ds_v5",
+	}
+	for _, nt := range nodeTypes.NodeTypes {
+		for _, excluded := range awsAndAzureNodeTypes {
+			assert.NotEqual(t, excluded, nt.NodeTypeId,
+				"node type %s should not be present in the response", excluded)
+		}
+	}
+
+	gcpNodeTypes := []string{
+		"n1-standard-4",
+	}
+	nodeTypeIds := make(map[string]bool)
+	for _, nt := range nodeTypes.NodeTypes {
+		nodeTypeIds[nt.NodeTypeId] = true
+	}
+	for _, expected := range gcpNodeTypes {
+		assert.True(t, nodeTypeIds[expected],
+			"expected GCP node type %s to be present in the response", expected)
+	}
+}

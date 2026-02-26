3700c6995ca893a9931b7fcf88f4e0f659acfc08
Tanmay Rustagi <tanmay.rustagi@databricks.com>
2026-02-26 21:35:48 +0100

    -

--- storage/gs_test.go
+++ storage/gs_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 
 	"github.com/databricks/databricks-sdk-go/apierr"
-	"github.com/databricks/terraform-provider-databricks/clusters"
 	"github.com/databricks/terraform-provider-databricks/common"
 	"github.com/databricks/terraform-provider-databricks/qa"
 	"github.com/stretchr/testify/assert"
@@ -30,77 +29,6 @@ func TestCreateOrValidateClusterForGoogleStorage_Failures(t *testing.T) {
 
 		err = createOrValidateClusterForGoogleStorage(ctx, client, d, "", "b")
 		assert.EqualError(t, err, "cannot create mounting cluster: cannot determine smallest node type: nope")
-	})
-}
-
-func TestCreateOrValidateClusterForGoogleStorage_WorksOnDeletedCluster(t *testing.T) {
-	qa.HTTPFixturesApply(t, []qa.HTTPFixture{
-		{
-			Method:   "GET",
-			Resource: "/api/2.0/clusters/get?cluster_id=removed-cluster",
-			Status:   404,
-			Response: &apierr.APIError{
-				ErrorCode:  "NOT_FOUND",
-				StatusCode: 404,
-				Message:    "cluster deleted",
-			},
-		},
-		{
-			Method:   "GET",
-			Resource: "/api/2.0/clusters/list",
-			Response: clusters.ClusterList{
-				Clusters: []clusters.ClusterInfo{},
-			},
-		},
-		{
-			ReuseRequest: true,
-			Method:       "GET",
-			Resource:     "/api/2.1/clusters/spark-versions",
-		},
-		{
-			ReuseRequest: true,
-			Method:       "GET",
-			Resource:     "/api/2.1/clusters/list-node-types",
-		},
-		{
-			Method:   "POST",
-			Resource: "/api/2.0/clusters/create",
-			ExpectedRequest: clusters.Cluster{
-				CustomTags: map[string]string{
-					"ResourceClass": "SingleNode",
-				},
-				ClusterName: "terraform-mount-gcs-03a56ec1d1576b505aabf088337cbf36",
-				GcpAttributes: &clusters.GcpAttributes{
-					GoogleServiceAccount: "service-account",
-				},
-				SparkVersion:           "11.3.x-scala2.12",
-				NumWorkers:             0,
-				NodeTypeID:             "i3.xlarge",
-				AutoterminationMinutes: 10,
-				SparkConf: map[string]string{
-					"spark.databricks.cluster.profile": "singleNode",
-					"spark.master":                     "local[*]",
-					"spark.scheduler.mode":             "FIFO",
-				},
-			},
-			Response: clusters.ClusterID{
-				ClusterID: "new-cluster",
-			},
-		},
-		{
-			Method:   "GET",
-			Resource: "/api/2.0/clusters/get?cluster_id=new-cluster",
-			Response: clusters.ClusterInfo{
-				ClusterID:    "new-cluster",
-				State:        "RUNNING",
-				StateMessage: "created",
-			},
-		},
-	}, func(ctx context.Context, client *common.DatabricksClient) {
-		d := ResourceMount().ToResource().TestResourceData()
-		err := createOrValidateClusterForGoogleStorage(ctx, client, d, "removed-cluster", "service-account")
-		assert.NoError(t, err)
-		assert.Equal(t, "new-cluster", d.Get("cluster_id"))
 	})
 }
 
